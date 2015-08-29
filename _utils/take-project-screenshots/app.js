f02bf0adab65f09424eddcdf396064b2c3881f11
omniaura <omniaura5@gmail.com>
2015-08-29 16:22:53 -0700

    node/phantom utility to save project screenshots

---
+++ _utils/take-project-screenshots/app.js
@@ -0,0 +1,29 @@
+var YAML = require('yamljs');
+var webshot = require('webshot');
+
+// settings
+var PROJECT_ROOT = '../../';
+var SCREENSHOT_DIR = PROJECT_ROOT + 'img/projects/';
+var PROJECTS_FILE = PROJECT_ROOT + '_data/projects.yml';
+var SCREENSHOT_OPTIONS = {
+  screenSize: {
+    width: 640,
+    height: 400
+  },
+};
+var FILE_EXTENSION = '.png';
+
+var projects = YAML.load(PROJECTS_FILE);
+
+for (var key in projects) {
+  if (projects.hasOwnProperty(key)) {
+    var project = projects[key];
+    webshot(project.link,
+            SCREENSHOT_DIR + key + FILE_EXTENSION, SCREENSHOT_OPTIONS, function(err) {
+      if (err)
+        console.log('ERROR saving screenshot for ' + project.name);
+      else
+        console.log('saved screenshot for ' + project.name);
+    });
+  }
+}
