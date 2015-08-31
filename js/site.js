f6527e43167a444771a589d78e32952b0eedbe59
omniaura <omniaura5@gmail.com>
2015-08-30 17:11:20 -0700

    added ascii logo, console apply link

--- js/site.js
+++ js/site.js
@@ -1,10 +1,32 @@
 (function(){
+  var asciiLogo = "████████╗██╗  ██╗███████╗    ███████╗████████╗ █████╗  ██████╗██╗  ██╗\n" +
+                  "╚══██╔══╝██║  ██║██╔════╝    ██╔════╝╚══██╔══╝██╔══██╗██╔════╝██║ ██╔╝\n" +
+                  "   ██║   ███████║█████╗      ███████╗   ██║   ███████║██║     █████╔╝ \n" +
+                  "   ██║   ██╔══██║██╔══╝      ╚════██║   ██║   ██╔══██║██║     ██╔═██╗ \n" +
+                  "   ██║   ██║  ██║███████╗    ███████║   ██║   ██║  ██║╚██████╗██║  ██╗\n" +
+                  "   ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝";
+
+  window.Stack = {
+    apply: function(){
+      console.log('Good luck!');
+      window.location = "http://recruiting.dailybruin.com/applications/daily-bruin/web-developer-intern/";
+    }
+  };
+
   function onLoad() {
+    console.log(asciiLogo);
+    console.log("Welcome to The Stack! Want to develop for us? Type Stack.apply();");
+
     // show a "jump to visualization" button if this page is a post that has
     // a visualization
     var visualization = document.querySelector('.post-content #visualization');
     if (visualization) {
-      document.getElementById('jump-to-visualization').style.display = "block";
+      var jumpToVisualization = document.getElementById('jump-to-visualization');
+      jumpToVisualization.style.display = "block";
+      // track click events on "jump to visualization" button
+      jumpToVisualization.addEventListener('click', function(){
+        ga('send', 'event', 'button', 'click', 'jump to visualization');
+      });
     }
   }
 
