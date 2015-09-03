f5f5e0c98768d2d957f37b1df231f72b4458c12c
omniaura <omniaura5@gmail.com>
2015-09-02 19:11:36 -0700

    improvements to take-project-screenshots utility

--- _utils/take-project-screenshots/app.js
+++ _utils/take-project-screenshots/app.js
@@ -6,12 +6,16 @@ var PROJECT_ROOT = '../../';
 var SCREENSHOT_DIR = PROJECT_ROOT + 'img/projects/';
 var PROJECTS_FILE = PROJECT_ROOT + '_data/projects.yml';
 var SCREENSHOT_OPTIONS = {
+  phantomPath: require('phantomjs2').path,
   screenSize: {
-    width: 640,
-    height: 400
+    width: 800,
+    height: 500
   },
+  renderDelay: 2000, // wait two seconds after page load to take screenshot
+  defaultWhiteBackground: true,
+  quality: 90
 };
-var FILE_EXTENSION = '.png';
+var FILE_EXTENSION = '.jpg';
 
 var projects = YAML.load(PROJECTS_FILE);
 
@@ -20,12 +24,16 @@ console.log('Saving screenshots of projects from ' + PROJECTS_FILE + ' to ' + SC
 for (var key in projects) {
   if (projects.hasOwnProperty(key)) {
     var project = projects[key];
+    if (!project.hasOwnProperty('link')) {
+      continue; // skip this project if it doesn't have a link
+    }
+    console.log('saving screenshot for ' + project.name + '...');
     webshot(project.link,
             SCREENSHOT_DIR + key + FILE_EXTENSION, SCREENSHOT_OPTIONS, function(err) {
       if (err)
-        console.log('ERROR saving screenshot for ' + project.name);
+        console.log('ERROR saving screenshot for ' + this.name + ': ' + err);
       else
-        console.log('saved screenshot for ' + project.name + ' as ' + key + FILE_EXTENSION);
-    });
+        console.log('saved screenshot for ' + this.name);
+    }.bind(project));
   }
 }
