34a65989dc730aa1d53f163e8da6901321b7bac9
tyson-ni <tni@ucla.edu>
2016-03-24 16:20:37 -0700

    Last commit before change of story

--- js/posts/profs-most/app.js
+++ js/posts/profs-most/app.js
@@ -20,7 +20,10 @@ var inBetween = [50, 30, 10, 10];
 var one = [92, 8];
 var smallberg = [57, 43];
 var eggert = [35, 30, 25, 10];
-
+var mcdevitt = [29, 19, 16, 10, 10, 6, 3, 3, 3];
+var sproul = [80, 20];
+var almohalwas = [17, 13, 13, 13, 9, 9, 9, 9, 4, 4];
+var gould = [50, 14, 9, 9, 9, 5, 5];
 
 var DATA = [
   {
@@ -30,26 +33,49 @@ var DATA = [
   {
     "name": "eggert",
     "numbers": eggert
+  },
+  {
+    "name": "mcdevitt",
+    "numbers": mcdevitt
+  },
+  {
+    "name": "sproul",
+    "numbers": sproul
   }
 ];
 
+var TREE_DATA = {
+  "prof": "mcdevitt",
+  "classes": [
+    {"name": 1, "number": 29},
+    {"name": 2, "number": 19},
+    {"name": 3, "number": 16},
+    {"name": 4, "number": 10},
+    {"name": 5, "number": 10},
+    {"name": 6, "number": 6},
+    {"name": 7, "number": 6},
+    {"name": 8, "number": 3},
+    {"name": 9, "number": 3}
+  ]
+};
+
+
+var horizontalGap = 0;
 var verticalGap = 200;
-var maxCircleRadius = 60;
+var maxRectWidth = 400;
+var rectHeight = 30;
 var centerX = 300;
 var firstCenterY = 150;
 
-var radiusScale = d3.scale.linear().domain([0, 100]).range([0, maxCircleRadius]);
+var maxCircleArea = 10000;
+
+var percentAreaScale = d3.scale.linear().domain([0, 100]).range([0, 1]);
+var widthScale = d3.scale.linear().domain([0, 100]).range([0, maxRectWidth]);
+var cornerScale = d3.scale.linear().domain([0, 100]).range([2, 30]);
 var colorScale = d3.scale.category10();
 
-var cumRadius = 0;
-
-for (var i = 0; i < DATA.length; i++) {
-  var radius = [];
-  DATA[i].numbers.map(function(d, i) {
-    radius.push(radiusScale(d));
-  });
-  DATA[i].radius = radius;
-}
+var cumWidth = 0;
+var cumPercentArea = 0;
 
 var g = d3.select("#viz")
     .append("svg")
@@ -59,25 +85,50 @@ var g = d3.select("#viz")
     .append("g")
     .translate(function(d, i) { return [centerX, firstCenterY + i * verticalGap] });
 
+var node = d3.select("#viz")
+    .append("div")
+    .style("width", 600)
+    .style("height", 600)
+
+
 var circles = g.selectAll("circle")
     .data(function(d) { return d.numbers })
     .enter()
     .append("circle");
 
-circles.attr("r", function(d, i) {
-      if (i == 0) {
-        cumRadius = 0;
-      }
-      cumRadius = cumRadius + radiusScale(d);
-      return cumRadius;
-    })
-    .attr("cx", 0)
-    .attr("cy", 0)
-    .style("stroke", "1px grey")
-    .style("fill", function(d, i) { return colorScale(i) })
-    .moveToBack()
-    .on("mouseover", function(d, i) { return mouseOver(d, i) })
-    .on("mouseleave", function(d, i) { return mouseLeave(d, i) })
+// circles.attr("r", function(d, i) {
+//     if (i == 0) {
+//       cumPercentArea = 0;
+//     }
+//     cumPercentArea = cumPercentArea + percentAreaScale(d);
+//     var radius = Math.sqrt(cumPercentArea * maxCircleArea / Math.PI);
+//     return radius;
+//   })
+//     .attr("cx", 0)
+//     .attr("cy", 0)
+//     .style("stroke", "1px grey")
+//     .style("fill", function(d, i) { return colorScale(i) })
+//     .moveToBack()
+
+// var rects = g.selectAll("rect")
+//     .data(function(d) { return d.numbers })
+//     .enter()
+//     .append("rect");
+//
+// rects.attr("x", function(d, i) {
+//     if (i === 0) {
+//       cumWidth = widthScale(d) + horizontalGap;
+//       return 0;
+//     } else {
+//       var oldPos = cumWidth;
+//       cumWidth = cumWidth + widthScale(d) + horizontalGap;
+//       return oldPos;
+//     }
+//   })
+//     .attr("y", 0)
+//     .attr("height", rectHeight)
+//     .attr("width", function(d, i) { return widthScale(d) })
+//     .style("fill", function(d, i) { return colorScale(i) })
 
 g.append("text")
   .text(function(d) { return d.name })
