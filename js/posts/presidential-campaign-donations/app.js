e9f6600d390b296df9d8a47d2b293802880c5aca
Chang Liu <shampliu@gmail.com>
2016-04-23 22:00:27 -0700

    bar chart

--- js/posts/presidential-campaign-donations/app.js
+++ js/posts/presidential-campaign-donations/app.js
@@ -2,8 +2,6 @@ var margin = {top: 40, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var formatPercent = d3.format(".0%");
-
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
 
@@ -17,14 +15,13 @@ var xAxis = d3.svg.axis()
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left")
-    .tickFormat(formatPercent);
 
-var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([-10, 0])
-  .html(function(d) {
-    return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
-  })
+// var tip = d3.tip()
+//   .attr('class', 'd3-tip')
+//   .offset([-10, 0])
+//   .html(function(d) {
+//     return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
+//   })
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -32,11 +29,13 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg.call(tip);
+// svg.call(tip);
 
-d3.tsv("/datasets/presidential-campaign-donations/data.tsv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+d3.json("/datasets/presidential-campaign-donations/result.json", function(error, data) {
+	data = data["dem"]["Sanders"];
+
+  x.domain(d3.range(data["colleges"]["ucla"]["jobs"].length));
+  y.domain([0, 200]);
 
   svg.append("g")
       .attr("class", "x axis")
@@ -46,27 +45,30 @@ d3.tsv("/datasets/presidential-campaign-donations/data.tsv", type, function(erro
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Frequency");
+    // .append("text")
+    //   .attr("transform", "rotate(-90)")
+    //   .attr("y", 6)
+    //   .attr("dy", ".71em")
+    //   .style("text-anchor", "end")
+    //   .text("Frequency");
 
   svg.selectAll(".bar")
-      .data(data)
+      .data(function(){
+      	console.log(data["colleges"]["ucla"]["jobs"])
+      	return data["colleges"]["ucla"]["jobs"]
+      })
     .enter().append("rect")
       .attr("class", "bar")
-      .attr("x", function(d) { return x(d.letter); })
+      .attr("x", function(d, i) { 
+      	return x(i); 
+      })
       .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.frequency); })
-      .attr("height", function(d) { return height - y(d.frequency); })
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide)
+      .attr("y", function(d) { 
+      	console.log(d.donators);
+      	return y(d.donators); 
+      })
+      .attr("height", function(d) { return height - y(d.donators); })
+      // .on('mouseover', tip.show)
+      // .on('mouseout', tip.hide)
 
 });
-
-function type(d) {
-  d.frequency = +d.frequency;
-  return d;
-}
