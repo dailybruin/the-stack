f489965379935d3256f372b82e8b4a8f4ea2a9b3
Li-Wei Chi <weberchichi@gmail.com>
2016-05-02 17:45:27 -0700

    animated horizontal chart

--- js/posts/presidential-campaign-donations/animated-horizontal-bar.js
+++ js/posts/presidential-campaign-donations/animated-horizontal-bar.js
@@ -1,15 +1,16 @@
-var margin = {top: 30, right: 0, bottom: 50, left: 50},
+var margin = {top: 30, right: 10, bottom: 50, left: 50},
 	width = 630 - margin.left - margin.right,
 	height = 500 - margin.top - margin.bottom;
 
 var xScale = d3.scale.linear()
+  .domain([0, 1])
 	.range([0, width]);
 
 var yScale = d3.scale.ordinal()
 	.rangeRoundBands([height, 0], 0.1);
 
 /* ADD BARCHART DIV TO HTML */
-var barSVG = d3.select("#barChart").append("svg")
+var barSVG = d3.select("#animated-horizontal-bar").append("svg")
 	.attr("width", width + margin.left + margin.right)
 	.attr("height", height + margin.top + margin.bottom)
 	.append("g")
@@ -17,7 +18,12 @@ var barSVG = d3.select("#barChart").append("svg")
 
 var xAxis = barSVG.append("g")
 	.attr("class", "x axis")
-	.call(xScale.axis = d3.svg.axis().scale(xScale).orient("top"));
+	.call(xScale.axis = d3.svg.axis().scale(xScale).orient("top"))
+  .append("text")
+  .attr("x", 550)
+  .attr("y", 20)
+  .style("text-anchor", "end")
+  .text("Percentage from UC schools (%)");
 
 var yAxis = barSVG.append("g")
 	.attr("class", "y axis")
@@ -29,6 +35,11 @@ var currMode = 1;
 function initBarGraph(initData) {
 	transitionyScale(initData);
 
+  var colleges = [];
+  // console.log(initData.colleges)
+  initData.colleges.map(function(d) { colleges.push(d.name); });
+  yScale.domain(colleges);
+
 	dataRects = barSVG.selectAll(".dataRect")
 		.data(initData.colleges)
 		.enter().append("g")
@@ -37,52 +48,40 @@ function initBarGraph(initData) {
 	dataRects.append("rect")
 		.attr("x", 0)
 		.attr("y", function(d) { return yScale(d.name); })
-		.attr("width", function(d) { return xScale(d.total); })
+		.attr("width", function(d) { return xScale(d.total/initData.colleges_total); })
 		.attr("height", yScale.rangeBand())
 		.style("fill", "#ff8c00");
 }
 
 function transitionyScale(transitionData) {
-	// for (var i = 0; i < transitionData.colleges.length; i++)
-	// 	console.log(transitionData.colleges[i].name);
-	console.log(transitionData)
-// ;	if (transitionData)
-	var map;
-	var newYDomain = [];
-	var newXDomain = [];
+  // for (var i = 0; i < transitionData.colleges.length; i++)
+  	// 	console.log(transitionData.colleges[i].name);
+  	console.log(transitionData)
+  // ;	if (transitionData)
+  	var map;
+  	var newYDomain = [];
 
-	// map = transitionData.map(function(d) {
-	// 	newYDomain.push(d["key"]);
-	// });
-	// newXDomain = [0, d3.max(transitionData, function(d) { return parseInt(d.values[0]["total"]); })];
-	if (transitionData.colleges.length == 0)
-		return;
-	map = transitionData.colleges.map(function(d) {
-		newYDomain.push(d["name"]);
-	});
-	newXDomain = [0, d3.max(transitionData.colleges, function(d) { return parseInt(d.total); })];
+  	// map = transitionData.map(function(d) {
+  	// 	newYDomain.push(d["key"]);
+  	// });
+  	// newXDomain = [0, d3.max(transitionData, function(d) { return parseInt(d.values[0]["total"]); })];
+  	map = transitionData.colleges.map(function(d) {
+  		newYDomain.push(d["name"]);
+  	});
 
-	yScale.domain(newYDomain);
-	yScale.rangeRoundBands([newYDomain.length*50, 0], 0.1);
+  	yScale.domain(newYDomain);
+  	yScale.rangeRoundBands([newYDomain.length*50, 0], 0.1);
 
-	yAxis.transition()
-		.duration(500)
-		.ease("linear")
-		.call(yScale.axis);
+  	yAxis.transition()
+  		.duration(500)
+  		.ease("linear")
+  		.call(yScale.axis);
 
-	// console.log(newXDomain);
-	xScale.domain(newXDomain);
-
-	xAxis.transition()
-		.duration(500)
-		.ease("linear")
-		.call(xScale.axis);
-
+  	// console.log(newXDomain);
 }
 
 function transitionBarGraph(data) {
 	transitionyScale(data);
-	console.log(data);
 
 	var dataRects = d3.selectAll(".dataRect").select("rect")
 		.transition()
@@ -91,9 +90,6 @@ function transitionBarGraph(data) {
 
 	for (var i = 0; i < data.colleges.length; i++) {
 		var specificSchoolRect = d3.select("." + data.colleges[i].name);
-
-		// console.log(data.colleges[i]);
-		// specificSchoolRect.data(data.colleges[i]);
 
 		specificSchoolRect.select("rect").transition()
 			.duration(500)
@@ -107,12 +103,37 @@ function transitionBarGraph(data) {
 			.attr("width", function(d) {
 				// if (xScale(data.colleges[i].total) == NaN)
 				// 	return 0;
-				return xScale(data.colleges[i].total);
+				return xScale(data.colleges[i].total/data.colleges_total);
 			});
 	}
 }
 
 d3.json("/datasets/presidential-campaign-donations/result.json", function(data) {
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+  var curr_cand;
+	console.log(data);
+	$('#d3').dropdown({
+    onChange: function (val) {
+
+      console.log(val.split(' ')[0]);
+      if (val.split(' ')[0] == 'martin') {
+        val = "O'Malley";
+      }
+      else {
+        val = capitalizeFirstLetter(val.split(' ')[1]);
+      }
+
+      var newData;
+
+      data.map(function(d) { if (d.name == val) newData = d; });
+      transitionBarGraph(newData);
+    }
+  });
+
 	var nest = d3.nest()
    	 	.key(function(d) { return d.party; })
     	.entries(data);
