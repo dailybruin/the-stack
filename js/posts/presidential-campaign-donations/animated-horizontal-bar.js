f9eae8928d93eed6e4dbf548b37363c0a6eaec4f
Li-Wei Chi <weberchichi@gmail.com>
2016-05-02 17:52:22 -0700

    fixed text label

--- js/posts/presidential-campaign-donations/animated-horizontal-bar.js
+++ js/posts/presidential-campaign-donations/animated-horizontal-bar.js
@@ -1,4 +1,4 @@
-var margin = {top: 30, right: 10, bottom: 50, left: 50},
+var margin = {top: 50, right: 10, bottom: 50, left: 50},
 	width = 630 - margin.left - margin.right,
 	height = 500 - margin.top - margin.bottom;
 
@@ -20,8 +20,8 @@ var xAxis = barSVG.append("g")
 	.attr("class", "x axis")
 	.call(xScale.axis = d3.svg.axis().scale(xScale).orient("top"))
   .append("text")
-  .attr("x", 550)
-  .attr("y", 20)
+  .attr("x", 570)
+  .attr("y", -20)
   .style("text-anchor", "end")
   .text("Percentage from UC schools (%)");
 
