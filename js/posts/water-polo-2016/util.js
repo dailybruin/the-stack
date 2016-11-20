9cd89c03fad51a76d1c3d62069ed1d4977b0e865
Chang Liu <shampliu@gmail.com>
2016-11-20 14:59:21 -0800

    util function to parse boxes

--- js/posts/water-polo-2016/util.js
+++ js/posts/water-polo-2016/util.js
@@ -1,3 +1,10 @@
+function parseBoxes(data, boxes) {
+	data["boxes"] = [];
+	for (i = 1; i <= boxes; i++) {
+		data["boxes"].push(data[String(i)])
+	}
+}
+
 function parseShots(row) {
 	var res = [0,0];
 	row.replace(/[^SG]/g, '').split('').map(function(c) { if (c === 'G') { res[0] += 1 } else { res[1] += 1 } });
