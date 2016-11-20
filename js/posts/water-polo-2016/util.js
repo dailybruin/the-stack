d4712f378403313c94ba2acfc7e83bd106571a59
Chang Liu <shampliu@gmail.com>
2016-11-20 15:29:18 -0800

    added wrapper function for all data in util

--- js/posts/water-polo-2016/util.js
+++ js/posts/water-polo-2016/util.js
@@ -1,13 +1,17 @@
-function parseShots(row) {
+function formatShots(row) {
 	var res = [0,0];
 	row.replace(/[^SG]/g, '').split('').map(function(c) { if (c === 'G') { res[0] += 1 } else { res[1] += 1 } });
 	return res;
 }
 
-function parseBoxes(data, boxes) {
+function formatBoxes(data, boxes) {
 	data["boxes"] = [];
 	for (i = 1; i <= boxes; i++) {
-		data["boxes"].push(parseShots(data[String(i)]))
+		data["boxes"].push(formatShots(data[String(i)]))
 		delete data[String(i)]; 
 	}
+}
+
+function formatData(data, boxes) {
+	data.map(function(d) { parseBoxes(data, boxes); })
 }
