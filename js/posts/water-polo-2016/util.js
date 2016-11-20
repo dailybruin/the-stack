f4a0e0dcabef2da60aea105937ff52da9259a3d3
Chang Liu <shampliu@gmail.com>
2016-11-20 15:13:35 -0800

    modify util function, add numbers to goalkeepers

--- js/posts/water-polo-2016/util.js
+++ js/posts/water-polo-2016/util.js
@@ -1,12 +1,13 @@
-function parseBoxes(data, boxes) {
-	data["boxes"] = [];
-	for (i = 1; i <= boxes; i++) {
-		data["boxes"].push(data[String(i)])
-	}
-}
-
 function parseShots(row) {
 	var res = [0,0];
 	row.replace(/[^SG]/g, '').split('').map(function(c) { if (c === 'G') { res[0] += 1 } else { res[1] += 1 } });
 	return res;
+}
+
+function parseBoxes(data, boxes) {
+	data["boxes"] = [];
+	for (i = 1; i <= boxes; i++) {
+		data["boxes"].push(parseShots(data[String(i)]))
+		delete data[String(i)]; 
+	}
 }
