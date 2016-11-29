5d3b998dfae75154164b813f922ac33b10076a71
Jeffrey Chan <jeffschan97@gmail.com>
2016-11-28 23:49:54 -0800

    made stuff nicer for goalkeeper chart

--- js/posts/water-polo-2016/goalkeeper.js
+++ js/posts/water-polo-2016/goalkeeper.js
@@ -1,31 +1,21 @@
 function initGoalkeeperChart(data) {
-    var svg = d3.select("#goalkeeper-chart");
-    var percentColors = [
-        { pct: 0.0, color: "#E52100" },
-        { pct: 0.1, color: "#E10023" },
-        { pct: 0.2, color: "#DD0064" },
-        { pct: 0.3, color: "#D900A4" },
-        { pct: 0.4, color: "#CA00D5" },
-        { pct: 0.5, color: "#8700D2" },
-        { pct: 0.6, color: "#4700CE" },
-        { pct: 0.7, color: "#0900CA" },
-        { pct: 0.8, color: "#0032C6" },
-        { pct: 0.9, color: "#006BC2" },
-        { pct: 1.0, color: "#00A2BF" }
-    ];
-    
-    var curr_player = 'player0';
-    
     formatData(data, 6);
     
-    console.log(data[0]);
+    // red - blue gradiant
+    var color = d3.scaleLinear().domain([0, 1]).range(["red", "blue"]);
     
-    var tooltip = d3.select("#goalkeeper-chart-wrapper").append("div")
-                      .attr("class", "tooltip")
-                      .style("opacity", 0);
-    
+    // global constants
+    var labelLength = 60;
     var regionHeight = 100;
     var regionWidth = 100;
+    
+    // initialize tooltip
+    var tooltip = d3.select("#goalkeeper-chart-wrapper").append("div")
+                      .attr("class", "tooltip")
+                      .style('display', 'none');
+    
+    // initial svg render
+    var svg = d3.select("#goalkeeper-chart");
     
     var region = svg.selectAll("g")
                     .data(data[0].boxes)
@@ -36,42 +26,41 @@ function initGoalkeeperChart(data) {
         .attr("width", regionWidth)
         .attr("height", regionHeight)
         .style("fill", function(d) {
-                        var percentage = d[1] / (d[0] + d[1]);
-                        return getColorForPercentage(percentage);
+                        var pct = d[1] / (d[0] + d[1]);
+                        return color(pct);
                     })
-        .on("mouseover", function(d) {})
+        .on("mouseover", function(d) {
+            tooltip.style('display', 'inline');
+        })
         .on("mousemove", function(d) {
                             var numBlocked = d[1]; // successfully blocked
                             var numFailed = d[0]; // failed to block
-                            var h = "<div class='left'><p>Successfully Blocked: " + numBlocked + " Failed to Block: " + numFailed + "</p></div>";
+                            var h = "<p>Successfully Blocked: " + numBlocked + "</p><p>Failed to Block: " + numFailed + "</p>";
         
-                            tooltip.style("display","block");
-                            tooltip.transition()
-                            .duration(200)
-                            .style("opacity", .9);
-                            tooltip.html(h)
-                            .style("left", (d3.event.pageX + 12) + "px")
-                            .style("top", (d3.event.pageY -10) + "px")
+                            tooltip
+                                .html(h)
+                                .style("left", (d3.event.pageX + 20) + "px")
+                                .style("top", (d3.event.pageY - 12) + "px");
                         })
         .on("mouseout", function(d) { tooltip.style("display", "none"); });
     
     region.append("text")
         .attr("y", regionHeight / 2)
         .attr("dy", ".35em")
+        .attr('x', (regionWidth - labelLength) / 2 )
+        .attr('textLength', labelLength)
+        .attr('fill', 'white')
         .text(function(d) {
-                var percentage = Math.round(d[1] / (d[0] + d[1]) * 100 *100) / 100;
+                var percentage = Math.round(d[1] / (d[0] + d[1]) * 100 *10) / 10;
                 return percentage + "%";
-            });
+            })
+        .style('font-size', '20px')
+        .style('pointer-events', 'none');
     
-    $('#mySwitch').click(function () {
-        if (curr_player == 'player0') {
-            updateGoalkeeperChart(data[1]);
-            curr_player = 'player1';
-        }
-        else {
-            curr_player = 'player0';
-            updateGoalkeeperChart(data[0]);
-        }
+    // Dropdown function - invokes update
+    $('#goalkeeperDropdown').change(function(option) {
+        var dataNumber = Number($('#goalkeeperDropdown').val());
+        updateGoalkeeperChart(data[dataNumber]);
     });
     
     // Update Function
@@ -91,53 +80,49 @@ function initGoalkeeperChart(data) {
             .attr("width", regionWidth)
             .attr("height", regionHeight)
             .style("fill", function(d) {
-                            var percentage = d[1] / (d[0] + d[1]);
-                            return getColorForPercentage(percentage);
+                            var pct = d[1] / (d[0] + d[1]);
+                            return color(pct);
                         })
-            .on("mouseover", function(d) {})
-            .on("mousemove", function(d) {
-                                var numBlocked = d[1]; // successfully blocked
-                                var numFailed = d[0]; // failed to block
-                                var h = "<div class='left'><p>Successfully Blocked: " + numBlocked + " Failed to Block: " + numFailed + "</p></div>";
-
-                                tooltip.style("display","block");
-                                tooltip.transition()
-                                .duration(200)
-                                .style("opacity", .9);
-                                tooltip.html(h)
-                                .style("left", (d3.event.pageX + 12) + "px")
-                                .style("top", (d3.event.pageY -10) + "px")
-                            })
-            .on("mouseout", function(d) { tooltip.style("display", "none"); });
+            .on("mouseover", function(d) {
+            tooltip.style('display', 'inline');
+        })
+        .on("mousemove", function(d) {
+                            var numBlocked = d[1]; // successfully blocked
+                            var numFailed = d[0]; // failed to block
+                            var h = "<p>Successfully Blocked: " + numBlocked + "</p><p>Failed to Block: " + numFailed + "</p>";
+        
+                            tooltip
+                                .html(h)
+                                .style("left", (d3.event.pageX + 20) + "px")
+                                .style("top", (d3.event.pageY - 12) + "px")
+                        })
+        .on("mouseout", function(d) { tooltip.style("display", "none"); });
 
         region.append("text")
             .attr("y", regionHeight / 2)
             .attr("dy", ".35em")
+            .attr('x', (regionWidth - labelLength) / 2 )
+            .attr('textLength', labelLength)
+            .attr('fill', 'white')
             .text(function(d) {
-                    var percentage = Math.round(d[1] / (d[0] + d[1]) * 100 *100) / 100;
+                    var percentage = Math.round(d[1] / (d[0] + d[1]) * 100 *10) / 10;
                     return percentage + "%";
-                });
+                })
+            .style('font-size', '20px')
+            .style('pointer-events', 'none');
     }
     
     // Calculation functions
     function xTransform(index) {
         // calculates x coordinate
-        return (index % 3) * (regionWidth + 10);
+        return (index % 3) * (regionWidth);
     }
 
     function yTransform(index) {
         // calculates y coordinate
         if (index > 2)
-            return regionHeight + 10;
+            return regionHeight;
         else
             return 0;
     }
-
-    function getColorForPercentage(pct) {
-        for (var i = 0; i < percentColors.length; i++) {
-            if (pct <= percentColors[i].pct) {
-                return percentColors[i].color;
-            }
-        }
-    }  
 }
