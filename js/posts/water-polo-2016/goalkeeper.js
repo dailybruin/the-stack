8956ff158c992857a67707e05bf0ce24ca187e1b
Jeffrey Chan <jeffschan97@gmail.com>
2016-12-01 23:42:09 -0800

    Added all goalies option, adjusted font, adjusted tooltip

--- js/posts/water-polo-2016/goalkeeper.js
+++ js/posts/water-polo-2016/goalkeeper.js
@@ -1,11 +1,26 @@
 function initGoalkeeperChart(data) {
+    // Initialize Data
     formatData(data, 6);
+    
+    var allGoalkeepers = {
+        boxes: []
+    };
+    for (var k = 0; k < 6; k += 1) {
+        allGoalkeepers.boxes.push([0,0]);
+    }
+    for (var k = 0; k < data.length; k += 1) {
+        for (var j = 0; j < data[k].boxes.length; j += 1) {
+            allGoalkeepers.boxes[j][0] += data[k].boxes[j][0];
+            allGoalkeepers.boxes[j][1] += data[k].boxes[j][1];
+        }
+    }
+    data.push(allGoalkeepers);
     
     // red - blue gradiant
     var color = d3.scaleLinear().domain([0, 1]).range(["#962D3E", "#348899"]);
     
     // global constants
-    var labelLength = 60;
+    var labelLength = 37;
     var regionHeight = 100;
     var regionWidth = 100;
     
@@ -16,79 +31,21 @@ function initGoalkeeperChart(data) {
     
     // initial svg render
     var svg = d3.select("#goalkeeper-chart");
-    
-    var region = svg.selectAll("g")
-                    .data(data[0].boxes)
-                    .enter().append("g")
-                    .attr("transform", function(d, i) {return "translate(" + xTransform(i) + "," + yTransform(i) + ")"});
-    
-    region.append("rect")
-        .attr("width", regionWidth)
-        .attr("height", regionHeight)
-        .style("fill", function(d) {
-                        var pct = d[1] / (d[0] + d[1]);
-                        return color(pct);
-                    })
-        .on("mouseover", function(d) {
-            tooltip.style('display', 'inline');
-        })
-        .on("mousemove", function(d) {
-                            var numBlocked = d[1]; // successfully blocked
-                            var numFailed = d[0]; // failed to block
-                            var h = "<p>Successfully Blocked: " + numBlocked + "</p><p>Failed to Block: " + numFailed + "</p>";
-        
-                            tooltip
-                                .html(h)
-                                .style("left", (d3.event.pageX + 20) + "px")
-                                .style("top", (d3.event.pageY - 12) + "px");
-                        })
-        .on("mouseout", function(d) { tooltip.style("display", "none"); });
-    
-    region.append("text")
-        .attr("y", regionHeight / 2)
-        .attr("dy", ".35em")
-        .attr('x', (regionWidth - labelLength) / 2 )
-        .attr('textLength', labelLength)
-        .attr('fill', 'white')
-        .text(function(d) {
-                var percentage = Math.round(d[1] / (d[0] + d[1]) * 100 *10) / 10;
-                return percentage + "%";
-            })
-        .style('font-size', '20px')
-        .style('pointer-events', 'none');
-    
-    var totalShotsBlocked = 0;
-    var totalShotsFailed = 0;
-    for (var k = 0; k < data.length; k += 1) {
-        for (var j = 0; j < data[k].boxes.length; j += 1) {
-            totalShotsBlocked += data[k].boxes[j][1];
-            totalShotsFailed += data[k].boxes[j][0];
-        }
-    }
-    var successRate = Math.round(totalShotsBlocked / (totalShotsBlocked + totalShotsFailed) * 100 * 1) / 1;
-    var statHTML = '<p style="text-align: center;">' + totalShotsBlocked + ' total shots blocked by UCLA goalkeepers, resulting in a ' + successRate + '% success rate.</p>';
-    var statistic = d3.select('#goalkeeper-chart-wrapper').append('div')
-                            .attr('id', 'statistic')
-                            .html(statHTML);
+    updateGoalkeeperChart(data[2]);
     
     // Dropdown function - invokes update
     $('#goalkeeperDropdown').change(function(option) {
         var dataNumber = Number($('#goalkeeperDropdown').val());
+        svg.selectAll('g').remove();
         updateGoalkeeperChart(data[dataNumber]);
     });
     
-    // Update Function
     function updateGoalkeeperChart(player) {
-        // remove rectangles in 
-        svg.selectAll('g').remove();
-
-        console.log(player);
         
         var region = svg.selectAll("g")
                     .data(player.boxes)
                     .enter().append("g")
                     .attr("transform", function(d, i) {return "translate(" + xTransform(i) + "," + yTransform(i) + ")"});
-        console.log(player.boxes);
     
         region.append("rect")
             .attr("width", regionWidth)
@@ -100,10 +57,10 @@ function initGoalkeeperChart(data) {
             .on("mouseover", function(d) {
             tooltip.style('display', 'inline');
         })
-        .on("mousemove", function(d) {
-                            var numBlocked = d[1]; // successfully blocked
-                            var numFailed = d[0]; // failed to block
-                            var h = "<p>Successfully Blocked: " + numBlocked + "</p><p>Failed to Block: " + numFailed + "</p>";
+        .on("mousemove", function(d, i) {
+                            var numBlocked = d[1]; // Shots blocked
+                            var numFailed = d[0]; // Shots Missed
+                            var h = "<p><b>" + goalSection(i) + "</b></p><p>Shots Blocked: <b>" + numBlocked + "</b></p><p>Shots Missed: <b>" + numFailed + "</b></p>";
         
                             tooltip
                                 .html(h)
@@ -116,27 +73,36 @@ function initGoalkeeperChart(data) {
             .attr("y", regionHeight / 2)
             .attr("dy", ".35em")
             .attr('x', (regionWidth - labelLength) / 2 )
-            .attr('textLength', labelLength)
             .attr('fill', 'white')
             .text(function(d) {
-                    var percentage = Math.round(d[1] / (d[0] + d[1]) * 100 *10) / 10;
+                    var percentage = Math.round(d[1] / (d[0] + d[1]) * 100 );
                     return percentage + "%";
                 })
             .style('font-size', '20px')
             .style('pointer-events', 'none');
     }
     
-    // Calculation functions
+    // Helper Functions
     function xTransform(index) {
-        // calculates x coordinate
         return (index % 3) * (regionWidth);
     }
 
     function yTransform(index) {
-        // calculates y coordinate
         if (index > 2)
             return regionHeight;
         else
             return 0;
+    }
+    
+    function goalSection(index) {
+        switch (index) {
+            case 0: return "Top Left";
+            case 1: return "Top Center";
+            case 2: return "Top Right";
+            case 3: return "Bottom Left";
+            case 4: return "Bottom Center";
+            case 5: return "Bottom Right";
+            default: return "Error in goalSection function";
+        }
     }
 }
