1b14e058fb5f98d68c579824da09744c5d21d087
Mahir Eusufzai <Mahir@Mahirs-MacBook-Pro.local>
2017-01-23 19:35:15 -0800

    Line chart and tables are showing.  Some text has been added.

--- _posts/2017-1-10-nba-players.md
+++ _posts/2017-1-10-nba-players.md
@@ -19,26 +19,42 @@ scripts:
     - /js/posts/nba-players/graphs.js
 
 stylesheets:
+    - /css/posts/nba-players/app.css
+    - //cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css
 
 ---
 
-**There’s a reason that Den Passes burn a $169 hole in your pocket.**
+There’s a reason that Den Passes burn a $169 hole in your pocket, or that students have slept in tents on Bruin Walk to secure the best seats in Pauley Pavillion.  
 
-There’s a reason that students have slept in tents on Bruin Walk to secure the best seats in Pauley Pavillion.
+With 11 National Champions —  more than any other school — UCLA touts a world-class basketball program that has produced **88 NBA Players.**  
 
-With 11 National Champions —  more than any other school — UCLA touts a world-class basketball program as elite as its dining halls.  
-
-**88 of UCLA's players have entered NBA.**  This post investigates the quantity and quality of these players over the tenure of UCLA’s basketball program.
+This post investigates the quantity and quality of these players over the tenure of UCLA’s basketball program.
 
 
+### Trends over Time
+
+Below you can toggle between various metrics to explore trends.
 
  <input type="button" id="vorp" value="NBA Performance" />
  <input type="button" id="pick" value="Draft Position" />
  <input type="button" id="yrsCollege" value="Years Spent in College" />
  <input type="button" id="numPlayers" value="Number of Players Drafted" />
 
+> The first is **VORP**, which measures how much a player contributes to the team's win over the average replacement player. While this is far from a perfect metric, star players usually have higher VORP's than role players, who usually have higher VORP's than benchwarmers.  Specifically, the graph below compares player's VORP during their best year in order to normalize the difference between active and retired players.
+
  <div id="chart"></div>
 
+<p> 
+<ul>
+<li/> Performance is measured with VORP (how much a player contributes to the team’s wins vs the average replacement player)
+<li/> The graph compares VORP in each player’s best year (not cumulative VORP) in order to normalize differences between active players and retired players
+<li/> If multiple players were drafted in a particular year, the graph adds their respective VORPS
+<li/> A time frame of 1969 to 2009 is used, since VORP data was not thoroughly available before 1969, and players drafted after 2009 have likely not hit their peak performance. 
+ </ul>
+</p>
+
+
+### Notable Players 
 
 
  <h3> Top 5 VORP </h3>
@@ -82,6 +98,7 @@ With 11 National Champions —  more than any other school — UCLA touts a worl
 </table>
 
 
+<br/>
 
 <h3> Top 5 All Star Appearances </h3>
 <table>
@@ -121,6 +138,8 @@ With 11 National Champions —  more than any other school — UCLA touts a worl
     </tbody>
 </table>
 
+<br/>
+
 <h3> Top 5 Recent Draft </h3>
 <table>
     <thead>
@@ -132,7 +151,7 @@ With 11 National Champions —  more than any other school — UCLA touts a worl
     </thead>
     <tbody>
         <tr>
-            <td>Normal Powell</td>
+            <td>Normal Powell </td>
             <td>2015</td>
             <td>Raptors</td>
         </tr>
--- js/posts/nba-players/graphs.js
+++ js/posts/nba-players/graphs.js
@@ -1,37 +1,79 @@
-/*
-var chart = c3.generate({
-    data: {
-        x: 'x',
-        xFormat: '%Y', // 'xFormat' can be used as custom format of 'x'
-		url: 'out.csv', 
-        columns: [
-            ['x', '2010', '2011', '2012', '2013', '2014', '2015'],
-//            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 130, 340, 200, 500, 250, 350]
-        ]
-    },
-    axis: {
-        x: {
-            type: 'timeseries',
-            tick: {
-                format: '%Y'
-            }
-        }
+
+
+
+var JSON_data;
+var names_array = [], VORP_array = [];
+ //var JSON_data = JSON.parse("/datasets/nba-players/tooltip_VORP.json");
+$.getJSON('/datasets/nba-players/tooltip_VORP.json', function(data) {         
+    JSON_data = data;
+    for (var key in JSON_data['NAME']) {
+        names_array.push(JSON_data['NAME'][key]);
     }
+
 });
 
-*/
+function tooltip_contents(d, defaultTitleFormat, defaultValueFormat, color) {
+    var $$ = this, config = $$.config, CLASS = $$.CLASS,
+        titleFormat = config.tooltip_format_title || defaultTitleFormat,
+        nameFormat = config.tooltip_format_name || function (name) { return name; },
+        valueFormat = config.tooltip_format_value || defaultValueFormat,
+        text, i, title, value, name, bgcolor;
+
+    // You can access all of data like this:
+    //console.log($$.data.targets);
+    //var formatTime = c3.timeFormat("%Y");
+    //var year = formatTime(d[i].x);
+    for (i = 0; i < d.length; i++) {
+        if (! (d[i] && (d[i].value || d[i].value === 0))) { continue; }
+
+        if (! text) {
+            title = d[i].x.getFullYear(); // SHOW X-VALUE
+            text = "<table class='" + CLASS.tooltip + "'>" + (title || title === 0 ? "<tr><th colspan='2'>" + title + "</th></tr>" : "");
+        }
+
+        name = nameFormat(d[i].name); //d[i].name = "MAX_VORP"
+        value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index); //value = VORP value
+        bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
+        
+        var bestPlayerName = JSON_data["NAME"][title];
+        var bestPlayerVORP = JSON_data["MAX_VORP"][title];
+
+
+        text += "<tr class='" + CLASS.tooltipName + "-" + d[i].id + "'>";
+        text += "<td class='name'><span style='background-color:" + bgcolor + "'></span>" + name + "</td>";
+        text += "<td class='value'>" + value + "</td>";
+        text += "</tr>";      
+        text += "<tr>";
+        text += "<td>" + "Top Player: " + "</td>";
+        text += "<td class='value'>"+ bestPlayerName + " (" + bestPlayerVORP + ") "+"</td> </tr>";      
+    }
+
+    return text + "</table>";    
+}
+
+
 var titles = ['http://c3js.org/samples/tooltip_format.html', 'b'];
 var titles2 = ['vva', 'g'];
 
-
 var chart = c3.generate({
+    tooltip: {
+        contents: tooltip_contents,
+        /*
+        format: {
+            value: function (value, ratio, id, index) {
+               // alert(chart.tooltip.title);
+                return value +  " (Top Player: \n" + names_array[index] + ")";
+                //return JSON_data["MAX_VORP"]["1999"];
+            }
+        }
+        */
+    },
+
     data: {
     	x: 'YEAR',
     	xFormat: '%Y',
         //url: 'out.csv',
-        url: 'maxVORP.csv',
+        url: '/datasets/nba-players/maxVORP.csv',
 
         //type: 'timeseries'
     },
@@ -48,18 +90,7 @@ var chart = c3.generate({
              padding: {top:1, bottom:0}
         }
     },
-    /*
-    tooltip: {
-        format: {
-            //title: function (d) { return 'Data ' + d; },
-            value: function (value, ratio, id, index) {
-                
-                return titles[index % 2];
-            }
-//            value: d3.format(',') // apply this format to both y and y2
-        }
-    }
-    */
+
 });
 
 
@@ -67,7 +98,7 @@ $('#vorp').on('click', function () {
 
     chart.load({
         //url: 'pick.csv',
-        url: 'maxVORP.csv',
+        url: '/datasets/nba-players/maxVORP.csv',
         unload: chart.url, 
     });
 });
@@ -76,7 +107,7 @@ $('#pick').on('click', function () {
 
     chart.load({
         //url: 'pick.csv',
-        url: 'pick.csv',
+        url: '/datasets/nba-players/pick.csv',
         unload: chart.url, 
     });
 });
@@ -85,7 +116,7 @@ $('#yrsCollege').on('click', function () {
 
     chart.load({
         //url: 'pick.csv',
-        url: 'yrs_mean.csv',
+        url: '/datasets/nba-players/yrs_mean.csv',
         unload: chart.url, 
     });
 });
@@ -94,7 +125,7 @@ $('#numPlayers').on('click', function () {
 
     chart.load({
         //url: 'pick.csv',
-        url: 'numPlayers.csv',
+        url: '/datasets/nba-players/numPlayers_2.csv',
         unload: chart.url, 
     });
 });
