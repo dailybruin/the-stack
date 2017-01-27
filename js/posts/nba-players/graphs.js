3d571737159c5a5b25ed87d8413da25bf8602809
Chang Liu <shampliu@gmail.com>
2017-01-27 12:33:31 -0800

    full width tables

--- js/posts/nba-players/graphs.js
+++ js/posts/nba-players/graphs.js
@@ -1,6 +1,6 @@
 var JSON_data;
 
-$.getJSON('/datasets/nba-players/tooltip_VORP.json', function(data) {         
+$.getJSON('/datasets/nba-players/tooltip_VORP.json', function(data) {
     JSON_data = data;
 });
 
@@ -26,7 +26,7 @@ function tooltip_contents(d, defaultTitleFormat, defaultValueFormat, color) {
         name = nameFormat(d[i].name); //d[i].name = "MAX_VORP"
         value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index); //value = VORP value
         bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
-        
+
         var bestPlayerName = JSON_data["NAME"][year];
         var bestPlayerVORP = JSON_data["MAX_VORP"][year] == -99 ? "N/A" : JSON_data["MAX_VORP"][year] ;
         var bestPlayerText = (year in JSON_data["NAME"]) ? bestPlayerName + " (" + bestPlayerVORP + " VORP) " : "N/A";
@@ -34,13 +34,13 @@ function tooltip_contents(d, defaultTitleFormat, defaultValueFormat, color) {
         text += "<tr class='" + CLASS.tooltipName + "-" + d[i].id + "'>";
         text += "<td class='name'><span style='background-color:" + bgcolor + "'></span>" + name + "</td>";
         text += "<td class='value'>" + value + "</td>";
-        text += "</tr>";      
+        text += "</tr>";
         text += "<tr>";
         text += "<td>" + "Top Player: " + "</td>";
-        text += "<td class='value'>"+ bestPlayerText +"</td> </tr>";    
+        text += "<td class='value'>"+ bestPlayerText +"</td> </tr>";
     }
 
-    return text + "</table>";    
+    return text + "</table>";
 }
 
 
@@ -108,13 +108,13 @@ $('.toggleButton.2').click( function () {
 });
 
 $('#top-VORP').on('click', function () {
-    document.getElementById("VORP-table").style = "display:block";
+    document.getElementById("VORP-table").style = "display:table";
 });
 $('#top-all-star').on('click', function () {
-    document.getElementById("all-star-table").style = "display:block";
+    document.getElementById("all-star-table").style = "display:table";
 });
 $('#top-recent').on('click', function () {
-    document.getElementById("recent-table").style = "display:block";
+    document.getElementById("recent-table").style = "display:table";
 });
 
 $('#vorp').on('click', function () {
@@ -122,7 +122,7 @@ $('#vorp').on('click', function () {
     chart.load({
         //url: 'pick.csv',
         url: '/datasets/nba-players/maxVORP.csv',
-        unload: chart.url, 
+        unload: chart.url,
     });
 
     document.getElementById("VORP_paragraph").style = "display:block";
@@ -134,7 +134,7 @@ $('#pick').on('click', function () {
     chart.load({
         //url: 'pick.csv',
         url: '/datasets/nba-players/pick.csv',
-        unload: chart.url, 
+        unload: chart.url,
     });
 
 
@@ -148,7 +148,7 @@ $('#yrsCollege').on('click', function () {
     chart.load({
         //url: 'pick.csv',
         url: '/datasets/nba-players/yrs_mean.csv',
-        unload: chart.url, 
+        unload: chart.url,
     });
 
     document.getElementById("Yrs_paragraph").style = "display:block";
@@ -160,12 +160,9 @@ $('#numPlayers').on('click', function () {
     chart.load({
         //url: 'pick.csv',
         url: '/datasets/nba-players/numPlayers_2.csv',
-        unload: chart.url, 
+        unload: chart.url,
     });
 
     document.getElementById("Num_drafted_paragraph").style = "display:block";
     document.getElementById("num-drafted-def").style = "display:block";
 });
-
-
-
