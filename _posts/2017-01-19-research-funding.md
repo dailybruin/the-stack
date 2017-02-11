fb48bb9ec136d1ad6148577e4c797017e7bb9f28
Jeffrey Chan <jeffschan97@gmail.com>
2017-02-10 16:54:38 -0800

    weird y axis update

--- _posts/2017-01-19-research-funding.md
+++ _posts/2017-01-19-research-funding.md
@@ -6,9 +6,14 @@ stylesheets:
   - /css/posts/water-polo-2016/util.css
 scripts:
   - //d3js.org/d3.v4.min.js
+  - //d3js.org/d3-transition.v1.min.js
   - //code.jquery.com/jquery-1.11.2.js
   - /js/posts/research-funding/linechart.js
   - /js/posts/research-funding/index.js
 ---
 <div id='line-chart-wrapper'>
+    <select style="margin: 0 auto; display: flex; width: 200px; height: 3em;" class="ui selection dropdown" id='lineChartDropdown'>
+        <option value='0'>TOTAL</option>
+        <option value='1'>BIOENGINEERING</option>
+    </select>
 </div>
--- js/posts/research-funding/index.js
+++ js/posts/research-funding/index.js
@@ -1,3 +1,29 @@
+// Set the dimensions of the canvas / graph
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+width = 650 - margin.left - margin.right,
+height = 350 - margin.top - margin.bottom;
+
+var svg = d3.select("#line-chart-wrapper")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.scaleLinear()
+          .rangeRound([0, width - 50]);
+
+var y = d3.scaleLinear()
+          .rangeRound([height - 10, 0]);
+
+var line = d3.line()
+    .x(function(d) {return x(d.year);})
+    .y(function(d) {return y(d.total);});
+
 d3.json("/datasets/research-funding/dummy.json", function(err, data) {
     initLineChart(data);
 })
+
+// Dropdown function - invokes update
+$('#lineChartDropdown').change(function(option) {
+    updateData();
+});
