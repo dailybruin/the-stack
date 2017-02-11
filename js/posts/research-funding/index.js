d6bed2e67ce1128940bcf364750c2216e2bd7b36
Jeffrey Chan <jeffschan97@gmail.com>
2017-02-10 21:05:58 -0800

    cleaned up code and got yaxis updating

--- js/posts/research-funding/index.js
+++ js/posts/research-funding/index.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  d3.json("/datasets/research-funding/dummy.json", function(err, data1) {
-      initBarChart(data1);
-
-      d3.json("/datasets/research-funding/sponsors.json", function(err, data2) {
-        initDonutChart(data2);
-
-      })
-  })
-})
+    d3.json("/datasets/research-funding/dummy.json", function(err, data1) {
+        d3.json("/datasets/research-funding/sponsors.json", function(err, data2) {
+            initBarChart(data1);
+            initDonutChart(data2);
+        });
+    });
+});
