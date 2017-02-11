0ccbd03f13055eea4c212412688b444be8c9f2c0
Chang Liu <shampliu@gmail.com>
2017-02-10 18:36:43 -0800

    convert line to bar chart

--- js/posts/research-funding/index.js
+++ js/posts/research-funding/index.js
@@ -1,13 +1,10 @@
-d3.json("/datasets/research-funding/dummy.json", function(err, data1) {
-    initLineChart(data1);
+document.addEventListener('DOMContentLoaded', function() {
+  d3.json("/datasets/research-funding/dummy.json", function(err, data1) {
+      initBarChart(data1);
 
-    d3.json("/datasets/research-funding/sponsors.json", function(err, data2) {
-      initDonutChart(data2);
+      d3.json("/datasets/research-funding/sponsors.json", function(err, data2) {
+        initDonutChart(data2);
 
-    })
-})
-
-// Dropdown function - invokes update
-document.getElementById("lineChartDropdown").addEventListener("change", function() {
-  updateData();
+      })
+  })
 })
