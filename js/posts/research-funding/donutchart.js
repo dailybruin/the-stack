20e861387f1180beb3d0435fc16fed077c867963
Mahir Eusufzai <Mahir@Mahirs-MacBook-Pro.local>
2017-02-15 15:14:43 -0800

    Added basic donut chart

--- js/posts/research-funding/donutchart.js
+++ js/posts/research-funding/donutchart.js
@@ -1,2 +1,32 @@
 function initDonutChart(data) {
+	var width = 360;
+	var height = 360;
+	var radius = Math.min(width, height) / 2;
+
+	var color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+	var svg = d3.select('#donut-chart-wrapper')
+	.append('svg')
+	.attr('width', width)
+	.attr('height', height)
+	.append('g')
+	.attr('transform', 'translate(' + (width / 2) +
+		',' + (height / 2) + ')');
+
+	var arc = d3.arc()
+	.innerRadius(100)
+	.outerRadius(radius);
+
+	var pie = d3.pie()
+	.value(function(d) { console.log(d); return d.total; })
+	.sort(null);
+
+	var path = svg.selectAll('path')
+	.data(pie(data[0].sponsors))
+	.enter()
+	.append('path')
+	.attr('d', arc)
+	.attr('fill', function(d, i) {
+		return color(i);
+	});
 }
