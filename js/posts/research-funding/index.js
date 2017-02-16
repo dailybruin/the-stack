8e6e3a0a90eceb4f7bb961c1dadb72b7d88cb10e
Jeffrey Chan <jeffschan97@gmail.com>
2017-02-16 10:08:15 -0800

    axes labels, rect hover opacity effect, tooltip text

--- js/posts/research-funding/index.js
+++ js/posts/research-funding/index.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-  d3.json("/datasets/research-funding/dummy.json", function(err, data1) {
-    d3.json("/datasets/research-funding/sponsors.json", function(err, data2) {
-      initBarChartDropdown(data1);
-      initDonutChartDropdown(data2);
+    d3.json("/datasets/research-funding/dummy.json", function(err, data1) {
+        d3.json("/datasets/research-funding/sponsors.json", function(err, data2) {
+            initBarChartDropdown(data1);
+            initDonutChartDropdown(data2);
 
-      initBarChart(data1);
-      initDonutChart(data2);
+            initBarChart(data1);
+            initDonutChart(data2);
+        });
     });
-  });
 });
