60fe97c5664535598c7a8e1d6038d6b78bb6bb54
Chang Liu <shampliu@gmail.com>
2017-04-12 15:06:38 -0700

    write csv

--- js/posts/sexual-harassment/app.js
+++ js/posts/sexual-harassment/app.js
@@ -2,38 +2,21 @@
 d3.csv("/datasets/sexual-harrassment/positions.csv", function(error, data) {
   if (error) throw error;
 
-  var nodesByName = {};
+  const BASE = 'position';
+  const COLLEGES = ["UCB","UCD","UCI","UCLA","UCM","UCR","UCSB","UCSC","UCSD","UCSF"];
 
-  data.forEach(function(d) {
-  	var parent = d.Position = nodeByNameTitle("Position",d.Position),
-  		child = d.UCB = nodeByName("UCB",d.UCB),
-  		child2 = d.UCD = nodeByName("UCD",d.UCD),
-  		child3 = d.UCI = nodeByName("UCI",d.UCI),
-  		child4 = d.UCLA = nodeByName("UCLA",d.UCLA),
-  		child5 = d.UCM = nodeByName("UCM",d.UCM),
-  		child6 = d.UCR = nodeByName("UCR",d.UCR),
-  		child7 = d.UCSB = nodeByName("UCSB",d.UCSB),
-  		child8 = d.UCSC = nodeByName("UCSC",d.UCSC),
-  		child9 = d.UCSD = nodeByName("UCSD",d.UCSD),
-  		child10 = d.UCSF = nodeByName("UCSF",d.UCSF);
-  	parent.children = [child,child2,child3,child4,child5,child6,child7,child8,child9,child10];
+  let ret = "data:text/csv;charset=utf-8,";
+  ret += '\n' + 'id, value' + '\n' + BASE + ',\n';
+
+  data.forEach(d => {
+    COLLEGES.forEach(c => {
+      let total = d[c];
+      let type = d.Position;
+
+      let row = BASE + '.' + type + '.' + c.toLowerCase() + ',' + total + '\n';
+      ret += row;
+    })
   });
-
-  var root = data[0].Position;
-  var nodes = d3.values(nodesByName);
-
-  function nodeByName(name,value) {
-    return (nodesByName[value] = {"college": name,"total": value});
-  }
-
-  function nodeByNameTitle(name,value) {
-  	return (nodesByName[value] = {[name]: value});
-  }
-
-  console.log(data);
-
-  // console.log(nodes);
-  // console.log(root);
-
-  // initBubbleChart(data)
+  console.log(ret);
+  console.log(encodeURI(ret));
 });
