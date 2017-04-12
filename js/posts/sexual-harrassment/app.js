093474b9856c6c87617b4c4ddf072067d6cde3a1
bubblynatalie <yummyraspberries@gmail.com>
2017-04-12 13:58:40 -0700

    Added total per position

--- js/posts/sexual-harrassment/app.js
+++ js/posts/sexual-harrassment/app.js
@@ -7,24 +7,26 @@ d3.csv("/datasets/sexual-harrassment/uc-sexual-assault-reports.csv", function(er
 
   data.forEach(function(d) {
   	var parent = d.Position = nodeByNameTitle("Position",d.Position),
-  		child = d.UCB = nodeByName("UCB",d.UCB),
-  		child2 = d.UCD = nodeByName("UCD",d.UCD),
-  		child3 = d.UCI = nodeByName("UCI",d.UCI),
-  		child4 = d.UCLA = nodeByName("UCLA",d.UCLA),
-  		child5 = d.UCM = nodeByName("UCM",d.UCM),
-  		child6 = d.UCR = nodeByName("UCR",d.UCR),
-  		child7 = d.UCSB = nodeByName("UCSB",d.UCSB),
-  		child8 = d.UCSC = nodeByName("UCSC",d.UCSC),
-  		child9 = d.UCSD = nodeByName("UCSD",d.UCSD),
-  		child10 = d.UCSF = nodeByName("UCSF",d.UCSF);
-  	parent.children = [child,child2,child3,child4,child5,child6,child7,child8,child9,child10];
+  		child = nodeByName("UCB",d.UCB),
+  		child2 = nodeByName("UCD",d.UCD),
+  		child3 = nodeByName("UCI",d.UCI),
+  		child4 = nodeByName("UCLA",d.UCLA),
+  		child5 = nodeByName("UCM",d.UCM),
+  		child6 = nodeByName("UCR",d.UCR),
+  		child7 = nodeByName("UCSB",d.UCSB),
+  		child8 = nodeByName("UCSC",d.UCSC),
+  		child9 = nodeByName("UCSD",d.UCSD),
+  		child10 = nodeByName("UCSF",d.UCSF),
+      total = child.total+child2.total+child3.total+child4.total+child5.total+child6.total+child7.total+child8.total+child9.total+child10.total;
+  	parent.breakdown = [child,child2,child3,child4,child5,child6,child7,child8,child9,child10];
+    parent.total = total;
   });
 
   var root = data[0].Position;
   var nodes = d3.values(nodesByName);
 
   function nodeByName(name,value) {
-    return (nodesByName[value] = {"college": name,"total": value});
+    return (nodesByName[value] = {"college": name,"total": parseInt(value,10)});
   }
 
   function nodeByNameTitle(name,value) {
@@ -33,6 +35,7 @@ d3.csv("/datasets/sexual-harrassment/uc-sexual-assault-reports.csv", function(er
 
   console.log(nodes);
   console.log(root);
+  console.log(data);
 
   // initBubbleChart(data)
 });
