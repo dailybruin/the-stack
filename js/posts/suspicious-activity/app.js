359b45a34ac02b01786d46fcd6333175c1b9959f
Benson Han <bensonjaminhan@gmail.com>
2017-04-12 15:07:53 -0700

    map

--- js/posts/suspicious-activity/app.js
+++ js/posts/suspicious-activity/app.js
@@ -1,27 +1,21 @@
-var map = L.map('map').setView([34.0689, -118.442], 12);
+//Map
 
-L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-  subdomains: 'abcd',
-  maxZoom: 20
-}).addTo(map);
-d3.csv('/datasets/suspicious-activity/data.csv', function(err, data) {
-  var geocoder = new google.maps.Geocoder();
+L.mapbox.accessToken = 'pk.eyJ1IjoiYmVuc29uaGFuIiwiYSI6ImNpdW4wc2V3NzAwZzAydG13eTB6bDdrdGMifQ.3qVecRB6mpgc1X-pURkDng';
+var map = L.mapbox.map('map', 'mapbox.light')
+  .setView([34.0689, -118.442], 12);
+
+d3.csv('/datasets/suspicious-activity/location.csv', function(data) {
+  data.forEach(function(d) {
+    d.Lat = +d.Lat;
+    d.Long = +d.Long;
+  });
   for (i = 0; i < data.length; i++) {
-    console.log(data[i].Location);
-    geocoder.geocode({address: data[i].Location}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        console.log(i)
-        console.log(results[0])
-        L.marker([results[0].geometry.location.lat(), results[0].geometry.location.lng()]).addTo(map);
-      }
-      if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
-      }
-      else {
-        alert("The Geocode was not successful for the following reason: " + status);
-      }
-    });
+    L.marker([data[i].Lat, data[i].Long]).addTo(map);
   }
+});
+
+//Graph
+
 var svg = d3.select("#bar-chart"),
   margin = {top: 20, right: 20, bottom: 30, left: 40},
   width = +svg.attr("width") - margin.left - margin.right,
