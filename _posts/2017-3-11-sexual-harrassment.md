b8259578249e4c23cc2e64881d739e96b984933a
bubblynatalie <yummyraspberries@gmail.com>
2017-04-12 13:29:32 -0700

    Json formatting

--- _posts/2017-3-11-sexual-harrassment.md
+++ _posts/2017-3-11-sexual-harrassment.md
@@ -1,12 +1,13 @@
 ---
-title: Sexual Harrassment
+title: Sexual Harassment
 
 scripts:
-    - //ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
-    - //cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js
-    - //cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js
-    - /js/posts/nba-players/graphs.js
+    - //d3js.org/d3.v4.min.js
+    - /js/posts/sexual-harrassment/app.js
+    - /js/posts/sexual-harrassment/bubble-chart.js
 
 stylesheets:
 
 ---
+
+<svg width="640" height="640" id="bubble-chart"></svg>
--- js/posts/sexual-harrassment/app.js
+++ js/posts/sexual-harrassment/app.js
@@ -1,1 +1,38 @@
-/* app.js */
+// http://stackoverflow.com/questions/11088303/how-to-convert-to-d3s-json-format
+d3.csv("/datasets/sexual-harrassment/uc-sexual-assault-reports.csv", function(error, data) {
+  if (error) throw error;
+  console.log(data);
+
+  var nodesByName = {};
+
+  data.forEach(function(d) {
+  	var parent = d.Position = nodeByNameTitle("Position",d.Position),
+  		child = d.UCB = nodeByName("UCB",d.UCB),
+  		child2 = d.UCD = nodeByName("UCD",d.UCD),
+  		child3 = d.UCI = nodeByName("UCI",d.UCI),
+  		child4 = d.UCLA = nodeByName("UCLA",d.UCLA),
+  		child5 = d.UCM = nodeByName("UCM",d.UCM),
+  		child6 = d.UCR = nodeByName("UCR",d.UCR),
+  		child7 = d.UCSB = nodeByName("UCSB",d.UCSB),
+  		child8 = d.UCSC = nodeByName("UCSC",d.UCSC),
+  		child9 = d.UCSD = nodeByName("UCSD",d.UCSD),
+  		child10 = d.UCSF = nodeByName("UCSF",d.UCSF);
+  	parent.children = [child,child2,child3,child4,child5,child6,child7,child8,child9,child10];
+  });
+
+  var root = data[0].Position;
+  var nodes = d3.values(nodesByName);
+
+  function nodeByName(name,value) {
+    return (nodesByName[value] = {"college": name,"total": value});
+  }
+
+  function nodeByNameTitle(name,value) {
+  	return (nodesByName[value] = {[name]: value});
+  }
+
+  console.log(nodes);
+  console.log(root);
+
+  // initBubbleChart(data)
+});
