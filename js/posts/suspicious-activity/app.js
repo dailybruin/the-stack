f08003ea502142a0b2ed6e94352c216e765df1cd
Chang Liu <shampliu@gmail.com>
2017-05-01 20:37:43 -0700

    formatting data works + clean up datasets

--- js/posts/suspicious-activity/app.js
+++ js/posts/suspicious-activity/app.js
@@ -1,11 +1,12 @@
 //Map
-
 L.mapbox.accessToken = 'pk.eyJ1IjoiYmVuc29uaGFuIiwiYSI6ImNpdW4wc2V3NzAwZzAydG13eTB6bDdrdGMifQ.3qVecRB6mpgc1X-pURkDng';
 var map = L.mapbox.map('map', 'mapbox.light')
   .setView([34.0689, -118.442], 12);
 
 $(document).ready(function () {
-  d3.csv('/datasets/suspicious-activity/test.csv', function(data) {
+  d3.csv('/datasets/suspicious-activity/suspicious.csv', function(data) {
+    
+
     data.forEach(d => { d.Lat = +d.Lat; d.Long = +d.Long; });
 
     let visibleMarkers = [];
@@ -21,125 +22,89 @@ $(document).ready(function () {
       });
     });
 
-    $('#myselect').change(function () {
-      console.log($(this).value);
-    });
+    initBarChart(data);
 
     document.getElementById('myselect').addEventListener('change', function(e) {
       let val = e.target.value;
 
       visibleMarkers.forEach(m => {
+
         if (m.data.Sex.toLowerCase() !== val) {
-          $(m.marker._icon).hide();
+          $(m.marker._icon).hide(); // add class "hidden" 
+          // $('.hidden').show();
         }
-      })
-    })
-
-
-
+      });
+    });
   });
-  
-  // $('#myselect').change(function () {
-  //   $('.gender').hide();
-  //   $('#'+$(this).val()).show();
-  //   var gender = $(this);
-  //   d3.csv('/datasets/suspicious-activity/test.csv', function(data) {
-  //     data.forEach(function(d) {
-  //       //convert to numbers
-  //       d.Lat = +d.Lat;
-  //       d.Long = +d.Long;
-  //     });
-  //     for (i = 0; i < data.length; i++) {
-  //       if (data[i].Sex == gender.text()) {
-  //         L.marker([data[i].Lat, data[i].Long]).addTo(map);
-  //       }
-  //     }
-  //   });
-  // })
 });
-
-//Graph
 
 var svg = d3.select("#bar-chart"),
-  margin = {top: 20, right: 20, bottom: 30, left: 40},
-  width = +svg.attr("width") - margin.left - margin.right,
-  height = +svg.attr("height") - margin.top - margin.bottom,
-  g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
 
-var x0 = d3.scaleBand()
-  .rangeRound([0, width])
-  .paddingInner(0.1);
+var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([height, 0]);
 
-var x1 = d3.scaleBand()
-  .padding(0.05);
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var y = d3.scaleLinear()
-  .rangeRound([height, 0]);
+function initBarChart(data) {
 
-var z = d3.scaleOrdinal()
-  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+  let genders = ["male", "female"];
+  let races = ["B", "W", "H", "I", "O"];
+  let ages; // 18 - 81
 
-d3.csv('/datasets/suspicious-activity/dummy.csv', function(d, i, columns) {
-  for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]];
-  return d;
-}, function(error, data) {
-  if (error) throw error;
+  let breakdown = {
+    gender: new Array(2).fill(0),
+    race: new Array(5).fill(0),
+    ages: []
+  }
+  
+  data.forEach(function(d) {
+    
+    let g = genders.indexOf(d.Sex.toLowerCase());
+    breakdown.gender[g] += 1; 
 
-  var keys = data.columns.slice(1);
+    let r = races.indexOf(d.Race);
+    breakdown.race[r] += 1;
 
-  x0.domain(data.map(function(d) { return d.State; }));
-  x1.domain(keys).rangeRound([0, x0.bandwidth()]);
-  y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
+    // do age
+  });
 
-  g.append("g")
-    .selectAll("g")
-    .data(data)
-    .enter().append("g")
-      .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; })
-    .selectAll("rect")
-    .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
-    .enter().append("rect")
-      .attr("x", function(d) { return x1(d.key); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("width", x1.bandwidth())
-      .attr("height", function(d) { return height - y(d.value); })
-      .attr("fill", function(d) { return z(d.key); });
+  console.log(breakdown);
+
+  x.domain(genders);
+  y.domain([0, 250]);
 
   g.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x0));
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
   g.append("g")
-    .attr("class", "axis")
-    .call(d3.axisLeft(y).ticks(null, "s"))
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y).ticks(10))
     .append("text")
-      .attr("x", 2)
-      .attr("y", y(y.ticks().pop()) + 0.5)
-      .attr("dy", "0.32em")
-      .attr("fill", "#000")
-      .attr("font-weight", "bold")
-      .attr("text-anchor", "start")
-      .text("Population");
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Frequency");
 
-  var legend = g.append("g")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 10)
-    .attr("text-anchor", "end")
-    .selectAll("g")
-    .data(keys.slice().reverse())
-    .enter().append("g")
-    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+  g.selectAll(".bar")
+    .data(breakdown.gender)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d, i) {
+        return x(genders[i]);
+      })
+      .attr("y", function(d) {
+        return y(d);
+      })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d); });
 
-  legend.append("rect")
-    .attr("x", width - 19)
-    .attr("width", 19)
-    .attr("height", 19)
-    .attr("fill", z);
+}
 
-  legend.append("text")
-    .attr("x", width - 24)
-    .attr("y", 9.5)
-    .attr("dy", "0.32em")
-    .text(function(d) { return d; });
-});
+
