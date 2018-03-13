5e6f03b0c348b82958ce3169a97f0b74fe4065af
Alex Zhao <alexzhao21@gmail.com>
2018-03-12 19:19:57 -0700

    Add more comments for readability

--- js/posts/student-fees/zhaoalex.js
+++ js/posts/student-fees/zhaoalex.js
@@ -6,7 +6,10 @@ var dataset = [
   { label: "Lemon", count: "15"},
   { label: "Mathematical", count: "31.4"}
 ];
- 
+
+// Dataset constants
+const sum = d3.sum(dataset.map(d => { return d.count; }));
+
 // Donut chart constants
 const donutWidth = 300;
 const donutHeight = 300;
@@ -15,12 +18,12 @@ const donutRadius = Math.min(donutWidth, donutHeight) / 2;
 // SVG constants
 const donutSvgWidth = donutWidth + 300;
 const donutSvgHeight = donutHeight + 200;
- 
+
 // Donut-ness constants
 const donutRadDiff = 50;
 const padAngle = 0.02;
 const cornerRadius = 5;
- 
+
 // Label constants
 const labelRadius = donutRadius + 25;
 //const labelArcDist = 25;
@@ -29,18 +32,18 @@ const labelRadius = donutRadius + 25;
 // Legend constants
 const legendRectSize = 18;
 const legendSpacing = 4;
- 
+
 let midAngle = d => { return d.startAngle + (d.endAngle - d.startAngle) / 2; };
- 
-const sum = d3.sum(dataset.map(d => { return d.count; }));
- 
+
+// Chart color
 var donutColor = d3.scaleOrdinal(d3.schemeCategory10);
- 
- // TODO PERCENTAGE?
+
+ // Tooltip setup TODO update for real data
 var donutTip = d3.tip()
   .attr("class", "d3-tip")
   .html(d => {return d.data.label + "<br>" + d.data.count + " pies";})
- 
+
+// Create SVG
 var donutSvg = d3.select("#donut-chart")
   .append("svg")
     .attr("width", donutSvgWidth)
@@ -48,16 +51,19 @@ var donutSvg = d3.select("#donut-chart")
   .append("g")
     .attr("transform", "translate(" + (donutSvgWidth / 2) + ", " + (donutSvgHeight / 2) + ")")
   .call(donutTip);
- 
-  donutSvg.append("g").attr("class", "slices");
-  donutSvg.append("g").attr("class", "labels");
-  donutSvg.append("g").attr("class", "lines");
- 
+
+// Add g sections for better organization
+donutSvg.append("g").attr("class", "slices");
+donutSvg.append("g").attr("class", "labels");
+donutSvg.append("g").attr("class", "lines");
+
+// Create donut chart arc
 var arc = d3.arc()
   .innerRadius(donutRadius - donutRadDiff)
   .outerRadius(donutRadius)
   .cornerRadius(cornerRadius);
 
+// Create arc for labels
 var labelArc = d3.arc()
   .outerRadius(labelRadius)
   .innerRadius(labelRadius);
@@ -67,12 +73,14 @@ var hoverArc = d3.arc()
   .outerRadius(radius + hoverArcDist)
   .innerRadius(radius + hoverArcDist);
 */
- 
+
+// Pie-ify the data
 var pie = d3.pie()
   .value(d => { return d.count; })
   .sort(null)
   .padAngle(padAngle);
- 
+
+// Create each slice
 var path = donutSvg.select(".slices")
   .selectAll("path")
   .data(pie(dataset))
@@ -82,7 +90,8 @@ var path = donutSvg.select(".slices")
   .attr("fill", d => {
     return donutColor(d.data.label);
   });
- 
+
+// Create all labels
 var labels = donutSvg.select(".labels")
   .selectAll("text")
   .data(pie(dataset))
@@ -101,6 +110,7 @@ var labels = donutSvg.select(".labels")
     return (midAngle(d) < Math.PI) ? "start" : "end";
   });
 
+// Create all polylines
 var polylines = donutSvg.select(".lines")
   .selectAll("polyline")
   .data(pie(dataset))
@@ -111,7 +121,7 @@ var polylines = donutSvg.select(".lines")
     pos[0] = (labelRadius - 5) * (midAngle(d) < Math.PI ? 1 : -1);
     return [arc.centroid(d), labelArc.centroid(d), pos];
   })
- 
+
 // labels inside chart
 /*
   .each((d, i, nodes) => {
@@ -125,14 +135,16 @@ var polylines = donutSvg.select(".lines")
 
 //path.call(tip);
 
+// Setup listeners for tooltip (mouse on, mouse move, and mouse off)
 
+// On mouseover, show the tooltip
 path.on("mouseover", (d, i, nodes) => {
   console.log("mouse on " + d.data.label);
   donutTip.style("left", (d3.event.pageX + 10) + "px")
     .style("top", (d3.event.pageY + 10) + "px")
     .show(d);
     
-  // darken donutColor
+  // darken donutColor on mouseover
   var currentColor = d3.hsl(d3.select(nodes[i]).attr("fill"))
   d3.select(nodes[i])
     .attr("fill", d => {
@@ -141,15 +153,19 @@ path.on("mouseover", (d, i, nodes) => {
     });
 })
 
+// On mousemove, update the tooltip position
 path.on("mousemove", d => {
   console.log("mouse move");
   donutTip.style("left", (d3.event.pageX + 10) + "px")
     .style("top", (d3.event.pageY + 10) + "px");
 })
 
+// On moseout, hide the tooltip
 path.on("mouseout", (d, i, nodes) => {
   console.log("mouse off " + d.data.label);
   donutTip.hide(d);
+
+  // Return slice to original color
   d3.select(nodes[i])
     .attr("fill", (d, i) => {
       return donutColor(d.data.label);
@@ -181,5 +197,3 @@ legend.append("text")
   .attr("y", legendRectSize - legendSpacing)
   .text(d => { return d; });
   */
-    
-
