521256af6ffa7b4b02475af85124afdc04fcb278
Nathan Smith <nathan@nathansmith.io>
2018-04-22 16:43:13 -0700

    Add some fee stuff

---
+++ js/posts/student-fees/data.js
@@ -0,0 +1,59 @@
+const bruinBillFees = [
+  {
+    name: "Ackerman/Kerckhoff Seismic Fee",
+    type: "construction",
+    date: "",
+    price: "",
+    adjustsForInflation: false
+  },
+  {
+    name: "Arts Restoring Community Fee",
+    type: "student government",
+    date: "Fall 2014",
+    price: "",
+    adjustsForInflation: true
+  },
+  {
+    name: "Bruin Bash Fee",
+    type: "student government",
+    date: "Fall 2013",
+    price: "",
+    adjustsForInflation: true
+
+  },
+  {
+    name: "Green Initiative Fee",
+    type: "student government",
+    date: "Fall 2013",
+    price: "",
+    adjustsForInflation: true
+
+  },
+  {
+    name: "PLEDGE Fee",
+    type: "student government",
+    date: "Fall 2009",
+    price: "",
+    adjustsForInflation: true
+
+  },
+  {
+    name: "SPARC Fee",
+    type: "construction",
+    date: "Fall 2009",
+    price: "",
+    adjustsForInflation: true
+  },
+  {
+    name: "Undergraduate Student Association Fee",
+    type: "student government",
+    date: "",
+    price: ""
+  },
+  {
+    name: "Wooden Center Fee",
+    type: "student government",
+    date: "",
+    price: ""
+  },
+]
