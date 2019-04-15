4b585225701377c86c480731f807a32b0deea940
Andrew <andrewzkan@gmail.com>
2019-04-15 14:44:48 -0700

    added text to the post, will add graphs and interactive later

--- _posts/2019-03-05-waste-audits.md
+++ _posts/2019-03-05-waste-audits.md
@@ -1,24 +1,36 @@
 ---
-title: Waste Audit Collection data
-teaser: something something
+title: UCLA Waste Bin Breakdown
+teaser: Learn how often students recycle and compost, and test your trash-sorting skills
 authors:
   - andrew_kan
 key_takeaways:
-  - tbd
+  - In non-dining buildings, around 30% of trash is placed in the landfill bin, 30% in the recycling bin, and 40% in the compost bin.
+  - In dining halls, roughly 80% of food waste is edible.
 featured_image:
   url: 2018db-recruitment/featured_image.png
 og_image: 2018db-recruitment/featured_image.png
 stylesheets:
-  - /css/posts/waste-auditsapp.css
+  - /css/posts/waste-audits/app.css
 scripts:
-  - //cdn.plot.ly/plotly-latest.min.js
   - //https://d3js.org/d3.v5.min.js
-  - /js/posts/audits.js
+  - /js/posts/waste-audits/audits.js
 ---
 
-<div id="title">
-  <p>waffle charts</p>
+<p>
+  Walking around campus, one can easily spot the trio of landfill, recycling, and compost bins. Recycling and composting is more eco-friendly than throwing everything into the landfill, but how often do students actually do so? The Daily Bruin investigates how students sort their trash into these bins around campus.
+</p>
+
+<p>
+  Ever wondered what bin you should put your just-finished burger into? Try testing your trash-sorting skills with the interactive game below. Simply drag the waste items into the correct waste bin and compare your score with others!
+</p>
+
+<div>
 </div>
+<div id="interactive"><h1>Interactive goes here</h1></div>
+<div id="title">
+  <p>UCLA Waste Bin Breakdown</p>
+</div>
+
 <div id="waffle">
 </div>
 <div id="legend">
--- js/posts/waste-audits/audits.js
+++ js/posts/waste-audits/audits.js
@@ -1,77 +1,72 @@
 
 var total = 0;
 var width,
-    height,
-    widthSquares = 20,
-    heightSquares = 5,
-    squareSize = 25,
-    squareValue = 0,
-    gap = 1,
-    theData = [];
+  height,
+  widthSquares = 20,
+  heightSquares = 5,
+  squareSize = 25,
+  squareValue = 0,
+  gap = 1,
+  theData = [];
 
 var color = d3.scale.category10();
 
-d3.csv("../datasets/waste-audits/data.csv", function(error, data)
-{
+d3.csv("../../datasets/waste-audits/data.csv", function (error, data) {
   //total
-  total = d3.sum(data, function(d) { return d.population; });
+  total = d3.sum(data, function (d) { return d.population; });
 
   //value of a square
-  squareValue = total / (widthSquares*heightSquares);
+  squareValue = total / (widthSquares * heightSquares);
 
   //remap data
-  data.forEach(function(d, i)
-  {
-      d.population = +d.population;
-      d.units = Math.floor(d.population/squareValue);
-      theData = theData.concat(
-        Array(d.units+1).join(1).split('').map(function()
-          {
-            return {  squareValue:squareValue,
-                      units: d.units,
-                      population: d.population,
-                      groupIndex: i};
-          })
-        );
+  data.forEach(function (d, i) {
+    d.population = +d.population;
+    d.units = Math.floor(d.population / squareValue);
+    theData = theData.concat(
+      Array(d.units + 1).join(1).split('').map(function () {
+        return {
+          squareValue: squareValue,
+          units: d.units,
+          population: d.population,
+          groupIndex: i
+        };
+      })
+    );
   });
 
-  width = (squareSize*widthSquares) + widthSquares*gap + 25;
-  height = (squareSize*heightSquares) + heightSquares*gap + 25;
+  width = (squareSize * widthSquares) + widthSquares * gap + 25;
+  height = (squareSize * heightSquares) + heightSquares * gap + 25;
 
-  var waffle = d3.select('waffle')
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .selectAll("div")
-      .data(theData)
-      .enter()
-      .append("rect")
-      .attr("width", squareSize)
-      .attr("height", squareSize)
-      .attr("fill", function(d)
-      {
-        return color(d.groupIndex);
-      })
-      .attr("x", function(d, i)
-        {
-          //group n squares for column
-          col = Math.floor(i/heightSquares);
-          return (col*squareSize) + (col*gap);
-        })
-      .attr("y", function(d, i)
-      {
-        row = i%heightSquares;
-        return (heightSquares*squareSize) - ((row*squareSize) + (row*gap))
-      })
-      .append("title")
-        .text(function (d,i)
-          {
-            return "Age range: " + data[d.groupIndex].age + " | " +  d.population + " , " + d.units + "%"
-          });
+  var waffle = d3.select("#waffle")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .selectAll("div")
+    .data(theData)
+    .enter()
+    .append("rect")
+    .attr("width", squareSize)
+    .attr("height", squareSize)
+    .attr("fill", function (d) {
+      return color(d.groupIndex);
+    })
+    .attr("x", function (d, i) {
+      //group n squares for column
+      col = Math.floor(i / heightSquares);
+      return (col * squareSize) + (col * gap);
+    })
+    .attr("y", function (d, i) {
+      row = i % heightSquares;
+      return (heightSquares * squareSize) - ((row * squareSize) + (row * gap))
+    })
+    .append("title")
+    .text(function (d, i) {
+      return "Age range: " + data[d.groupIndex].age + " | " + d.population + " , " + d.units + "%"
+    });
 
-   //add legend with categorical data
-   var legend = d3.select('legend')
+  //add legend with categorical data
+  var legend = d3.select("#legend")
     .append("svg")
     .attr('width', 300)
     .attr('height', 200)
@@ -79,26 +74,26 @@ d3.csv("../datasets/waste-audits/data.csv", function(error, data)
     .selectAll("div")
     .data(data)
     .enter()
-      .append("g")
-      .attr('transform', function(d,i){ return "translate(0," + i*20 + ")";});
+    .append("g")
+    .attr('transform', function (d, i) { return "translate(0," + i * 20 + ")"; });
   legend.append("rect")
     .attr("width", 18)
     .attr("height", 18)
-    .style("fill", function(d, i) { return color(i)});
+    .style("fill", function (d, i) { return color(i) });
   legend.append("text")
     .attr("x", 25)
     .attr("y", 13)
-    .text( function(d) { return d.age});
+    .text(function (d) { return d.age });
 
-    //add value of a unit square
-    var legend2 = d3.select("#legend")
-      .select('svg')
-      .append('g')
-      .attr('transform', "translate(100,0)");
+  //add value of a unit square
+  var legend2 = d3.select("#legend")
+    .select('svg')
+    .append('g')
+    .attr('transform', "translate(100,0)");
 
-legend2.append("text")
-        .attr('y', '14')
-        .attr('font-size', '18px')
-        .text("Total: " + total)
-        .attr("fill", "#444444");
+  legend2.append("text")
+    .attr('y', '14')
+    .attr('font-size', '18px')
+    .text("Total: " + total)
+    .attr("fill", "#444444");
 });
