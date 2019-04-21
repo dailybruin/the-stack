7a44c0c7ad861798bf6de121ddb274f91c15076c
Andrew <andrewzkan@gmail.com>
2019-04-21 16:06:10 -0700

    added single dropdown menu but transitions don't work anymore and idk but graphs won't load in stack folder

--- _posts/2019-03-05-waste-audits.md
+++ _posts/2019-03-05-waste-audits.md
@@ -4,7 +4,7 @@ teaser: Learn how often students recycle and compost, and test your trash-sortin
 authors:
   - andrew_kan
 key_takeaways:
-  - In non-dining buildings, around 30% of trash is placed in the landfill bin, 30% in the recycling bin, and 40% in the compost bin.
+  - In non-dining buildings, around 30% of trash by weight is placed in the landfill bin, 30% in the recycling bin, and 40% in the compost bin.
   - In dining halls, roughly 80% of food waste is edible.
 featured_image:
   url: 2018db-recruitment/featured_image.png
@@ -12,10 +12,42 @@ og_image: 2018db-recruitment/featured_image.png
 stylesheets:
   - /css/posts/waste-audits/app.css
 scripts:
-  - //https://d3js.org/d3.v5.min.js
+  - //https://d3js.org/d3.v3.min.js
   - /js/posts/waste-audits/audits.js
 ---
+<style>
+    select {
+      display: block;
+    }
 
+    #landfillGraph .bar {
+      fill: tan;
+      opacity: 0.8;
+    }
+
+    #recyclingGraph .bar {
+      fill: lightblue;
+      opacity: 0.8;
+    }
+
+    #compostGraph .bar {
+      fill: lightgreen;
+      opacity: 0.8;
+    }
+
+    .axis path,
+    .axis line {
+      fill: none;
+      stroke: #000;
+      shape-rendering: crispEdges;
+    }
+
+    .graph-container {
+      display: flex;
+      flex-wrap: wrap;
+      flex-direction: row;
+    }
+  </style>
 <p>
   Walking around campus, one can easily spot the trio of landfill, recycling, and compost bins. Recycling and composting is more eco-friendly than throwing everything into the landfill, but how often do students actually do so? The Daily Bruin investigates how students sort their trash into these bins around campus.
 </p>
@@ -27,11 +59,27 @@ scripts:
 <div>
 </div>
 <div id="interactive"><h1>Interactive goes here</h1></div>
-<div id="title">
-  <p>UCLA Waste Bin Breakdown</p>
-</div>
 
-<div id="waffle">
-</div>
-<div id="legend">
-</div>
+ <div id="title">
+    <p style="font: 36px Garamond; text-align: center">UCLA Waste Bin Breakdown</p>
+  </div>
+
+  <div id="dropdown-menu"></div>
+  <div class="graph-container">
+    <div id="landfillGraph"></div>
+    <div id="landfillLegend"></div>
+    <div id="recyclingGraph"></div>
+    <div id="recyclingLegend"></div>
+    <div id="compostGraph"></div>
+    <div id="compostLegend"></div>
+  </div>
+  <p style="font-size: 15px">
+    Things to do:
+    <ul style="font-size: 14px">
+      <li>tried to make transitions work for like 2 hours but never did correctly :( </li>
+      <li>get copy illustrations, message nicole</li>
+      <li>make backend for interactive work, talk with neil</li>
+      <li>make numbers show up in middle of bars instead of having to use tooltip</li>
+      <li>integrate numerical weights somewhere</li>
+    </ul>
+  </p>
--- js/posts/waste-audits/audits.js
+++ js/posts/waste-audits/audits.js
@@ -1,99 +1,419 @@
+// Load and munge data, then make the visualization.
+let landfillFileName = "../../datasets/waste-audits/Landfill&Other.csv";
+let landfillFields = ["Landfill", "Construction", "OCC"];
+let recyclingFileName = "../../datasets/waste-audits/Recycling.csv";
+let recyclingFields = ["Recycling", "Paper", "Plastic", "Metal", "Glass", "Straws & Utensils"];
+let compostFileName = "../../datasets/waste-audits/Compost.csv";
+let compostFields = ["Compost", "Liquid", "Edible", "Non-edible", "Napkins"];
 
-var total = 0;
-var width,
-  height,
-  widthSquares = 20,
-  heightSquares = 5,
-  squareSize = 25,
-  squareValue = 0,
-  gap = 1,
-  theData = [];
+let dropdown = d3.select("#dropdown-menu")
+  .insert("select", "svg");
+let landfillMap = {};
+let recyclingMap = {};
+let compostMap = {};
+let landfills;
+let dropdownValue;
+// Handler for dropdown value change
+let DropdownChange = function () {
+  dropdownValue = d3.select(this).property('value');
+  makeLandfillVis(landfillMap);
+  makeRecyclingVis(recyclingMap);
+  makeCompostVis(compostMap);
+};
 
-var color = d3.scale.category10();
+dropdown
+  .on("change", DropdownChange);
 
-d3.csv("../../datasets/waste-audits/data.csv", function (error, data) {
-  //total
-  total = d3.sum(data, function (d) { return d.population; });
+d3.csv(landfillFileName, function (error, data) { //landfill csv input
+  data.forEach(function (d) {
+    let place = d.place;
+    landfillMap[place] = [];
 
-  //value of a square
-  squareValue = total / (widthSquares * heightSquares);
-
-  //remap data
-  data.forEach(function (d, i) {
-    d.population = +d.population;
-    d.units = Math.floor(d.population / squareValue);
-    theData = theData.concat(
-      Array(d.units + 1).join(1).split('').map(function () {
-        return {
-          squareValue: squareValue,
-          units: d.units,
-          population: d.population,
-          groupIndex: i
-        };
-      })
-    );
-  });
-
-  width = (squareSize * widthSquares) + widthSquares * gap + 25;
-  height = (squareSize * heightSquares) + heightSquares * gap + 25;
-
-  var waffle = d3.select("#waffle")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .selectAll("div")
-    .data(theData)
-    .enter()
-    .append("rect")
-    .attr("width", squareSize)
-    .attr("height", squareSize)
-    .attr("fill", function (d) {
-      return color(d.groupIndex);
-    })
-    .attr("x", function (d, i) {
-      //group n squares for column
-      col = Math.floor(i / heightSquares);
-      return (col * squareSize) + (col * gap);
-    })
-    .attr("y", function (d, i) {
-      row = i % heightSquares;
-      return (heightSquares * squareSize) - ((row * squareSize) + (row * gap))
-    })
-    .append("title")
-    .text(function (d, i) {
-      return "Age range: " + data[d.groupIndex].age + " | " + d.population + " , " + d.units + "%"
+    // { cerealName: [ bar1Val, bar2Val, ... ] }
+    landfillFields.forEach(function (field) {
+      landfillMap[place].push(+d[field]);
+      //console.log(+d[field]);
     });
-
-  //add legend with categorical data
-  var legend = d3.select("#legend")
-    .append("svg")
-    .attr('width', 300)
-    .attr('height', 200)
-    .append('g')
-    .selectAll("div")
-    .data(data)
-    .enter()
-    .append("g")
-    .attr('transform', function (d, i) { return "translate(0," + i * 20 + ")"; });
-  legend.append("rect")
-    .attr("width", 18)
-    .attr("height", 18)
-    .style("fill", function (d, i) { return color(i) });
-  legend.append("text")
-    .attr("x", 25)
-    .attr("y", 13)
-    .text(function (d) { return d.age });
-
-  //add value of a unit square
-  var legend2 = d3.select("#legend")
-    .select('svg')
-    .append('g')
-    .attr('transform', "translate(100,0)");
-
-  legend2.append("text")
-    .attr('y', '14')
-    .attr('font-size', '18px')
-    .text("Total: " + total)
-    .attr("fill", "#444444");
+  });
+  // Get names of places, for dropdown
+  landfills = Object.keys(landfillMap).sort();
+  dropdownValue = landfills[0];
+  dropdown.selectAll("option")
+    .data(landfills)
+    .enter().append("option")
+    .attr("value", function (d) { return d; })
+    .text(function (d) {
+      return d[0].toUpperCase() + d.slice(1, d.length); // capitalize 1st letter
+    });
+  makeLandfillVis(landfillMap);
 });
+
+d3.csv(recyclingFileName, function (error, data) { //recycling csv input
+  data.forEach(function (d) {
+    let place = d.place;
+    recyclingMap[place] = [];
+
+    // { cerealName: [ bar1Val, bar2Val, ... ] }
+    recyclingFields.forEach(function (field) {
+      recyclingMap[place].push(+d[field]);
+      //console.log(+d[field]);
+    });
+  });
+  makeRecyclingVis(recyclingMap);
+});
+
+d3.csv(compostFileName, function (error, data) { //compost csv input
+  data.forEach(function (d) {
+    let place = d.place;
+    compostMap[place] = [];
+
+    // { cerealName: [ bar1Val, bar2Val, ... ] }
+    compostFields.forEach(function (field) {
+      compostMap[place].push(+d[field]);
+      //console.log(+d[field]);
+    });
+  });
+  makeCompostVis(compostMap);
+});
+
+
+
+let makeLandfillVis = function (landfillMap) {
+  // Define dimensions of vis
+  let margin = { top: 30, right: 30, bottom: 70, left: 40 },
+    width = 370 - margin.left - margin.right,
+    height = 490 - margin.top - margin.bottom;
+
+  // Make x scale
+  let xScale = d3.scale.ordinal()
+    .domain(landfillFields)
+    .rangeRoundBands([0, width], 0.1);
+
+  // Make y scale, the domain will be defined on bar update
+  let yScale = d3.scale.linear()
+    .range([height, 0]);
+
+  //trying to remove the old graphs
+  /*let oldcanvas = document.getElementById("#landfillGraph");
+  oldcanvas.innerHTML = "";*/
+
+  // Delete old canvas and create new canvasas
+  d3.select("#landfillGraph")
+    .selectAll("*")
+    .remove();
+  let canvas = d3.select("#landfillGraph")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Make x-axis and add to canvas
+  let xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+
+  canvas.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+    .append("text")
+    .attr("x", 90)
+    .attr("y", 40)
+    .text("Landfill Bins")
+    .attr("font-size", "18px")
+    .attr("font-weight", "bold");
+
+  // Make y-axis and add to canvas
+  let yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+  let yAxisHandleForUpdate = canvas.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+  yAxisHandleForUpdate.append("text")
+    .attr("y", -25)
+    .attr("x", 60)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Percentage")
+    .attr("font-weight", "bold")
+    .attr("font-size", "18px");
+
+  let updateLandfillBars = function (data) {
+    // First update the y-axis domain to match data
+    yScale.domain(d3.extent([0, 100]));
+    yAxisHandleForUpdate.call(yAxis);
+
+    let bars = canvas.selectAll(".bar").data(data);
+
+    // Add bars for new data
+    bars.enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", function (d, i) { return xScale(landfillFields[i]); })
+      .attr("width", xScale.rangeBand())
+      .attr("y", function (d, i) { return yScale(d); })
+      .attr("height", function (d, i) { return height - yScale(d); });
+
+    //WORKING ON TEXT ON BARS
+    bars.selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .text(function (d) { return d; });
+
+    let isMobile = window.innerWidth <= 767 ? true : false;
+    let tooltip = d3.select("body").append("div") //tooltip stuff
+      .style("position", "absolute")
+      .style("z-index", "10") //depth of the tooltip
+      .style("visibility", "hidden") //makes the tooltip hidden
+      .style("color", "white")
+      .style("padding", "8px")
+      .style("background-color", "rgba(0, 0, 0, 0.75)")
+      .style("border-radius", "6px")
+      .style("font", "12px sans-serif")
+      .text("tooltip");
+    //where tooltip code will go
+    if (!isMobile) {
+      bars.on("mouseover", function (d, i) {
+        tooltip.html(d + '%'); //creates the text when you mouseover
+        tooltip.style("visibility", "visible"); //makes the tooltip visible
+      })
+        .on("mousemove", function () {
+          return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"); //makes the tooltip next to the cursor
+        })
+        .on("mouseout", function () {
+          return tooltip.style("visibility", "hidden") //makes the tooltip disappear when you cursor out
+        });
+    }
+  };
+
+  updateLandfillBars(landfillMap[dropdownValue]);
+};
+
+
+
+
+//---------------------------------------------------------------------------------------------------------
+// Load and munge data, then make the visualization.
+
+
+
+let makeRecyclingVis = function (recyclingMap) {
+  // Define dimensions of vis
+  let margin = { top: 30, right: 30, bottom: 70, left: 40 },
+    width = 530 - margin.left - margin.right,
+    height = 490 - margin.top - margin.bottom;
+
+  // Make x scale
+  let xScale = d3.scale.ordinal()
+    .domain(recyclingFields)
+    .rangeRoundBands([0, width], 0.1);
+
+  // Make y scale, the domain will be defined on bar update
+  let yScale = d3.scale.linear()
+    .range([height, 0]);
+
+  // Delete old canvas and create new canvasas
+  d3.select("#recyclingGraph")
+    .selectAll("*")
+    .remove();
+
+  let canvas = d3.select("#recyclingGraph")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Make x-axis and add to canvas
+  let xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+
+  canvas.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+    .append("text")
+    .attr("x", 180)
+    .attr("y", 40)
+    .text("Recycling Bins")
+    .attr("font-size", "18px")
+    .attr("font-weight", "bold");
+
+  // Make y-axis and add to canvas
+  let yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+  let yAxisHandleForUpdate = canvas.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+  yAxisHandleForUpdate.append("text")
+    .attr("y", -25)
+    .attr("x", 60)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Percentage")
+    .attr("font-weight", "bold")
+    .attr("font-size", "18px");
+
+  let updateRecyclingBars = function (data) {
+    // First update the y-axis domain to match data
+    yScale.domain(d3.extent([0, 100]));
+    yAxisHandleForUpdate.call(yAxis);
+
+    let bars = canvas.selectAll(".bar").data(data);
+
+    // Add bars for new data
+    bars.enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", function (d, i) { return xScale(recyclingFields[i]); })
+      .attr("width", xScale.rangeBand())
+      .attr("y", function (d, i) { return yScale(d); })
+      .attr("height", function (d, i) { return height - yScale(d); });
+
+    let isMobile = window.innerWidth <= 767 ? true : false;
+    let tooltip = d3.select("body").append("div") //tooltip stuff
+      .style("position", "absolute")
+      .style("z-index", "10") //depth of the tooltip
+      .style("visibility", "hidden") //makes the tooltip hidden
+      .style("color", "white")
+      .style("padding", "8px")
+      .style("background-color", "rgba(0, 0, 0, 0.75)")
+      .style("border-radius", "6px")
+      .style("font", "12px sans-serif")
+      .text("tooltip");
+    //where tooltip code will go
+    if (!isMobile) {
+      bars.on("mouseover", function (d, i) {
+        tooltip.html(d + '%'); //creates the text when you mouseover
+        tooltip.style("visibility", "visible"); //makes the tooltip visible
+      })
+        .on("mousemove", function () {
+          return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"); //makes the tooltip next to the cursor
+        })
+        .on("mouseout", function () {
+          return tooltip.style("visibility", "hidden") //makes the tooltip disappear when you cursor out
+        });
+    }
+  };
+
+  updateRecyclingBars(recyclingMap[dropdownValue]);
+};
+
+
+//---------------------------------------------------------------------------------------------------------
+// Load and munge data, then make the visualization.
+
+let makeCompostVis = function (compostMap) {
+  // Define dimensions of vis
+  let margin = { top: 30, right: 30, bottom: 70, left: 40 },
+    width = 500 - margin.left - margin.right,
+    height = 490 - margin.top - margin.bottom;
+
+  // Make x scale
+  let xScale = d3.scale.ordinal()
+    .domain(compostFields)
+    .rangeRoundBands([0, width], 0.1);
+
+  // Make y scale, the domain will be defined on bar update
+  let yScale = d3.scale.linear()
+    .range([height, 0]);
+
+  // Delete old canvas and create new canvas
+  d3.select("#compostGraph")
+    .selectAll("*")
+    .remove();
+
+  let canvas = d3.select("#compostGraph")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Make x-axis and add to canvas
+  let xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+
+  canvas.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+    .append("text")
+    .attr("x", 190)
+    .attr("y", 40)
+    .text("Compost Bins")
+    .attr("font-size", "18px")
+    .attr("font-weight", "bold");
+
+  // Make y-axis and add to canvas
+  let yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+  let yAxisHandleForUpdate = canvas.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+  yAxisHandleForUpdate.append("text")
+    .attr("y", -25)
+    .attr("x", 60)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Percentage")
+    .attr("font-weight", "bold")
+    .attr("font-size", "18px");
+
+  let updateCompostBars = function (data) {
+    // First update the y-axis domain to match data
+    yScale.domain(d3.extent([0, 100]));
+    yAxisHandleForUpdate.call(yAxis);
+
+    let bars = canvas.selectAll(".bar").data(data);
+
+    // Add bars for new data
+    bars.enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", function (d, i) { return xScale(compostFields[i]); })
+      .attr("width", xScale.rangeBand())
+      .attr("y", function (d, i) { return yScale(d); })
+      .attr("height", function (d, i) { return height - yScale(d); });
+
+    let isMobile = window.innerWidth <= 767 ? true : false;
+    let tooltip = d3.select("body").append("div") //tooltip stuff
+      .style("position", "absolute")
+      .style("z-index", "10") //depth of the tooltip
+      .style("visibility", "hidden") //makes the tooltip hidden
+      .style("color", "white")
+      .style("padding", "8px")
+      .style("background-color", "rgba(0, 0, 0, 0.75)")
+      .style("border-radius", "6px")
+      .style("font", "12px sans-serif")
+      .text("tooltip");
+    //where tooltip code will go
+    if (!isMobile) {
+      bars.on("mouseover", function (d, i) {
+        tooltip.html(d + '%'); //creates the text when you mouseover
+        tooltip.style("visibility", "visible"); //makes the tooltip visible
+      })
+        .on("mousemove", function () {
+          return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"); //makes the tooltip next to the cursor
+        })
+        .on("mouseout", function () {
+          return tooltip.style("visibility", "hidden") //makes the tooltip disappear when you cursor out
+        });
+    }
+  };
+
+  updateCompostBars(compostMap[dropdownValue]);
+};
