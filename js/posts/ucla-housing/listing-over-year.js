7695fa1b376c07aca855f202ca9133099101f041
Ung Lik Teng <unglikteng.dev@gmail.com>
2019-05-06 17:18:10 -0700

    edit dataset

--- js/posts/ucla-housing/listing-over-year.js
+++ js/posts/ucla-housing/listing-over-year.js
@@ -1,4 +1,4 @@
-Plotly.d3.csv('/datasets/posts/ucla-housing/listing_over_year.csv', function(err, rows){
+Plotly.d3.csv('/datasets/ucla-housing/listing_over_year.csv', function(err, rows){
     function unpack(rows, key) {
           return rows.map(function(row) { return row[key]; });
     }
--- js/posts/ucla-housing/westwood-apt-bubble-map.js
+++ js/posts/ucla-housing/westwood-apt-bubble-map.js
@@ -1,4 +1,4 @@
-Plotly.d3.csv('/datasets/posts/ucla-housing/price_by_street.csv', function(err, rows){
+Plotly.d3.csv('/datasets/ucla-housing/price_by_street.csv', function(err, rows){
     function unpack(rows, key) {
           return rows.map(function(row) { return row[key]; });
     }
