02077001c64022c835c6a87e2f881aa6fef7b9cb
Keith Atienza <keithatienza@keiths-mbp.home>
2019-05-22 21:44:13 -0700

    change plotly mobile link

--- js/posts/change-major/sankey.js
+++ js/posts/change-major/sankey.js
@@ -394,5 +394,5 @@ d3.select('iframe')
     .attr("width", function(){if(width > 600) return('900')
                                 else return('400')})
     .attr("src", function(){if(width > 600) return("//plot.ly/~jeanettelin8/9.embed?showlink=false")
-                            else return("//plot.ly/~jeanettelin8/5.embed?showlink=false")})
+                            else return("//plot.ly/~strikeitrich/3.embed?showlink=false")})
     .attr('height', '600');
