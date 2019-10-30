fa7f3d3ce9a5ce9759ee8cd4a1dc72db3aa2130d
Andrew <andrewzkan@gmail.com>
2019-10-29 19:53:57 -0700

    made clicking pages work for mac

--- parsing/class_parser.py
+++ parsing/class_parser.py
@@ -5,43 +5,44 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 import unittest
 import re
 
 page_num = 1
+timeout = 10
 
 driver = webdriver.Chrome('./chromedriver')
-# have selenium get the mathematics website
+# have selenium get the department website
+# driver.get("https://sa.ucla.edu/ro/Public/SOC/Results?t=19F&sBy=subject&sName=Computer+Science+%28COM+SCI%29&subj=COM+SCI&crsCatlg=Enter+a+Catalog+Number+or+Class+Title+%28Optional%29&catlg=&cls_no=&btnIsInIndex=btn_inIndex")
 driver.get("https://sa.ucla.edu/ro/Public/SOC/Results?t=19F&sBy=subject&sName=Mathematics+%28MATH%29&subj=MATH&crsCatlg=Enter+a+Catalog+Number+or+Class+Title+%28Optional%29&catlg=&cls_no=&btnIsInIndex=btn_inIndex")
+# driver.get("https://sa.ucla.edu/ro/Public/SOC/Results?t=19F&sBy=subject&sName=English+%28ENGL%29&subj=ENGL&crsCatlg=Enter+a+Catalog+Number+or+Class+Title+%28Optional%29&catlg=&cls_no=&btnIsInIndex=btn_inIndex")
 driver.maximize_window()
 # iterate through all the pages in the department
-while(page_num <= 4):
-    wait = WebDriverWait(driver, 3)
-    """wait.until(EC.element_to_be_clickable(
-        (By.XPATH, "//ul[@class='jPag-pages']")))
-    page_col = driver.find_element_by_xpath(
-        "//ul[@class='jPag-pages']")
-    pages = page_col.find_elements_by_tag_name("a")
-    for page in pages:
-        if (page.text == str(page_num) and page_num != 1):
-            # driver.execute_script("arguments[0].click();", page)
-            page.click()"""
-    """wait.until(EC.presence_of_element_located(
-        (By.XPATH, "//input[@id='pageCount']")))
-    page = driver.find_element_by_id("pageCount")
-    driver.execute_script(
-        "arguments[0].setAttribute('value', {page_num})", page)
-    driver.set_page_load_timeout(3)"""
-    # wait for up to 5 seconds for the page to be clickable
-
+all_pages = driver.find_element_by_class_name("jPag-pages")
+pages = all_pages.find_elements_by_tag_name("li")
+num_pages = len(pages)
+while(page_num <= num_pages):
+    wait = WebDriverWait(driver, timeout)
+    # wait for up to 3 seconds for the page to be clickable
+    page_string = "ul.jPag-pages li:nth-child(" + str(page_num) + ")"
+    wait.until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, page_string)))
+    actions = ActionChains(driver)
+    cur_page = driver.find_element_by_css_selector(
+        page_string)
+    actions.move_to_element(cur_page).click().perform()
+    # wait for the results to load
+    wait.until(EC.visibility_of_element_located(
+        (By.XPATH, "//div[@class='row-fluid class-title']")))
     # wait for up to 5 seconds for the expand all classes to be clickable, then click it
-    expand_button = wait.until(
+    """expand_button = wait.until(
         EC.element_to_be_clickable((By.XPATH, "//a[@id='expandAll']")))
-    expand_button.click()
-
-    # wait for up to 3 seconds for the new page to load
-    driver.set_page_load_timeout(3)
+    expand_button.click()"""
+    actions = ActionChains(driver)
+    expand_button = driver.find_element_by_xpath("//a[@id='expandAll']")
+    actions.move_to_element(expand_button).click().perform()
     # get and return all the status columns when they're loaded
     innerHTML = wait.until(EC.visibility_of_all_elements_located(
         (By.XPATH, "//div[@class='statusColumn']")))
@@ -68,13 +69,12 @@ while(page_num <= 4):
         for s in status:
             status_text = s.find('p').text
             if (status_text != "Status"):
-                print(status_text)
-                print()
+                print(status_text + '\n')
             if (status_text.find('Closed') == 0):
                 closed_classes += 1
             if (status_text.find('Open') == 0):
                 open_classes += 1
-
-    print("Open Classes " + str(open_classes))
-    print("Closed Classes " + str(closed_classes))
     page_num += 1
+
+print("Open Classes " + str(open_classes))
+print("Closed Classes " + str(closed_classes))
