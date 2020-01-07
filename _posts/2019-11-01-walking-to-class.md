fca0bd1d6de8a858d6127db55e0f546e60dad44f
Laurel Woods <laurelrwoods@gmail.com>
2020-01-06 20:56:00 -0800

    Updated routes and added stats table.

--- _posts/2019-11-01-walking-to-class.md
+++ _posts/2019-11-01-walking-to-class.md
@@ -9,10 +9,11 @@ og_image:
 stylesheets:
     - //api.mapbox.com/mapbox-gl-js/v0.36.0/mapbox-gl.css
     - //unpkg.com/leaflet@1.0.3/dist/leaflet.css
+    - /css/posts/course-schedule/dropdown.min.css
     - /css/posts/walking-to-class/app.css
 scripts:
     - /js/lib/jquery-3.1.1.min.js
-    - //d3js.org/d3.v5.min.js
+    - //d3js.org/d3.v3.min.js
     - //api.mapbox.com/mapbox-gl-js/v0.36.0/mapbox-gl.js
     - //api.mapbox.com/mapbox.js/plugins/leaflet-omnivore/v0.2.0/leaflet-omnivore.min.js
     - //unpkg.com/leaflet@1.0.3/dist/leaflet.js
@@ -23,22 +24,56 @@ scripts:
 
 
 <div id="dropdowns">
+        <span>I'm walking from </span>
         <select class="ui search selection dropdown" id="start_location"></select>
+        <span> to </span>
         <select class="ui search selection dropdown" id="end_location"></select>
-    </div>
+</div>
 <div id="vis">
     <div id="mapid"></div>
     <div id="stats">
-        <h4>Route A</h4>
-        <p>Distance</p>
-        <p>feet elevation gain</p>
-        <p>feet elevation loss</p>
-        <p>stairs</p>
-        <h4>Route B</h4>
-        <p>Distance</p>
-        <p>feet elevation gain</p>
-        <p>feet elevation loss</p>
-        <p>stairs</p>
+        <h2>Route Statistics</h2>
+        <table id="stats_table"> 
+            <tr> 
+                <th></th>
+                <th id="route_A">Route A</th>
+                <th id="route_B">Route B</th>
+            </tr>
+            <tr>
+                <td class="header_cell">distance:</td>
+                <td id="dist_A"></td>
+                <td id="dist_B"></td>
+            </tr>
+            <tr> 
+                <td class="header_cell">elevation gain:</td>
+                <td id="gain_A"></td>
+                <td id="gain_B"></td>
+            </tr>
+            <tr> 
+                <td class="header_cell">elevation loss:</td>
+                <td id="loss_A"></td>
+                <td id="loss_B"></td>
+            </tr>
+            <tr> 
+                <td class="header_cell">number of stairs:</td>
+                <td id="stairs_A"></td>
+                <td id="stairs_B"></td>
+            </tr>
+            <tr> 
+                <td class="header_cell">time:</td>
+                <td id="time_A"></td>
+                <td id="time_B"></td>
+            </tr>
+        </table>
+        <h4>How fast do you walk?</h4>
+        <div class="btn-group" data-toggle="buttons">
+            <input type="radio" id="slow" name="speed" value="24">
+            <label class="btn" for="slow">Slow (~2.5mph)</label>
+            <input type="radio" id="medium" name="speed" value="20" checked>
+            <label class="btn" for="medium">Medium (~3mph)</label>
+            <input type="radio" id="fast" name="speed" value="15">
+            <label class="btn" for="fast">Fast (~4mph)</label>
+        </div>
     </div>
 </div>
 
--- js/posts/walking-to-class/map.js
+++ js/posts/walking-to-class/map.js
@@ -2,7 +2,7 @@ var mymap = L.map('mapid', {
 	center: [34.0711224,-118.4418554],
 	zoom: 15.5,
 	minZoom: 15,
-	maxBounds : [[34.07925, -118.45774], [34.06278, -118.4362]]
+	maxBounds : [[34.07925, -118.45774], [34.060, -118.4362]]
 });
 
 var baseLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVycnlsaW5ldyIsImEiOiJjajIwc2E1YXkwMmt6MzNuMXZnaWRjb2lhIn0.u0AHZNQd8_8hFmpaVvI1nQ', {
@@ -15,20 +15,26 @@ var start_locations = {
 	"Hedrick" : "hedrick", 
 	"Rieber" : "rieber",
 	"Sproul" : "sproul",
-	"De Neve" : "cross"
+	"De Neve" : "deneve"
 };
 
 var end_locations = {
-	"Sculpture Garden" : "sculpture", 
 	"Powell" : "powell",
-	"Boelter" : "boelter",
+	"Anderson" : "anderson", 
+	"Public Affairs" : "public",
+	"Court of Sciences" : "court",
+	"Young Research Library" : "yrl",
 	"Target" : "target"
 };
 
+var min_per_mile = 20;
+
 d3.select('#start_location')
         .on('change', function(e) {
-        	generateRoutes($('#start_location option:selected').text(),
-				$('#end_location option:selected').text());
+        	let start = $('#start_location option:selected').text();
+        	let end = $('#end_location option:selected').text();
+        	generateRoutes(start, end);
+        	loadStats(start, end);
         })
     	.selectAll('option')
         .data(Object.keys(start_locations))
@@ -39,8 +45,10 @@ d3.select('#start_location')
 
 d3.select('#end_location')
         .on('change', function(e) {
-        	generateRoutes($('#start_location option:selected').text(),
-				$('#end_location option:selected').text());
+        	let start = $('#start_location option:selected').text();
+        	let end = $('#end_location option:selected').text();
+        	generateRoutes(start, end);
+        	loadStats(start, end);
         })
     	.selectAll('option')
         .data(Object.keys(end_locations))
@@ -49,12 +57,19 @@ d3.select('#end_location')
     	.attr("value", function (d) {return d;})
         .text(function (d) {return d;});
 
+d3.selectAll("input[name='speed']")
+		.on('change', function(e) {
+			min_per_mile = this.value;
+			let start = $('#start_location option:selected').text();
+        	let end = $('#end_location option:selected').text();
+			loadStats(start, end);
+		});
 
-//generateRoutes("Hedrick", "Sculpture Garden");
+
+generateRoutes("Hedrick", "Powell");
+loadStats("Hedrick", "Powell");
 // calculateStats();
 
-var x = generateLayer("/datasets/walking-to-class/hedrick_anderson_bruinwalk.gpx", "red");
-mymap.addLayer(x);
 
 function generateRoutes(start, end) {
 	
@@ -65,32 +80,14 @@ function generateRoutes(start, end) {
 	});
 
 	var path = "/datasets/walking-to-class/";
-	var route_A_hill = path + start_locations[start] + "_cross.gpx"; 
-	var route_A_campus = path + "cross_" + end_locations[end] + ".gpx";
+	var route_A = path + start_locations[start] + "_" + end_locations[end] + "_A.gpx"; 
+	var route_B = path + start_locations[start] + "_" + end_locations[end] + "_B.gpx"; 
 
-	if (end == "Sculpture Garden" || end == "Powell") {
-		var route_B_hill = path + start_locations[start] + "_stop.gpx"; 
-		var route_B_campus = path + "stop_" + end_locations[end] + ".gpx";
-	} else {
-		var route_B_hill = path + start_locations[start] + "_cross.gpx"; 
-		var route_B_campus = path + "cross_" + end_locations[end] + "_B.gpx";
-	}
+	var r1 = this.generateLayer(route_A, "green");
+	mymap.addLayer(r1);
 
-	if (start != "De Neve") {
-		var r1 = this.generateLayer(route_A_hill, "green");
-		mymap.addLayer(r1);
-	}
-
-	var r2 = this.generateLayer(route_A_campus, "green");
+	var r2 = this.generateLayer(route_B, "blue");
 	mymap.addLayer(r2);
-
-	if (start != "De Neve" || (end == "Sculpture Garden" || end == "Powell")) {
-		var r3 = this.generateLayer(route_B_hill, "blue");
-		mymap.addLayer(r3);
-	}
-
-	var r4 = this.generateLayer(route_B_campus, "blue");
-	mymap.addLayer(r4);
 
 }
 
@@ -103,80 +100,73 @@ function generateLayer(filePath, color) {
 	    	shadowUrl: ''
 		},
 		polyline_options: {
-	    color: color
-	  }
-	}).on('loaded', function(e) {
-	  console.log(e.target.get_distance_imp());
-	  console.log(e.target.get_elevation_gain());
-	  console.log(e.target.get_elevation_loss());
-	}));
+		    color: color
+		  }
+		}));
 }
 
+async function loadStats(start, end) {
+
+	let data = await loadCSVData(start_locations[start], end_locations[end]);
+
+	document.getElementById("dist_A").innerHTML = Number(data[0]["distance"]).toFixed(3) + " miles";
+	document.getElementById("gain_A").innerHTML = Number(data[0]["elevation_gain"]).toFixed(1) + " feet";
+	document.getElementById("loss_A").innerHTML = Number(data[0]["elevation_loss"]).toFixed(1) + " feet";
+	document.getElementById("stairs_A").innerHTML = data[0]["number_stairs"];
+	document.getElementById("time_A").innerHTML = Number(data[0]["distance"] * min_per_mile).toFixed(0) + " minutes";
+
+
+	document.getElementById("dist_B").innerHTML = Number(data[1]["distance"]).toFixed(3) + " miles";
+	document.getElementById("gain_B").innerHTML = Number(data[1]["elevation_gain"]).toFixed(1) + " feet";
+	document.getElementById("loss_B").innerHTML = Number(data[1]["elevation_loss"]).toFixed(1) + " feet";
+	document.getElementById("stairs_B").innerHTML = data[1]["number_stairs"];
+	document.getElementById("time_B").innerHTML = Number(data[1]["distance"] * min_per_mile).toFixed(0) + " minutes";
+
+}
+
+function loadCSVData(start, end) {
+	return new Promise(resolve => {
+		d3.csv("/datasets/walking-to-class/route_stats.csv", function(csv) {
+    	csv = csv.filter(function(row) {
+        	return row['start'] == start && row['stop'] == end;
+    	})
+    	resolve(csv);
+	});
+	});
+}
 
 
 
 async function calculateStats() {
-	let route_data = "start,stop,route,trial,distance,elevation_gain,elevation_loss,number_stairs\n";
+	let route_data = "start,stop,route,distance,elevation_gain,elevation_loss,number_stairs\n";
 
-	let filePath = "/datasets/walking-to-class/cross_powell_2.gpx";
-	let s = await getStats(filePath);
-	console.log(filePath + ": " + s[0] + ", " + s[1] + ", " + s[2]);
+	for (s in start_locations) {
+		for (e in end_locations) {
+			let start = start_locations[s];
+			let end = end_locations[e];
 
-	// for (s in start_locations) {
-	// 	let start = start_locations[s];
+			let path = "/datasets/walking-to-class/";
+			let route_A = path + start + "_" + end + "_A.gpx"; 
+			let route_B = path + start + "_" + end + "_B.gpx"; 
 
-	// 	for (let i = 1; i <= 3; i++) {
-	// 		let path = "/datasets/walking-to-class/";
-	// 		let suffix = i != 1 ? "_" + i + ".gpx" : ".gpx"; 
-		
-	// 		if (start != "cross") {
-	// 			let route_cross = path + start + "_cross" + suffix;
-	// 			let cross_stats = await getStats(route_cross);
-	// 			route_data += (start + ",cross,A," + i + "," + cross_stats[0] + "," + cross_stats[1] + "," + cross_stats[2] + ",\n");
-	// 		}	
+			let a_stats = await getStats(route_A);
+			let b_stats = await getStats(route_B);
 
-	// 		let route_stop = path + start + "_stop" + suffix; 
-	// 		let stop_stats = await getStats(route_stop);
+			route_data += (start + "," + end + ",A," + a_stats[0] + "," + a_stats[1] + "," + a_stats[2] + ",\n");
+			route_data += (start + "," + end + ",B," + b_stats[0] + "," + b_stats[1] + "," + b_stats[2] + ",\n");
+		}
+	}
 
-	// 		route_data += (start + ",stop,A," + i + "," + stop_stats[0] + "," + stop_stats[1] + "," + stop_stats[2] + ",\n");
-	// 	}
-	// }
-
-	// for (e in end_locations) {
-	// 	let end = end_locations[e];
-
-	// 	for (let i = 1; i <= 3; i++) {
-	// 		let path = "/datasets/walking-to-class/";
-	// 		let suffix = i != 1 ? "_" + i + ".gpx" : ".gpx"; 
-
-	// 		if (end != "powell" || i == 1) {
-	// 			let route_A = path + "cross_" + end + suffix;
-	// 			let a_stats = await getStats(route_A);
-	// 			route_data += ("cross," + end + ",A," + i + "," + a_stats[0] + "," + a_stats[1] + "," + a_stats[2] + ",\n");
-	// 		}
-			
-	// 		if (end == "sculpture" || end == "powell") {
-	// 			let route_B = path + "stop_" + end + suffix;
-	// 			let b_stats = await getStats(route_B);
-	// 			route_data += ("stop," + end + ",A," + i + "," + b_stats[0] + "," + b_stats[1] + "," + b_stats[2] + ",\n");
-	// 		} else if (end != "target" || i < 3){
-	// 			let route_B = path + "cross_" + end + "_B" + suffix;
-	// 			let b_stats = await getStats(route_B);
-	// 			route_data += ("cross," + end + ",B," + i + "," + b_stats[0] + "," + b_stats[1] + "," + b_stats[2] + ",\n");
-	// 		}
-
-	// 	}
-	// }
-
-	// console.log("\n\n" + route_data);
+	console.log("\n\n" + route_data);
 
 
-	// var hiddenElement = document.createElement('a');
- //    hiddenElement.href = 'data:text/route_data;charset=utf-8,' + encodeURI(route_data);
- //    hiddenElement.target = '_blank';
- //    hiddenElement.download = 'route_data.csv';
- //    hiddenElement.click();
+	var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/route_data;charset=utf-8,' + encodeURI(route_data);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'route_data.csv';
+    hiddenElement.click();
 }
+
 function getStats(filePath) {
 	return new Promise(resolve => {
 		let x = new L.GPX(filePath, {
