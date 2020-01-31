302c19e98da7e88a318d651efe9d350b00ac979a
Juan Rios <25701682+jcr7467@users.noreply.github.com>
2020-01-31 15:28:31 -0800

    Ticket data (#76)
    * Finalized heatmap
    
    * Tweaked text
    
    * Modified URL's and CSS
    
    * updated JS
    
    * Modified hourly graph
    
    * Modified CSS for responsive plot
    
    * CSS tweak top padding
    
    * Changed from iframe to HTML
    
    * shifted title plot #2
    
    * fixed height

---
+++ datasets/ticket-data/september/convertToJSData.py
@@ -0,0 +1,65 @@
+import csv
+
+
+
+fileToBeOpened = "uclaTick2018Month9.csv"
+# initializing the titles list
+fields = []
+
+googleCoordinates = open('data.js', 'a')
+
+#To begin the file, we are creating an array
+googleCoordinates.write("var septemberData = [\n")
+
+
+
+size = 0
+# reading csv file and counting the total size count
+with open(fileToBeOpened, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+
+    # extracting field names through first row
+    fields = csvreader.__next__();
+
+    # extracting each data row one by one
+    for i, row in enumerate(csvreader):
+        #print(int(row[0]))
+
+        size = csvreader.line_num
+
+    # print total number of rows
+    print("Total no. of rows: %d" % (size))
+
+
+
+
+
+
+
+
+# writing to the seperate file where we are going to store our coordinates
+with open(fileToBeOpened, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+
+    # extracting field names through first row
+    fields = csvreader.__next__()
+
+    # extracting the latitude and longitude for each data entry
+    for i, row in enumerate(csvreader):
+        #rows.append(row)
+        if i == size - 2:
+            itemEntry = "new google.maps.LatLng(%(latitude)s, %(longitude)s)\n];" % {"latitude": row[19], "longitude": row[20]}
+        else:
+            itemEntry = "new google.maps.LatLng(%(latitude)s, %(longitude)s),\n" % {"latitude": row[19], "longitude": row[20]}
+
+        #print(itemEntry)
+        #print(int(row[0]))
+
+
+        googleCoordinates.write(itemEntry)
+
+
+
+googleCoordinates.close()
