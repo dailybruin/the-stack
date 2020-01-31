ba803a7a35eb535f8ff58452c5c0a35f86312388
jcr7467 <juan.rios2@yahoo.com>
2020-01-31 11:33:22 -0800

    updated JS

--- js/posts/ticket-data/index.js
+++ js/posts/ticket-data/index.js
@@ -1,10 +1,4 @@
-// This example requires the Visualization library. Include the libraries=visualization
-  // parameter when you first load the API. For example:
-  // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
-
-
-
-
+  var config = {response: true};
 
   var map, heatmap;
 
