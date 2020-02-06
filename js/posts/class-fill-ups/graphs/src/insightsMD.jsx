9970610c00b7785fff3e4fa7ada61ea6016cbf8f
Andrew <andrewzkan@gmail.com>
2020-02-05 20:29:42 -0800

    added timeline graph

--- js/posts/class-fill-ups/graphs/src/insightsMD.jsx
+++ js/posts/class-fill-ups/graphs/src/insightsMD.jsx
@@ -18,23 +18,56 @@ const graphSize =
   screenScale * screen.width > 1000 ? 1000 : screenScale * screen.width;
 
 /* "fields": ["dept", "class_full", "total_classes", "pct_full"] */
+//classes full per department
 let DEPARTMENTSCLASS = [];
+//above but spliced
 let TOPCLASS = [];
+//% classes full per department
 let DEPARTMENTSPCT = [];
+//above but spliced
 let TOPPCT = [];
+//total classes per department
 let DEPARTMENTSIZE = [];
 let classSlice = graphSize == 1000 ? 30 : 20;
+//timeline data
 let TIMELINE = [];
+const LAST_TIME = 299;
+//timeline data json'ified
+let TIMEDATA = [];
+//tick times
+let COMPRESSEDTIMES = [];
+//dates
+let DATES = [];
+
+//same as other file, used for creating label lines
+const YEARS = ["Junior", "Sophomore", "Freshman"];
+const FIRST_PASS_DATES = [58, 100, 122];
+const SECOND_PASS_DATES = [226, 268, 290];
+const SECOND_PASS_DATE = 166;
+const PASS_DATES = FIRST_PASS_DATES.concat(SECOND_PASS_DATE, SECOND_PASS_DATES);
+let academicYearLines = [];
+
+/* tick formatter for the X axis labels */
+function tickFormatter(t) {
+  return (
+    <tspan>
+      <tspan x="0" dy="1em">
+        {DATES[t].split(" ")[0]} {DATES[t].split(" ")[1]}
+      </tspan>
+    </tspan>
+  );
+}
 
 class Insights extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: 4,
+      loading: 5,
       isMobile: graphSize == screenScale * screen.width ? true : false,
       graphSize: graphSize,
       classHoveredClass: 10,
-      classHoveredPct: 10
+      classHoveredPct: 10,
+      timeHovered: 150
     };
   }
 
@@ -129,9 +162,22 @@ class Insights extends React.Component {
         });
         text = JSON.parse(text);
         TIMELINE = text;
-        for (let i = 0; i < 299; i++) {
-          console.log(TIMELINE[i] + "," + i);
+        if (TIMEDATA.length != LAST_TIME) {
+          for (let i = 0; i < LAST_TIME; i++) {
+            TIMEDATA.push({ x: i, y: TIMELINE[i] });
+          }
         }
+        if (this._isMounted) this.dataLoaded();
+      });
+  }
+
+  fetchDates() {
+    fetch("../../../../datasets/class-fill-ups/dates.json")
+      .then(res => {
+        return res.json();
+      })
+      .then(text => {
+        DATES = text;
         if (this._isMounted) this.dataLoaded();
       });
   }
@@ -142,14 +188,117 @@ class Insights extends React.Component {
     const fetchPct = this.fetchPct();
     const fetchSize = this.fetchSize();
     const fetchTimeline = this.fetchTimeline();
-    Promise.all([fetchClass, fetchPct, fetchSize, fetchTimeline]);
+    const fetchDates = this.fetchDates();
+    Promise.all([fetchClass, fetchPct, fetchSize, fetchTimeline, fetchDates]);
     window.addEventListener("resize", this.updateDimensions.bind(this));
   }
 
   dataLoaded() {
-    let { loading } = this.state;
+    let { loading, isMobile } = this.state;
     loading = loading - 1;
     this.setState({ loading: loading });
+
+    if (loading == 0) {
+      if (!isMobile) {
+        for (let i = 0; i < LAST_TIME / 24; i++) {
+          COMPRESSEDTIMES.push(24 * i);
+        }
+      } else {
+        for (let i = 0; i < 6; i++) {
+          COMPRESSEDTIMES.push(48 * i);
+        }
+      }
+
+      for (let i = 0; i < YEARS.length; i++) {
+        academicYearLines.push(
+          <LineSeries
+            key={"firstPass" + YEARS[i]}
+            className={"firstPass" + YEARS[i]}
+            color="gray"
+            style={{
+              strokeDasharray: "2 2"
+            }}
+            data={[
+              { x: FIRST_PASS_DATES[i], y: 0 },
+              { x: FIRST_PASS_DATES[i], y: 550 }
+            ]}
+          />
+        );
+        academicYearLines.push(
+          <LabelSeries
+            key={"firstPass" + YEARS[i] + "Label"}
+            className={"firstPass" + YEARS[i] + "Label"}
+            labelAnchorX="middle"
+            style={{ opacity: 0.6 }}
+            data={[
+              {
+                x: FIRST_PASS_DATES[i],
+                y: 400 - 30 * i,
+                label: YEARS[i]
+              }
+            ]}
+          />
+        );
+        academicYearLines.push(
+          <LineSeries
+            key={"secondPass" + YEARS[i]}
+            className={"secondPass" + YEARS[i]}
+            color="gray"
+            style={{
+              strokeDasharray: "2 2"
+            }}
+            data={[
+              { x: SECOND_PASS_DATES[i], y: 0 },
+              { x: SECOND_PASS_DATES[i], y: 550 }
+            ]}
+          />
+        );
+        academicYearLines.push(
+          <LabelSeries
+            key={"secondPass" + YEARS[i] + "Label"}
+            className={"secondPass" + YEARS[i] + "Label"}
+            labelAnchorX="middle"
+            style={{ opacity: 0.6 }}
+            data={[
+              {
+                x: SECOND_PASS_DATES[i],
+                y: 400 - 30 * 4 - 30 * i,
+                label: YEARS[i]
+              }
+            ]}
+          />
+        );
+      }
+      academicYearLines.push(
+        <LineSeries
+          key="secondPass"
+          className="secondPass"
+          color="gray"
+          style={{
+            strokeDasharray: "2 2"
+          }}
+          data={[
+            { x: SECOND_PASS_DATE, y: 0 },
+            { x: SECOND_PASS_DATE, y: 550 }
+          ]}
+        />
+      );
+      academicYearLines.push(
+        <LabelSeries
+          key="secondPassLabel"
+          className="secondPassLabel"
+          labelAnchorX="middle"
+          style={{ opacity: 0.6 }}
+          data={[
+            {
+              x: SECOND_PASS_DATE,
+              y: 310,
+              label: "Second Pass"
+            }
+          ]}
+        />
+      );
+    }
   }
 
   componentWillUnmount() {
@@ -160,14 +309,13 @@ class Insights extends React.Component {
   /* used to track mouse position for the hint*/
   _onNearestXCLASS = (value, { index }) => {
     this.setState({ classHoveredClass: index });
-    //THIS 270 IS HARDCODED IDK WHY WE NEED TO SUBTRACT BY 270
-    //this.setState({ mouseY: window.event.clientY - 270 });
   };
 
   _onNearestXPCT = (value, { index }) => {
     this.setState({ classHoveredPct: index });
-    //THIS 270 IS HARDCODED IDK WHY WE NEED TO SUBTRACT BY 270
-    //this.setState({ mouseY: window.event.clientY - 270 });
+  };
+  _onNearestXTime = (value, { index }) => {
+    this.setState({ timeHovered: index });
   };
 
   render() {
@@ -176,16 +324,19 @@ class Insights extends React.Component {
       graphSize,
       classHoveredClass,
       classHoveredPct,
-      isMobile
+      isMobile,
+      timeHovered
     } = this.state;
     const BarSeries = VerticalBarSeries;
     let classValue = TOPCLASS[classHoveredClass];
     let pctValue = TOPPCT[classHoveredPct];
+    let timeValue = TIMEDATA[timeHovered];
 
     return loading > 0 ? (
       <h1>Graph loading</h1>
     ) : (
       <div>
+        {/* graph of top departments full by classes */}
         {isMobile ? (
           <h4>{classSlice} Most Full Departments by Classes</h4>
         ) : (
@@ -198,7 +349,6 @@ class Insights extends React.Component {
           width={graphSize}
           height={graphSize / 2}
           xDistance={100}
-          style={{ marginBottom: 200 }}
           margin={{ bottom: isMobile ? 70 : 150 }}
         >
           <HorizontalGridLines />
@@ -210,7 +360,7 @@ class Insights extends React.Component {
               className="alt-x-label"
               includeMargin={false}
               xPercent={0.018}
-              yPercent={1.2}
+              yPercent={1.21}
               style={{
                 fontWeight: "bold"
               }}
@@ -219,7 +369,7 @@ class Insights extends React.Component {
           <BarSeries
             className="Insights"
             data={TOPCLASS}
-            color="#3c59ff"
+            color="#2D898B"
             onNearestX={this._onNearestXCLASS}
           />
           <Hint
@@ -237,6 +387,7 @@ class Insights extends React.Component {
           ></Hint>
         </XYPlot>
 
+        {/* graph of top classes full by % */}
         {isMobile ? (
           <h4>
             {classSlice} Most Full Departments by Percentage (> 5 classes)
@@ -271,7 +422,7 @@ class Insights extends React.Component {
           <BarSeries
             className="Insights"
             data={TOPPCT}
-            color="lightBlue"
+            color="#88CCF1"
             onNearestX={this._onNearestXPCT}
           />
           <Hint
@@ -284,6 +435,55 @@ class Insights extends React.Component {
               return [
                 { title: point.x, value: point.y + "%" },
                 { title: "Total Classes", value: dep.y }
+              ];
+            }}
+          ></Hint>
+        </XYPlot>
+
+        {/* timeline of # of classes full */}
+        {isMobile ? (
+          <h4>Timeline of Classes Filling Up</h4>
+        ) : (
+          <h2 style={{ marginLeft: graphSize / 3 }}>
+            Timeline of Classes Filling Up
+          </h2>
+        )}
+        <XYPlot
+          xType="ordinal"
+          width={graphSize}
+          height={graphSize / 2}
+          xDistance={100}
+          margin={{ bottom: isMobile ? 60 : 90 }}
+        >
+          <HorizontalGridLines />
+          <VerticalGridLines tickValues={COMPRESSEDTIMES} />
+          <XAxis tickValues={COMPRESSEDTIMES} tickFormat={tickFormatter} />
+          <YAxis />
+          {isMobile ? null : (
+            <ChartLabel
+              text="Time Passed"
+              className="alt-x-label"
+              includeMargin={false}
+              xPercent={0.018}
+              yPercent={1.12}
+              style={{
+                fontWeight: "bold"
+              }}
+            />
+          )}
+          <LineSeries
+            className="Insights"
+            data={TIMEDATA}
+            color="#219CCF"
+            onNearestX={this._onNearestXTime}
+          />
+          {isMobile || loading > 0 ? null : academicYearLines}
+          <Hint
+            value={timeValue}
+            format={point => {
+              return [
+                { title: "Time Passed", value: DATES[point.x] },
+                { title: "Classes Filled", value: point.y + " classes" }
               ];
             }}
           ></Hint>
