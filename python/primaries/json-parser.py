21a11d8aba812088b47fb271b5a9ccacaf7ea961
Radhika Ahuja <ahujaradhika@g.ucla.edu>
2020-02-08 17:46:33 -0800

    added donald trump's tweet dataset

--- python/primaries/json-parser.py
+++ python/primaries/json-parser.py
@@ -1,6 +1,7 @@
 import json
+import requests
 
-candidate_list = [
+CANDIDATES = [
     'joe-biden', 
     'elizabeth-warren', 
     'bernie-sanders', 
@@ -10,14 +11,54 @@ candidate_list = [
     'andrew-yang',
     'donald-trump']
 
-candidate_dict = dict.fromkeys(candidate_list)
-print(candidate_dict)
+# NOTE: Change path before use!
+PATH = 'C:/Users/ahuja/Desktop/Clubs/stack/the-stack/datasets/primaries/'
 
-for i in candidate_list:
-    tweets_unparsed_data = open('/datasets/primaries/candidates-json/' + i + '.json', 'rb')
-    candidate_dict[i] = json.load(tweets_unparsed_data)
+# parse tweet json dataset
+def json_parser():
 
-for key in candidate_dict.keys():
-    print (key.capitalize())
-    for i in range(len(candidate_dict[key])):
-        print(candidate_dict[key][i]['text'])
+    # dictionary: candidate name -> tweets text
+    candidate_tweets = dict.fromkeys(CANDIDATES)
+
+    for i in CANDIDATES:
+        # open json file
+        unparsed_json = open(PATH + i + '.json', 'rb')
+        parsed_json = json.load(unparsed_json)
+
+        # assign each candidate's tweet text to candidate name 
+        tweet_data = ""
+        for j in range(len(parsed_json)):
+            tweet_data = tweet_data + parsed_json[j]['text']
+        candidate_tweets[i] = tweet_data
+
+    return candidate_tweets
+
+# access candidate's tweets
+def tweets(candidate_name):
+    candidate_tweets = json_parser()
+    return candidate_tweets[candidate_name]
+
+print(tweets('donald-trump'))
+
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+}
+
+params = (
+    ('version', '2017-10-13'),
+    ('consumption_preferences', 'true'),
+    ('raw_scores', 'true'),
+)
+
+
+#TODO: Do we need to remove links?
+#data = open('C:/Users/ahuja/Desktop/Clubs/stack/the-stack/datasets/primaries/andrew-yang.json', 'rb').read()
+#response = requests.post('http://%7Burl%7D/v3/profile', headers=headers, params=params, data=data, auth=('apikey', 'Evef9Le9VD1b4wMKw3N0dfauvn3EQDVydNOcGxjQf9oM'))
+#print(response)
+
+#NB. Original query string below. It seems impossible to parse and
+#reproduce query strings 100% accurately so the one below is given
+#in case the reproduced version is not "correct".
+# response = requests.post('http://{url}/v3/profile?version=2017-10-13&consumption_preferences=true&raw_scores=true', headers=headers, data=data, auth=('apikey', '{apikey}'))
+
