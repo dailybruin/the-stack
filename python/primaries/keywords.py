b1580264bdc6b8a5e58392e173371da293f84b07
Radhika Ahuja <ahujaradhika@g.ucla.edu>
2020-02-20 19:30:51 -0800

    added keywords

--- python/primaries/keywords.py
+++ python/primaries/keywords.py
@@ -2,3 +2,175 @@ def get_keywords() :
 	temp = ['college' , 'higher education', 'daca', 'tuition', 'gun control', 'gun violence', 'healthcare', 'environment', 'climate',
 	'undocumented', 'scholarship', 'student visa', 'immigration', 'travel ban', 'stem', 'opt', 'h1b', 'women\'s rights', 'black lives matter', 'racial equality', 'lgbtq', 'student', 'medicare', 'obamacare', 'insurance', 'gun restriction', 'gun law', 'university', 'regents', 'higher-education', 'obama care', 'obama-care', 'nra', 'shooting', 'school', 'women', 'race', 'racial', 'scholar', 'community college', 'loan', 'junior college', 'abortion', 'reproductive', 'planned parenthood', 'ucla', 'public education', 'education', 'sexual harassment', 'title 9', 'title ix', 'title9', 'titleix', 'sexual assault', 'mental health', 'counseling', 'mental illness', 'domestic violence', 'weed', 'marijuana']
 	return temp
+
+# COLLEGE
+######
+# (Previous)
+# College
+# Higher Education
+# Tuition
+# student
+# university
+# regents
+# school
+# scholar
+# community college
+# loan
+# junior college
+# ucla
+# public education
+# education
+######
+# (New)
+# refinance?
+# public school
+
+
+# EMPLOYMENT
+# employment rate
+# jobs (is also a hashtag)
+# employment
+# wage
+# minimum wage
+# unemployment
+# Higher Wages
+# Lower Unemployment
+
+# IMMIGRATION
+# (Previous)
+# undocumented
+# scholarship
+# student visa
+# immigration
+# travel ban
+# stem
+# opt
+# h1b
+# DACA
+######
+# (New)
+# ICE
+# illegal immigration
+# illegal aliens
+# legal immigration
+# border wall?
+######
+
+# MENTAL HEALTH
+# (Previous)
+# mental health
+# counseling
+# mental illness
+######
+# (New)
+# ptsd
+######
+
+# DISCRIMINATION & EQUALITY
+# (Previous)
+# women's rights
+# black lives matter
+# racial equality
+# lgbtq
+# women
+# race
+# racial
+# sexual harassment
+# title 9
+# sexual assault
+# domestic violence
+######
+# (New)
+# female
+# empowerment
+# strong women
+# equal rights amendment
+# era
+# universal child care?
+# medical
+# doctors
+# human rights?
+# modern day slavery 
+# diverse
+# diversity
+# racism
+######
+
+# HEALTHCARE
+# (Previous)
+# healthcare
+# medicare
+# obamacare
+# insurance
+######
+# (New)
+# health care
+# health
+# health care plan
+# prescription drugs
+# affordable care
+# affordable cara act
+# aca
+# pharmaceuitcals
+# pharma
+# primary care
+# dental 
+# clinics
+# pharmacy
+# urgent care
+######
+
+# ENVIRONMENT
+# (Previous)
+# environment
+# climate
+# carbon neutral
+# carbon
+# fossil fuel
+# renewable energy
+# petrol
+######
+# (New)
+######
+
+# WOMEN'S HEALTH
+# (Previous)
+# women
+# abortion
+# reproductive
+# planned parenthood
+######
+# (New)
+# female
+# contraceptives
+# contraception
+# birth control
+# ReproRights (twitter handle for reproductive rights)
+# NARAL (twitter handle)
+# AllAboveAllAct (twitter handle)
+######
+
+# MARIJUANA
+# (Previous)
+# weed
+# marijuana
+######
+# (New)
+######
+
+# GUN RIGHTS
+# (Previous)
+# gun control
+# gun violence
+# gun restriction
+# gun law
+# nra
+# shooting
+######
+# (New)
+# background checks
+# parkland
+# parkland shooting 
+# gun safety
+######
+
