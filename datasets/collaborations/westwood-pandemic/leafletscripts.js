650211435fe1bffbe63f2d8461b2885ba9ce3f2b
Radhika Ahuja <ahujaradhika@g.ucla.edu>
2020-04-01 14:07:15 -0700

    temp changes

--- datasets/collaborations/westwood-pandemic/leafletscripts.js
+++ datasets/collaborations/westwood-pandemic/leafletscripts.js
@@ -3,6 +3,8 @@ var baseLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10
     maxZoom: 18
 });
 
+var business_lists = [];
+
 var dining_list = [];
 var ae_list = [];
 var shopping_list = [];
@@ -11,82 +13,36 @@ var lon;
 var latlon;
 var popup;
 var layer_lists = [dining_list, ae_list, shopping_list];
-for (var j = 0; j < 3; j++) {
+var layer_jsons = [dining_json, ae_json, shopping_json];
+
+for (var j = 0; j < layer_lists.length; j++) {
     var current_list = layer_lists[j];
-    var len;
-    if (j == 0)
-        len = Object.keys(dining_json).length;
-    else if (j == 1)
-        len = Object.keys(ae_json).length;
-    else
-        len = Object.keys(shopping_json).length;
-    for (var i = 0; i < len; i++) {
-        if (j == 0) {
-            var str1 = "";
-            //console.log(dining_json);
-            console.log(i);
-            if (dining_json[i].open == true) {
-                str1 = "Open\n";
-            }
-            else {
-                str1 = "Closed\n";
-            }
-            //console.log(dining_json[i]['geo']['latitude']);
-            lat = parseFloat(dining_json[i]['geo']['latitude']);
-            lon = parseFloat(dining_json[i]['geo']['longitude']);
-            latlon = L.latLng(lat,lon);
-            //console.log(latlon);
-            var name = dining_json[i].name;
-            popup = name.concat("<br>", str1, dining_json[i].status);
-        }
-        else if (j == 1) {
-            var str1 = "";
-            //console.log(dining_json);
-            console.log(i);
-            if (ae_json[i].open == true) {
-                str1 = "Open\n";
-            }
-            else {
-                str1 = "Closed\n";
-            }
-            //console.log(dining_json[i]['geo']['latitude']);
-            lat = parseFloat(ae_json[i]['geo']['latitude']);
-            lon = parseFloat(ae_json[i]['geo']['longitude']);
-            latlon = L.latLng(lat,lon);
-            //console.log(latlon);
-            var name = ae_json[i].name;
-            popup = name.concat("<br>", str1, ae_json[i].status);
+    var current_json = layer_jsons[j];
+    for (var i = 0; i < layer_jsons.length; i++) {
+        var str1 = ""
+        if (current_json[i].open == true) {
+            str1 = "Open";
         }
         else {
-            var str1 = "";
-            //console.log(dining_json);
-            console.log(i);
-            if (shopping_json[i].open == true) {
-                str1 = "Open\n";
-            }
-            else {
-                str1 = "Closed\n";
-            }
-            //console.log(dining_json[i]['geo']['latitude']);
-            lat = parseFloat(shopping_json[i]['geo']['latitude']);
-            lon = parseFloat(shopping_json[i]['geo']['longitude']);
-            latlon = L.latLng(lat,lon);
-            //console.log(latlon);
-            var name = shopping_json[i].name;
-            popup = name.concat("<br>", str1, shopping_json[i].status);
+            str1 = "Closed";
         }
-        current_list.push((L.marker(L.latLng(lat, lon))).bindPopup(popup));
+        lat = parseFloat(current_json[i]['geo']['latitude']);
+        lon = parseFloat(current_json[i]['geo']['longitude']);
+        latlon = L.latLng(lat,lon);
+        //console.log(latlon);
+        var name = current_json[i].name;
+        popup = name.concat("<br>", str1, current_json[i].status);
     }
+    business_lists[j].push((L.marker(L.latLng(lat, lon))).bindPopup(popup));
 }
 
-//console.log(dining_list);
 var dining_layer = L.layerGroup(dining_list);
 var ae_layer = L.layerGroup(ae_list);
 var shopping_layer = L.layerGroup(shopping_list);
 var overlay_maps = {
-    "Dining": dining_layer,
-    "Shopping": shopping_layer,
-    "Arts and Entertainment": ae_layer
+    "Dining": business_lists[0],
+    "Shopping": business_lists[1],
+    "Arts and Entertainment": business_lists[2]
 }
 
 var mymap = L.map('mapid', {
@@ -94,7 +50,7 @@ var mymap = L.map('mapid', {
     zoom: 15,
     minZoom: 16,
     maxBounds : [[34.064519, -118.450942], [34.058672, -118.440260]],
-    layers: [dining_layer]
+    layers: business_lists[0]
 });
 
 mymap.addLayer(baseLayer);
