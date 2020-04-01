bf1c1b536676c76fbd98681915e37dab33f77998
mattiesansev <mesansev@gmail.com>
2020-03-31 22:14:19 -0700

    pins work

--- datasets/collaborations/westwood-pandemic/leafletscripts.js
+++ datasets/collaborations/westwood-pandemic/leafletscripts.js
@@ -5,25 +5,33 @@ var baseLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10
 });
 
 var dining_list = [];
-for (var i = 0; i < 90; i++) {
+for (var i = 0; i < 87; i++) {
     var str1 = "";
-    if (dining[i].open == true) {
+    //console.log(dining_json);
+    console.log(i);
+    if (dining_json[i].open == true) {
         str1 = "Open\n";
     }
     else {
         str1 = "Closed\n";
     }
-    var popup = str1.concat(dining[i].status);
-    dining_list.push(L.marker([dining[i].latitude, dining[i].longitude])).bindPopup(popup);
+    //console.log(dining_json[i]['geo']['latitude']);
+    var lat = parseFloat(dining_json[i]['geo']['latitude']);
+    var lon = parseFloat(dining_json[i]['geo']['longitude']);
+    var latlon = L.latLng(lat,lon);
+    console.log(latlon);
+    var popup = str1.concat(dining_json[i].status);
+    dining_list.push((L.marker(L.latLng(lat, lon))).bindPopup(popup));
 }
 var dining_layer = L.layerGroup(dining_list);
 
 var mymap = L.map('mapid', {
     center: [34.060716, -118.445345],
-    zoom: 12,
-    minZoom: 15,
-    maxBounds : [[34.063644, -118.449161], [34.055600, -118.437968]],
+    zoom: 15,
+    minZoom: 16,
+    maxBounds : [[34.064519, -118.450942], [34.058672, -118.440260]],
     layers: [dining_layer]
 });
 
 mymap.addLayer(baseLayer);
+L.control.layers(dining_layer).addTo(mymap);
