13854c8fdd57a085095b4a7bc064bc14b8b911ae
Radhika Ahuja <ahujaradhika@g.ucla.edu>
2020-04-01 14:23:16 -0700

    cleaned up code

--- datasets/collaborations/westwood-pandemic/leafletscripts.js
+++ datasets/collaborations/westwood-pandemic/leafletscripts.js
@@ -1,57 +1,68 @@
-var baseLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVycnlsaW5ldyIsImEiOiJjajIwc2E1YXkwMmt6MzNuMXZnaWRjb2lhIn0.u0AHZNQd8_8hFmpaVvI1nQ', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18
+var greenIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
 });
 
-var business_lists = [];
+var blackIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
 
-var dining_list = [];
-var ae_list = [];
-var shopping_list = [];
-var lat;
-var lon;
-var latlon;
-var popup;
-var layer_lists = [dining_list, ae_list, shopping_list];
-var layer_jsons = [dining_json, ae_json, shopping_json];
+var baseLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVycnlsaW5ldyIsImEiOiJjajIwc2E1YXkwMmt6MzNuMXZnaWRjb2lhIn0.u0AHZNQd8_8hFmpaVvI1nQ', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    minZoom: 5,
+});
 
-for (var j = 0; j < layer_lists.length; j++) {
-    var current_list = layer_lists[j];
-    var current_json = layer_jsons[j];
-    for (var i = 0; i < layer_jsons.length; i++) {
-        var str1 = ""
-        if (current_json[i].open == true) {
-            str1 = "Open";
-        }
+var layer_lists = [dining_json, shopping_json, ae_json];
+var business_layers = [];
+
+for (let j = 0; j < layer_lists.length; j++) {
+    var business_list = [];
+    for (let i = 0; i < layer_lists[j].length; i++) {
+        var str1 = "";
+        if (layer_lists[j][i].open == true) {
+            str1 = "<br> <em>Open</em> <br>";
+        }  
         else {
-            str1 = "Closed";
+            str1 = "<br><em>Closed</em><br>";
         }
-        lat = parseFloat(current_json[i]['geo']['latitude']);
-        lon = parseFloat(current_json[i]['geo']['longitude']);
-        latlon = L.latLng(lat,lon);
-        //console.log(latlon);
-        var name = current_json[i].name;
-        popup = name.concat("<br>", str1, current_json[i].status);
+        var lat = parseFloat(layer_lists[j][i]['geo']['latitude']);
+        var lon = parseFloat(layer_lists[j][i]['geo']['longitude']);
+        var latlon = L.latLng(lat,lon);
+    
+        var name = "<b style='font-size:14px'>" + layer_lists[j][i].name + "</b>";
+        var popup = name.concat("\n", str1, layer_lists[j][i].status);
+        if (layer_lists[j][i].open == true)
+            business_list.push((L.marker(L.latLng(lat, lon), {icon: greenIcon})).bindPopup(popup));
+        else
+            business_list.push((L.marker(L.latLng(lat, lon), {icon: blackIcon})).bindPopup(popup));
     }
-    business_lists[j].push((L.marker(L.latLng(lat, lon))).bindPopup(popup));
+    business_layers.push(L.layerGroup(business_list));
 }
 
-var dining_layer = L.layerGroup(dining_list);
-var ae_layer = L.layerGroup(ae_list);
-var shopping_layer = L.layerGroup(shopping_list);
-var overlay_maps = {
-    "Dining": business_lists[0],
-    "Shopping": business_lists[1],
-    "Arts and Entertainment": business_lists[2]
-}
-
+// default layer
 var mymap = L.map('mapid', {
     center: [34.060716, -118.445345],
     zoom: 15,
     minZoom: 16,
-    maxBounds : [[34.064519, -118.450942], [34.058672, -118.440260]],
-    layers: business_lists[0]
+   // maxBounds : [[34.064519, -118.450942], [34.058672, -118.440260]],
+    layers: business_layers[0] // default dining
 });
+
+var overlay_maps = {
+    "Dining": business_layers[0],
+    "Shopping": business_layers[1],
+    "Arts and Entertainment": business_layers[2]
+};
 
 mymap.addLayer(baseLayer);
 L.control.layers(overlay_maps).addTo(mymap);
