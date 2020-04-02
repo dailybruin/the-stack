26ad6909c3f5c5dcd8cf50a3a0da15fcf2c67132
Radhika Ahuja <ahujaradhika@g.ucla.edu>
2020-04-01 19:14:46 -0700

    fixed typos, changed styling

--- datasets/collaborations/westwood-pandemic/leafletscripts.js
+++ datasets/collaborations/westwood-pandemic/leafletscripts.js
@@ -16,7 +16,7 @@ var blackIcon = new L.Icon({
     shadowSize: [41, 41]
 });
 
-var baseLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVycnlsaW5ldyIsImEiOiJjajIwc2E1YXkwMmt6MzNuMXZnaWRjb2lhIn0.u0AHZNQd8_8hFmpaVvI1nQ', {
+var baseLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVycnlsaW5ldyIsImEiOiJjajIwc2E1YXkwMmt6MzNuMXZnaWRjb2lhIn0.u0AHZNQd8_8hFmpaVvI1nQ', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     minZoom: 5,
