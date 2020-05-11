a16610eae82b23ea8c21cb1ff9bd49bfe06bf67f
Radhika Ahuja <ahujaradhika@g.ucla.edu>
2020-05-10 21:13:33 -0700

    added connections field, removed edges

--- python/posts/covid-model/sbm/sbm.py
+++ python/posts/covid-model/sbm/sbm.py
@@ -18,16 +18,16 @@ def generate_student_communities():
 
 # INPUT - Generate Probability matrix
 
-# EDGE CASE
-def edge_case(degree, num_of_students):
-    return nx.random_regular_graph(degree, num_of_students, seed=None)
-
-# # SBM
+# GENERAL CASE (SBM)
 # n = [48, 133, 94, 18, 456, 58, 47, 211, 49, 187, 293, 328, 1262, 323, 1270, 219, 22, 184, 166, 477, 55, 67, 136, 673, 2669, 366, 831, 53, 2027, 87, 188, 30, 259, 5, 33, 192, 709, 216, 628, 77, 689, 254, 5, 13, 207, 241, 5, 5, 364, 143, 1612, 557, 272, 799, 872, 206, 36, 36, 797, 194, 128, 514, 308, 1941, 3778, 46, 5, 12, 1473, 142, 496, 383, 24, 157, 266, 221]
 # p = [[0.5, 0.2],
 #      [0.2, 0.05]]
 
 # g = nx.stochastic_block_model(n, p, seed=0)
+
+# EDGE CASE
+def edge_case(degree, num_of_students):
+    return nx.random_regular_graph(degree, num_of_students, seed=None)
 
 # OUTFILE
 def generate_nodes_and_edges(g, path):
@@ -41,17 +41,11 @@ def generate_nodes_and_edges(g, path):
             "id": node,
             "status": 0,
             "length": 0,
-            # "connections": students[key]['connections']
-        })
-
-    for edge in g.edges():
-        graph["links"].append({
-            "source": edge[0],
-            "target": edge[1],
-            "weight": 1
+            "connections": list(g.__getitem__(node))
         })
 
     with open(path, 'w') as outfile:
         json.dump(graph, outfile)
 
 generate_nodes_and_edges(edge_case(10, data.MAX_STUDENTS), "datasets/covid-model/best_case.json")
+
