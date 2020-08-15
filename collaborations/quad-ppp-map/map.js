f5d8151f291f473032506347361b8211bd73c0bf
Andrew <andrewzkan@gmail.com>
2020-08-15 15:15:47 -0700

    use leaflet instead of mapbox

--- collaborations/quad-ppp-map/map.js
+++ collaborations/quad-ppp-map/map.js
@@ -1,31 +1,17 @@
-mapboxgl.accessToken =
-  'pk.eyJ1Ijoia2FuZHJld3oiLCJhIjoiY2tkdzR5cHh1MWxybDJ0bnV3NGJranE5dyJ9.8_eKzn7R-rcIWyjOFYq5Zg';
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [12.550343, 55.665957],
-  zoom: 8,
-});
+var mymap = L.map('mapid').setView([34.060716, -118.445345], 15);
 
-var marker = new mapboxgl.Marker().setLngLat([12.550343, 55.665957]).addTo(map);
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      'pk.eyJ1Ijoia2FuZHJld3oiLCJhIjoiY2tkdzR5cHh1MWxybDJ0bnV3NGJranE5dyJ9.8_eKzn7R-rcIWyjOFYq5Zg',
+  }
+).addTo(mymap);
 
-var markerLayer = mapbox.markers.layer();
-var interaction = mapbox.markers.interaction(markerLayer);
-interaction.formatter(function(feature) {
-  console.log(feature);
-  var o = '<strong>' + feature.properties.title + '</strong>';
-  return o;
-});
-$.get('./westwood_ppp_small_cleaned.csv', function(data) {
-  var crimes = $.csv.toObjects(data);
-  $.each(crimes, function(i, crime) {
-    var newfeature = {
-      geometry: { coordinates: [crime.lon, crime.lat] },
-      properties: {
-        title: crime.title,
-      },
-    };
-    markerLayer.add_feature(newfeature);
-    map.addLayer(markerLayer);
-  });
-});
+var marker = L.marker([34.060716, -118.445345]).addTo(mymap);
