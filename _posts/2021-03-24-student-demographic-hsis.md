b3630e5df0bb66d1eeef7b55e05d4caa6844877e
Samantha Low <samlow88@g.ucla.edu>
2021-03-24 20:46:53 -0700

    added basic markdown for student demographis hsis

---
+++ _posts/2021-03-24-student-demographic-hsis.md
@@ -0,0 +1,25 @@
+---
+title: Student Demographics HSIs article
+teaser: 
+authors:
+  - 
+key_takeaways:
+  - 
+featured_image:
+  url: 
+og_image: 
+stylesheets:
+  - //cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css
+scripts:
+  - //cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js
+  - //ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
+  - //d3js.org/d3.v6.min.js
+  - //cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0
+  - /js/posts/student-demographic-hsis/graduation-rates.js
+---
+hi this is a test sfsgljgpsgjs;gs;;a
+
+<div id="line-chart-wrapper">
+  <canvas id="grad-rate-line"></canvas>
+</div>
+
---
+++ js/posts/student-demographic-hsis/graduation-rates.js
@@ -0,0 +1,18 @@
+let ctx = document.getElementById('grad-rate-line');
+    //var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 00, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+    // Configuration options go here
+    options: {}
+});
