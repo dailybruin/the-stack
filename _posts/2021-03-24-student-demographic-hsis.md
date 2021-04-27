fcc18ab33a9b75b61fc4c304c37b9f8494a3338c
Samantha Low <samlow88@g.ucla.edu>
2021-04-27 01:14:10 -0700

    grad rrate graphs

--- _posts/2021-03-24-student-demographic-hsis.md
+++ _posts/2021-03-24-student-demographic-hsis.md
@@ -17,12 +17,26 @@ scripts:
   - //cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js
   - //ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
   - //d3js.org/d3.v6.min.js
+  - //cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-annotation/0.5.5/chartjs-plugin-annotation.min.js
   - //cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0
-  - /js/posts/student-demographic-hsis/graduation-rates.js
+  - //cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js
+  - /js/posts/student-demographic-hsis/graduation-rates.js 
+  - /js/posts/student-demographic-hsis/graduation-rates-change.js
+  
 ---
 hi this is a test sfsgljgpsgjs;gs;;a
 
-<div id="line-chart-wrapper">
+plug in script in case anyone wants it:  - //cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0
+
+### The Story in Graduation Rates
+<div id="chartContainer1">
   <canvas id="grad-rate-line"></canvas>
 </div>
+
+<div id="chartContainer2">
+  <canvas id="grad-rate-change"></canvas>
+  <canvas id="LineWithLine1" width="600" height="400"></canvas>
+</div>
+sodfisdfisd;fsdl;fs
+
 
--- js/posts/student-demographic-hsis/graduation-rates.js
+++ js/posts/student-demographic-hsis/graduation-rates.js
@@ -1,18 +1,154 @@
-let ctx = document.getElementById('grad-rate-line');
-    //var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
+
+let ctx1 = document.getElementById('grad-rate-line').getContext("2d");
+    //let ctx = document.getElementById('myChart').getContext('2d');
+    let chart = new Chart(ctx1, {
     // The type of chart we want to create
     type: 'line',
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 00, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            label: 'UCLA',
+            fill: false,
+            borderColor: '#3284BF',
+            backgroundColor: '#3284BF',
+            data: [85.20, 87.60, 83.50, 85.00, 87.90, 86.50, 86.40, 85.80, 86.80], 
+        },{
+            
+            label: 'UCI',
+            fill: false,
+            borderColor: '#FFD200',
+            backgroundColor: '#FFD200',
+            data: [79.20, 78.90, 78.30, 81.90, 83.70, 79.90, 77.40, 75.80, 78.50],
+
+        },{
+            
+            label: 'UCSB',
+            fill: false,
+            borderColor: '#004D9F',
+            backgroundColor: '#004D9F',
+            data: [73.30, 73.50, 75.70, 78.30, 77.80, 79.60, 76.80, 78.40, 80.90],
+
+        },{
+
+            label: 'UC Merced',
+            fill: false,
+            borderColor: '#0091B3',
+            backgroundColor: '#0091B3',
+            data: [54.40, 58.60, 61.00, 62.20, 66.90, 63.50, 57.40, 63.40, 67.70],
+
+        },{
+            
+            label: 'UC Davis',
+            fill: false,
+            borderColor: '#B3A369',
+            backgroundColor: '#B3A369',
+            data: [70.90, 71.60, 71.70, 77.90, 76.90, 76.20, 77.00, 80.20, 80.30],
+
+        },{
+            
+            label: 'UCR',
+            fill: false,
+            borderColor: '#add8e6',
+            backgroundColor: '#add8e6',
+            data: [65.10, 57.90, 62.40, 66.20, 69.50, 70.10, 72.80, 74.80, 74.60],
+
+        },{
+            
+            label: 'UCSD',
+            fill: false,
+            borderColor: '#C69214',
+            backgroundColor: '#C69214',
+            data: [75.90, 78.90, 74.10, 76.80, 77.90, 81.50, 76.40, 76.40, 82.20],
+        },{
+
+            label: 'UCSC',
+            fill: false,
+            borderColor: '#f29813',
+            backgroundColor: '#f29813',
+            data: [71.80, 69.80, 67.70, 71.50, 75.00, 73.40, 73.50, 70.10, 72.40],
+
+        },{
+
+            label: 'UC Berkeley',
+            fill: false,
+            borderColor: '#041E42',
+            backgroundColor: '#041E42',
+            data: [81.20, 80.80, 84.30, 84.70, 84.80, 88.70, 87.30, 85.30, 84.00],
+
+
+
         }]
     },
     // Configuration options go here
-    options: {}
+  
+    options: { 
+        title: {
+           display: true,
+           text: 'UC Hispanic Graduation Rates From 2011-2019 by Institution '
+       },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    callback: function(value) {
+                      return (value ) + '%'; // convert it to percentage
+                    }
+                    },
+
+                scaleLabel: {
+                    display: true, 
+                    labelString: 'Hispanic Graduation Rate'
+                }
+            }], 
+            xAxes: [{
+                scaleLabel: {
+                    display: true, 
+                    labelString: 'Year'
+                }
+            }], 
+            plugins: false
+        }, 
+
+            tooltips: {
+                callback: function (value) {
+                    label: return (value) + '%'
+                    }
+                   
+    
+          }
+           
+        }
+      
+       },
+);
+    
+
+
+var originalLineDraw = Chart.controllers.line.prototype.draw;
+Chart.helpers.extend(Chart.controllers.line.prototype, {
+  draw: function() {
+    originalLineDraw.apply(this, arguments);
+
+    var chart = this.chart;
+    var ctx = chart.chart.ctx;
+
+    var index = chart.config.data.lineAtIndex;
+    if (index) {
+      var xaxis = chart.scales['x-axis-0'];
+      var yaxis = chart.scales['y-axis-0'];
+
+      ctx.save();
+      ctx.beginPath();
+      ctx.moveTo(xaxis.getPixelForValue(undefined, index), yaxis.top);
+      ctx.strokeStyle = '#ff0000';
+      ctx.lineTo(xaxis.getPixelForValue(undefined, index), yaxis.bottom);
+      ctx.stroke();
+      ctx.restore();
+    }
+  }
 });
+
+
+
+
+
