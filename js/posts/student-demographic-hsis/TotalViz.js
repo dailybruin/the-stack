7354b4942f6fe520a70f2b24f818b2a9d3aa2de4
lindseyp314 <lindseyp314@ucla.edu>
2021-04-29 22:40:11 -0600

    added grant charts to markdown file

---
+++ js/posts/student-demographic-hsis/TotalViz.js
@@ -0,0 +1,81 @@
+
+
+let TotalBerkeley = [15034.02075, 15165.52833, 17505.63043, 16980.76499, 16955.38212, 16919.53482, 17564.38678, 16537.03714, 16193.89056, 20030.81283, 16975.2556, 18591.00817, 18829.23795];
+let TotalDavis = [10162.16773, 14275.57188, 15395.14767, 16137.71437, 18051.55177, 17944.07685, 20077.63721, 19728.08239, 18531.80706, 20640.22483, 20380.74396, 20436.61203, 22638.40125, 23323.4978, 27317.4188];
+let TotalIrvine = [5600.26376, 763.25496, 9052.50894, 8767.10513, 9266.63395, 9186.23442, 8551.08936, 8328.40815, 8502.35534, 8139.72865, 10934.49301, 10351.06324, 9917.92628, 12034.76452, 14226.11396];
+let TotalLosAngeles = [13790.54815, 20694.16299, 20107.84641, 21775.13723, 23149.21027, 24111.86214, 22808.83836, 19699.0309, 21823.46605, 23285.12226, 24178.56352, 23889.91141, 20627.37883, 28539.60817, 32227.99252];
+let TotalMerced = [559.42681, 1330.58246, 1776.18831, 2507.71527, 2190.75653, 1537.026, 1906.92811, 1915.98237, 3441.94054, 2891.35594, 2667.70939, 3864.75901, 2812.39505, 5774.05493, 5026.20934];
+let TotalRiverside = [2232.3638, 3838.88222, 4076.53701, 3487.64078, 4505.87701, 3646.17736, 4423.85638, 3665.20715, 4378.71249, 4879.50816, 5420.11688, 5755.42424, 6292.18276, 6943.3402, 9243.14381];
+let TotalSanDiego = [12880.04159, 18591.28748, 21534.34312, 22973.26776, 27567.62781, 25300.68918, 26347.7452, 25968.11666, 28226.96303, 26397.0134, 28033.0119, 28480.28999, 29316.33976, 33900.67144, 31934.78947];
+let TotalSantaCruz = [4089.69959, 4283.62062, 5146.20989, 4734.38497, 5618.29159, 4834.25257, 5518.98491, 5456.78464, 5264.39424, 5253.61473, 4699.46382, 3940.52808, 5754.08851, 6302.77772, 6012.85373];
+let TotalSantaBarbara = [6394.93157, 8828.87422, 9980.21227, 8959.19216, 11507.49236, 9459.98199, 11180.31466, 8491.73192, 10660.74279, 9556.21791, 9443.97989, 9429.65169, 10750.52119, 11058.03647, 12051.52026];
+let TotalDataNames = [TotalBerkeley, TotalDavis, TotalIrvine, TotalLosAngeles, TotalMerced, TotalRiverside, TotalSanDiego, TotalSantaCruz, TotalSantaBarbara]
+
+let Totaldata = []
+for (let i = 0; i < schools.length; ++i) {
+    chartdata = {
+        label: schools[i],
+        fill: false,
+        data: TotalDataNames[i],
+        backgroundColor: [
+            colors[i],
+        ],
+        borderColor: [
+            colors[i],
+        ],
+        borderWidth: 1,
+        hidden: TotalDataNames[i] != TotalLosAngeles,
+        lineTension: 0
+    }
+    Totaldata.push(chartdata);
+};
+
+var ctxTotal = document.getElementById('TotalChart');
+var myChart = new Chart(ctxTotal, {
+    type: 'line',
+    data: {
+        labels: YearLabels,
+        datasets: Totaldata,
+    },
+    options: {
+        title: {
+            display: true,
+            test: "Total Grant Money by Campus per Student"
+        },
+        scales: {
+            yAxes: {
+                min: 0,
+                max: 600,
+                labelString: "Total Grant Money per Student"
+            },
+            xAxes: {
+                display: true,
+                labelString: "Year"
+            }
+        },
+        plugins: {
+            annotation: {
+                annotations: [
+                    {
+                        type: "line",
+                        mode: "vertical",
+                        scaleID: "x-axis-0",
+                        value: "2015-16",
+                        borderColor: "red",
+                        label: {
+                            content: "TODAY",
+                            enabled: true,
+                            position: "top"
+                        }
+                    }]
+            }
+        }
+    },
+    lineAtIndex: [
+        { index: 5.5, text: ['2012', 'UCSC becomes', 'a HSI'] },
+        { index: 8.5, text: ['2015', 'UCSB becomes', 'a HSI'] },
+        { index: 10.5, text: ['2017', 'UCI becomes', 'a HSI'] },
+    ],
+})
+
+
--- js/posts/student-demographic-hsis/graduation-rates.js
+++ js/posts/student-demographic-hsis/graduation-rates.js
@@ -1,29 +1,22 @@
-<<<<<<< HEAD
 
 let ctx1 = document.getElementById('grad-rate-line').getContext("2d");
-    //let ctx = document.getElementById('myChart').getContext('2d');
-    let chart = new Chart(ctx1, {
-=======
-let ctx = document.getElementById('grad-rate-line');
-//var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
->>>>>>> 1327cd894b19621f34a16d89822d3f96262ced2c
+//let ctx = document.getElementById('myChart').getContext('2d');
+let chart = new Chart(ctx1, {
     // The type of chart we want to create
     type: 'line',
     // The data for our dataset
     data: {
         labels: ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'],
         datasets: [{
-<<<<<<< HEAD
             label: 'UCLA',
             fill: false,
             borderColor: '#3284BF',
             backgroundColor: '#3284BF',
             lineTension: 0,
-            data: [85.20, 87.60, 83.50, 85.00, 87.90, 86.50, 86.40, 85.80, 86.80], 
-            
-        },{
-            
+            data: [85.20, 87.60, 83.50, 85.00, 87.90, 86.50, 86.40, 85.80, 86.80],
+
+        }, {
+
             label: 'UCI',
             fill: false,
             borderColor: '#FFD200',
@@ -31,19 +24,19 @@ var chart = new Chart(ctx, {
             lineTension: 0,
             data: [79.20, 78.90, 78.30, 81.90, 83.70, 79.90, 77.40, 75.80, 78.50],
             hidden: true,
-            
 
-        },{
-            
+
+        }, {
+
             label: 'UCSB',
             fill: false,
             borderColor: '#004D9F',
             backgroundColor: '#004D9F',
             lineTension: 0,
             data: [73.30, 73.50, 75.70, 78.30, 77.80, 79.60, 76.80, 78.40, 80.90],
-           
 
-        },{
+
+        }, {
 
             label: 'UC Merced',
             fill: false,
@@ -52,10 +45,10 @@ var chart = new Chart(ctx, {
             lineTension: 0,
             data: [54.40, 58.60, 61.00, 62.20, 66.90, 63.50, 57.40, 63.40, 67.70],
             hidden: true,
-            
 
-        },{
-            
+
+        }, {
+
             label: 'UC Davis',
             fill: false,
             borderColor: '#B3A369',
@@ -63,10 +56,10 @@ var chart = new Chart(ctx, {
             lineTension: 0,
             data: [70.90, 71.60, 71.70, 77.90, 76.90, 76.20, 77.00, 80.20, 80.30],
             hidden: true,
-            
 
-        },{
-            
+
+        }, {
+
             label: 'UCR',
             fill: false,
             borderColor: '#add8e6',
@@ -74,8 +67,8 @@ var chart = new Chart(ctx, {
             lineTension: 0,
             data: [65.10, 57.90, 62.40, 66.20, 69.50, 70.10, 72.80, 74.80, 74.60],
 
-        },{
-            
+        }, {
+
             label: 'UCSD',
             fill: false,
             borderColor: '#C69214',
@@ -83,8 +76,8 @@ var chart = new Chart(ctx, {
             lineTension: 0,
             data: [75.90, 78.90, 74.10, 76.80, 77.90, 81.50, 76.40, 76.40, 82.20],
             hidden: true,
-            
-        },{
+
+        }, {
 
             label: 'UCSC',
             fill: false,
@@ -93,9 +86,9 @@ var chart = new Chart(ctx, {
             lineTension: 0,
             data: [71.80, 69.80, 67.70, 71.50, 75.00, 73.40, 73.50, 70.10, 72.40],
             hidden: true,
-            
 
-        },{
+
+        }, {
 
             label: 'UC Berkeley',
             fill: false,
@@ -105,88 +98,82 @@ var chart = new Chart(ctx, {
 
             data: [81.20, 80.80, 84.30, 84.70, 84.80, 88.70, 87.30, 85.30, 84.00],
             hidden: true,
-           
 
 
-=======
-            label: 'My First dataset',
-            backgroundColor: 'rgb(100, 25, 142)',
-            borderColor: 'rgb(255, 100, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
->>>>>>> 1327cd894b19621f34a16d89822d3f96262ced2c
+
         }]
     },
     // Configuration options go here
-  
-    options: { 
+
+    options: {
         title: {
-           display: true,
-           text: 'UC Hispanic Graduation Rates From 2011-2019 by Institution '
-       },
+            display: true,
+            text: 'UC Hispanic Graduation Rates From 2011-2019 by Institution '
+        },
         scales: {
             yAxes: [{
                 ticks: {
-                    callback: function(value) {
-                      return (value ) + '%'; // convert it to percentage
+                    callback: function (value) {
+                        return (value) + '%'; // convert it to percentage
                     },
                     min: 0,
                     max: 100,
                     stepSize: 10
-                    },
-                    
+                },
+
 
                 scaleLabel: {
-                    display: true, 
+                    display: true,
                     labelString: 'Hispanic Graduation Rate',
-                    
+
                 }
-            }], 
+            }],
             xAxes: [{
                 scaleLabel: {
-                    display: true, 
+                    display: true,
                     labelString: 'Year'
                 }
-            }], 
+            }],
             plugins: false
-        }, 
+        },
 
-            tooltips: {
-                callback: function (value) {
-                    label: return (value) + '%'
-                    }
-                   
-    
-          }
-           
+        tooltips: {
+            callback: function (value) {
+                label: return (value) + '%'
+            }
+
+
         }
-      
-       },
+
+    }
+
+},
 );
-    
+
 
 
 var originalLineDraw = Chart.controllers.line.prototype.draw;
 Chart.helpers.extend(Chart.controllers.line.prototype, {
-  draw: function() {
-    originalLineDraw.apply(this, arguments);
+    draw: function () {
+        originalLineDraw.apply(this, arguments);
 
-    var chart = this.chart;
-    var ctx = chart.chart.ctx;
+        var chart = this.chart;
+        var ctx = chart.chart.ctx;
 
-    var index = chart.config.data.lineAtIndex;
-    if (index) {
-      var xaxis = chart.scales['x-axis-0'];
-      var yaxis = chart.scales['y-axis-0'];
+        var index = chart.config.data.lineAtIndex;
+        if (index) {
+            var xaxis = chart.scales['x-axis-0'];
+            var yaxis = chart.scales['y-axis-0'];
 
-      ctx.save();
-      ctx.beginPath();
-      ctx.moveTo(xaxis.getPixelForValue(undefined, index), yaxis.top);
-      ctx.strokeStyle = '#ff0000';
-      ctx.lineTo(xaxis.getPixelForValue(undefined, index), yaxis.bottom);
-      ctx.stroke();
-      ctx.restore();
+            ctx.save();
+            ctx.beginPath();
+            ctx.moveTo(xaxis.getPixelForValue(undefined, index), yaxis.top);
+            ctx.strokeStyle = '#ff0000';
+            ctx.lineTo(xaxis.getPixelForValue(undefined, index), yaxis.bottom);
+            ctx.stroke();
+            ctx.restore();
+        }
     }
-  }
 });
 
 
