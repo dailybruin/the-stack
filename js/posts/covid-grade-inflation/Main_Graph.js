75b23cd071988aa89bb7d10a08d4c76e63a4a85d
lindseyp314 <lindseyp314@ucla.edu>
2021-05-15 10:51:53 -0600

    Bar chart element for the main graph

--- js/posts/covid-grade-inflation/Main_Graph.js
+++ js/posts/covid-grade-inflation/Main_Graph.js
@@ -1,30 +1,30 @@
+//This is the basic code for the chart. We can then grab the data for each class using
+//similar code to the class fill-ups article and make the data that information for each class. 
+//If we want to be able to show more than one class at a time, we can work on that. 
+
 //Creating the bar graphs to show grade inflation
-const labels = ['1', '2', '3'];
+const labels = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F'];
 const data = {
     labels: labels,
     datasets: [{
-        label: 'COVID Grades',
-        data: [65, 59],
+        label: 'Grades During Online Learning',
+        data: [65, 59, 12, 150, 98, 14, 18, 23, 10, 8, 5, 13, 8],
         backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
         ],
         borderColor: [
             'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
         ],
         borderWidth: 1,
         index: 1
     }, {
-        label: 'Other Grades',
-        data: [80, 56],
+        label: 'Grades During On-Campus Learning',
+        data: [80, 56, 43, 100, 25, 12, 25, 100, 10, 8, 5, 12, 32],
         backgroundColor: [
             'red',
-            'pink'
         ],
         borderColor: [
             'pink',
-            'red'
         ],
         index: 2,
     }]
