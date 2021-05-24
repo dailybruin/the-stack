72a22f9eef0b72f8b8bbbedc9e4d1cbad3530111
luoziqing99 <69550825+luoziqing99@users.noreply.github.com>
2021-05-23 19:40:31 -0700

    add pie chart js

---
+++ js/posts/covid-grade-inflation/pie_chart.js
@@ -0,0 +1,22 @@
+anychart.onDocumentReady(function() {
+
+    // set the data
+    var data = [
+        {x: "Letter Grade", value: 128521},
+        {x: "Pass/No Pass", value: 5527}
+    ];
+  
+    // create the chart
+    var chart = anychart.pie();
+  
+    // set the chart title
+    chart.title("Percentage for Letter Grade and Pass/No Pass Before and After COVID-19");
+  
+    // add the data
+    chart.data(data);
+  
+    // display the chart in the container
+    chart.container('container');
+    chart.draw();
+  
+  });
