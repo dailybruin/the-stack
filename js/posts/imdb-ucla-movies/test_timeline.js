52bb05063c5ab7a9765e98b7f08c0f6e5e957779
mansakrishna23 <mansakrishna23@gmail.com>
2021-05-29 13:59:16 -0700

    edited timeline

--- js/posts/imdb-ucla-movies/test_timeline.js
+++ js/posts/imdb-ucla-movies/test_timeline.js
@@ -15,223 +15,158 @@ am4core.ready(function () {
 
     chart.dateFormatter.inputDateFormat = "yyyy-MM-dd HH:mm";
     chart.dateFormatter.dateFormat = "yyyy";
-    /*THE IMAGE PATH IS ../../../img/imdb-ucla-movies/IMAGE NAME*/
+    
+    //Path for images: 
+    let imagePath = "../../../img/imdb-ucla-movies/"
+
     chart.data = [{
-        //Emergency! (EP: Nurse's Wild)
+        //Emergency! EP: Nurse's Wild
         "category": "",
         "start": "1972-03-04 12:00",
         "end": "1972-03-04 12:15",
         "color": colorSet.getIndex(0),
-        "text": "Emergency! (Ep: Nurse's Wild)\nIMDB Rating = 8.0/10.0",
-        "icon": "../../../img/imdb-ucla-movies/emergency.jpg"
+        "text": "Emergency! (Ep: Nurse's Wild)",
+        "extra": "TV Series • 1972 - 1979",
+        "rating": "IMDB Rating = 8.0 / 10.0"
     }, {
         //The Godfather
         "category": "",
-        "start": "1972-08-24 12:00",
-        "end": "1972-08-24 12:15",
+        "start": "1972-03-14 12:00",
+        "end": "1972-03-14 12:15",
         "color": colorSet.getIndex(1),
-        "text": "The Godfather (1972)\nIMDB Rating = 9.2/10.0",
-        "icon": "../../../img/imdb-ucla-movies/godfather.jpeg"
+        "text": "The Godfather",
+        "extra": "Film • 1972 • 2h 55 min",
+        "rating": "IMDB Rating = 9.2 / 10.0"
     }, {
-        //Barnaby Jones (EP: Sunday: Doomsday)
+        //Barnaby Jones
         "category": "",
         "start": "1973-02-25 12:00",
         "end": "1973-02-25 12:15",
         "color": colorSet.getIndex(2),
-        "text": "Barnaby Jones (EP: Sunday: Doomsday)\nIMDB Rating = 7.6/10.0",
-        "icon": "../../../img/imdb-ucla-movies/barnaby_jones.jpg"
+        "text": "Barnaby Jones (Ep: Sunday: Doomsday)",
+        "extra": "TV Series • 1973 - 1980",
+        "rating": "IMDB Rating = 7.6 / 10.0"
     }, {
-        //In Concert (EP #1.15)
+        //The Bolero
         "category": "",
-        "start": "1973-08-17 12:00",
-        "end": "1973-08-17 12:15",
+        "start": "1973-10-15 12:00",
+        "end": "1973-10-15 12:15",
         "color": colorSet.getIndex(3),
-        "text": "In Concert (EP: #1.15)\nIMDB Rating = N/A",
-        "icon": "../../../img/imdb-ucla-movies/in-concert.jpg"
+        "text": "The Bolero",
+        "extra": "Short • Music • Documentary • 1973 • 26 min",
+        "rating": "IMDB Rating = 7.6 / 10.0"
     }, {
-        //The Emergency! (EP: The Promise)
+        //Emergency! Ep; The Promise
         "category": "",
         "start": "1973-12-01 12:00",
         "end": "1973-12-01 12:15",
         "color": colorSet.getIndex(4),
-        "text": "The Emergency! (EP: The Promise)\nIMDB Rating = 8.1/10.0",
-        "icon": "../../../img/imdb-ucla-movies/emergency.jpg"
-    }, {
-        //The Reincarnation of Peter Proud
-        "category": "",
-        "start": "1975-04-25 12:00",
-        "end": "1975-04-25 12:15",
-        "color": colorSet.getIndex(5),
-        "text": "The Reincarnation of Peter Proud\nIMDB Rating = 6.5/10.0",
-        "icon": "../../../img/imdb-ucla-movies/peter-proud.jpg"
+        "text": "Emergency! (Ep: The Promise)",
+        "extra": "TV Series • 1972 - 1979",
+        "rating": "IMDB Rating = 8.1 / 10.0"
     }, {
         //Washington: Behind Closed Doors
         "category": "",
         "start": "1977-09-07 12:00",
         "end": "1977-09-07 12:15",
-        "color": colorSet.getIndex(6),
-        "text": "Washington: Behind Closed Doors\nIMDB Rating = 9.2/10.0"
+        "color": colorSet.getIndex(5),
+        "text": "Washington: Behind Closed Doors (Part 2)",
+        "extra": "TV Mini Series • 1977",
+        "rating": "IMDB Rating = 9.2 / 10.0"
     }, {
-        //The Rockford Files (EP: The Prisoner of Rosemont Hall)
+        //The Rockford Files
         "category": "",
         "start": "1978-02-17 12:00",
         "end": "1978-02-17 12:15",
-        "color": colorSet.getIndex(7),
-        "text": "The Rockford Files (EP: The Prisoner of Rosemont Hall)\nIMDB Rating = 7.7/10.0"
+        "color": colorSet.getIndex(6),
+        "text": "The Rockford Files (Ep: The Prisoner of Rosemont Hall)",
+        "extra": "TV Series • 1974 - 1980",
+        "rating": "IMDB Rating = 7.7 / 10.0"
     }, {
-        //The Big Fix
-        "category": "",
-        "start": "1978-10-06 12:00",
-        "end": "1978-10-06 12:15",
-        "color": colorSet.getIndex(8),
-        "text": "The Big Fix\nIMDB Rating = 6.3/10.0"
-    }, {
-        //America Alive (EP: Lucille Ball at UCLA)
-        "category": "",
-        "start": "1978-11-10 12:00",
-        "end": "1978-11-10 12:15",
-        "color": colorSet.getIndex(9),
-        "text": "America Alive! (EP: Lucille Ball at UCLA)\nIMDB Rating = N/A"
-    }, {
-        //North Dallas Forty
-        "category": "",
-        "start": "1979-08-03 12:00",
-        "end": "1979-08-03 12:15",
-        "color": colorSet.getIndex(10),
-        "text": "North Dallas Forty\nIMDB Rating = 7.0/10.0"
-    }, {
-        //CHiPs EP: Hot Wheels
+        //CHiPs Ep; Hot Wheels
         "category": "",
         "start": "1979-11-03 12:00",
         "end": "1979-11-03 12:15",
-        "color": colorSet.getIndex(11),
-        "text": "CHiPs (EP: Hot Wheels)\nIMDB Rating = 7.6/10.0"
+        "color": colorSet.getIndex(7),
+        "text": "CHiPs (Ep: Hot Wheels)",
+        "extra": "TV Series • 1977 - 1983",
+        "rating": "IMDB Rating = 7.6 / 10.0"
     }, {
-        //Penitentiary
-        "category": "",
-        "start": "1979-12-01 12:00",
-        "end": "1979-12-01 12:15",
-        "color": colorSet.getIndex(12),
-        "text": "Penitentiary\nIMDB Rating = 5.7/10.0"
-    }, {
-        //American Gigolo
-        "category": "",
-        "start": "1980-02-01 12:00",
-        "end": "1980-02-01 12:15",
-        "color": colorSet.getIndex(13),
-        "text": "American Gigolo\nIMDB Rating = 6.2/10.0"
-    }, {
-        //Charlie's Angels EP: Angels on Campus
+        //Charlie's Angels
         "category": "",
         "start": "1980-04-08 12:00",
         "end": "1980-04-08 12:15",
-        "color": colorSet.getIndex(14),
-        "text": "Charlie's Angels (EP: Angels on Campus)\nIMDB Rating = 7.5/10.0"
+        "color": colorSet.getIndex(8),
+        "text": "Charlie's Angels (Ep: Angels on Campus)",
+        "extra": "TV Series • 1976 - 1981",
+        "rating": "IMDB Rating = 7.5 / 10.0"
     }, {
-        //CHiPs (EP: E.M.T.)
+        //CHiPs EP: E.M.T.
         "category": "",
         "start": "1980-10-19 12:00",
         "end": "1980-10-19 12:15",
-        "color": colorSet.getIndex(15),
-        "text": "CHiPs (EP: E.M.T.)\nIMDB Rating = 7.6/10.0"
+        "color": colorSet.getIndex(9),
+        "text": "CHiPs (Ep: E.M.T.)",
+        "extra": "TV Series • 1977 - 1983",
+        "rating": "IMDB Rating = 7.6 / 10.0"
     }, {
-        //CHiPs (EP: Satan's Angels)
-        "category": "",
-        "start": "1980-12-14 12:00",
-        "end": "1980-12-14 12:15",
-        "color": colorSet.getIndex(16),
-        "text": "CHiPs (EP: Satan's Angels)\nIMDB Rating = 7.1/10.0"
-    }, {
-        //Simon & Simon (EP: Love, Christy)
+        //Simon & Simon - Love, Christy
         "category": "",
         "start": "1981-12-01 12:00",
         "end": "1981-12-01 12:15",
-        "color": colorSet.getIndex(17),
-        "text": "Simon & Simon (EP: Love, Christy)\nIMDB Rating = 7.5/10.0"
+        "color": colorSet.getIndex(10),
+        "text": "Simon & Simon (Ep: Love, Christy)",
+        "extra": "TV Series • 1981 - 1989",
+        "rating": "IMDB Rating = 7.5 / 10.0"
     }, {
-        //McDonaldland (EP: How I Met Grimace)
-        "category": "",
-        "start": "1981-12-19 12:00",
-        "end": "1981-12-19 12:15",
-        "color": colorSet.getIndex(18),
-        "text": "McDonaldland (EP: How I Met Grimace)\nIMDB Rating = N/A"
-    }, {
-        //The Kid from Nowhere
-        "category": "",
-        "start": "1982-01-04 12:00",
-        "end": "1982-01-04 12:15",
-        "color": colorSet.getIndex(19),
-        "text": "The Kid from Nowhere\nIMDB Rating = 6.1/10.0"
-    }, {
-        //CHiPs (EP: Bright Flashes)
-        "category": "",
-        "start": "1982-01-17 12:00",
-        "end": "1982-01-17 12:15",
-        "color": colorSet.getIndex(20),
-        "text": "CHiPs (EP: Bright Flashes)\nIMDB Rating = 7.0/10.0"
-    }, {
-        //Personal Best
-        "category": "",
-        "start": "1982-02-05 12:00",
-        "end": "1982-02-05 12:15",
-        "color": colorSet.getIndex(21),
-        "text": "Personal Best\nIMDB Rating = 6.3/10.0"
-    }, {
-        //CHiPs EP: Silent Partner
-        "category": "",
-        "start": "1982-02-28 12:00",
-        "end": "1982-02-28 12:15",
-        "color": colorSet.getIndex(22),
-        "text": "CHiPs (EP: Silent Partner)\nIMDB Rating = 6.9/10.0"
-    }, {
-        //CHiPs: A Threat of War
+        //CHiPs EP: A Threat of War
         "category": "",
         "start": "1982-03-21 12:00",
         "end": "1982-03-21 12:15",
-        "color": colorSet.getIndex(23),
-        "text": "CHiPs (EP: A Threat of War)\nIMDB Rating = 7.5/10.0"
-    }, {
-        //CHiPs EP: Trained for Trouble
-        "category": "",
-        "start": "1982-04-04 12:00",
-        "end": "1982-04-04 12:15",
-        "color": colorSet.getIndex(24),
-        "text": "CHiPs (EP: Trained for Trouble)\nIMDB Rating = 6.5/10.0"
-    }, {
-        //Tag: The Assassinatin Game
-        "category": "",
-        "start": "1982-04-20 12:00",
-        "end": "1982-04-20 12:15",
-        "color": colorSet.getIndex(25),
-        "text": "Tag: The Assassination Game\nIMDB Rating = 5.9/10.0"
+        "color": colorSet.getIndex(11),
+        "text": "CHiPs (Ep: A Threat of War)",
+        "extra": "TV Series • 1977 - 1983",
+        "rating": "IMDB Rating = 7.5 / 10.0"
     }, {
         //CHiPs EP: K-9-1
         "category": "",
         "start": "1982-05-09 12:00",
         "end": "1982-05-09 12:15",
-        "color": colorSet.getIndex(26),
-        "text": "CHiPs (EP: K-9-1)\nIMDB Rating = 7.9/10.0"
+        "color": colorSet.getIndex(12),
+        "text": "CHiPs (Ep: K-9-1)",
+        "extra": "TV Series • 1977 - 1983",
+        "rating": "IMDB Rating = 7.9 / 10.0"
     }, {
-        //Six Weeks
+        //Dallas EP: Eye of the Beholder
         "category": "",
-        "start": "1982-12-17 12:00",
-        "end": "1982-12-17 12:15",
-        "color": colorSet.getIndex(27),
-        "text": "Six Weeks\nIMDB Rating = 5.7/10.0"
-    },
-    {
-        //Dynasty EP: The Hearing
+        "start": "1984-02-14 12:00",
+        "end": "1984-02-14 12:15",
+        "color": colorSet.getIndex(13),
+        "text": "Dallas (Ep: Eye of the Beholder)",
+        "extra": "TV Series • 1978-1991",
+        "rating": "IMDB Rating = 7.5 / 10.0"
+    }, {
+        //Paper Dolls 
         "category": "",
-        "start": "1983-03-26 12:00",
-        "end": "1983-03-26 12:15",
-        "color": colorSet.getIndex(28),
-        "text": "Dynasty (EP: The Hearing)\nIMDB Rating = 7.3/10.0"
-    }
-
-    ];
+        "start": "1984-09-23 12:00",
+        "end": "1984-09-23 12:15",
+        "color": colorSet.getIndex(14),
+        "text": "Paper Dolls (Ep: #1.1)",
+        "extra": "TV Series • 1984",
+        "rating": "IMDB Rating = 7.8 / 10.0"
+    }, {
+        //Highway to Heaven 
+        "category": "",
+        "start": "1986-10-01 12:00",
+        "end": "1986-10-01 12:15",
+        "color": colorSet.getIndex(15),
+        "text": "Highway to Heaven (Ep: A Special Love: Part Two)",
+        "extra": "TV Series • 1984 - 1989",
+        "rating": "IMDB Rating = 8.7 / 10.0"
+    }];
 
     chart.fontSize = 10;
-    chart.tooltipContainer.fontSize = 10;
+    chart.tooltipContainer.fontSize = 13;
 
     var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
     categoryAxis.dataFields.category = "category";
@@ -263,8 +198,8 @@ am4core.ready(function () {
     labelTemplate.fillOpacity = 0.7;
     labelTemplate.background.fill = new am4core.InterfaceColorSet().getFor("background");
     labelTemplate.fill = new am4core.InterfaceColorSet().getFor("text");
-    labelTemplate.background.fillOpacity = 1;
-    labelTemplate.padding(7, 7, 7, 7);
+    labelTemplate.background.fillOpacity = 0.90;
+    labelTemplate.padding(10, 10, 10, 10);
 
     var series = chart.series.push(new am4plugins_timeline.CurveColumnSeries());
     series.columns.template.height = am4core.percent(30);
@@ -287,10 +222,11 @@ am4core.ready(function () {
     imageBullet1.image.propertyFields.href = "icon";
     imageBullet1.image.scale = 1;
     imageBullet1.circle.radius = am4core.percent(100);
-    imageBullet1.dy = -5;
+    imageBullet1.dy = -2;
     imageBullet1.background.pointerBaseWidth = 10;
     imageBullet1.background.pointerLength = 10
-    imageBullet1.tooltipText = "{text}";
+    imageBullet1.tooltipText = "[bold]{text}\n[normal]{extra}\n{rating}";
+    imageBullet1.tooltipText.background = "color";
 
     series.tooltip.pointerOrientation = "up";
 
@@ -321,7 +257,7 @@ am4core.ready(function () {
     //chart.scrollbarX.parent = chart.curveContainer;
     //chart.scrollbarX.height = 300;
     //chart.scrollbarX.orientation = "vertical";
-    chart.scrollbarX.opacity = 0.5;
+    chart.scrollbarX.opacity = 0.7;
 
     var cursor = new am4plugins_timeline.CurveCursor();
     chart.cursor = cursor;
@@ -368,18 +304,18 @@ am4core.ready(function () {
     }
 
     var label = chart.createChild(am4core.Label);
-    label.text = "Fifty Years of Hollywood at UCLA"
+    label.text = "Fifty Years of Movies & Television at UCLA"
     label.isMeasured = false;
-    label.y = am4core.percent(20);
+    label.y = am4core.percent(5);
     label.x = am4core.percent(50);
     label.horizontalCenter = "middle";
-    label.fontSize = 20;
+    label.fontSize = 25;
 
 }); // end am4core.ready()
 
 function getPoints() {
 
-    var points = [{ x: -1500, y: 200 }, { x: 0, y: 200 }];
+    var points = [{ x: -1000, y: 200 }, { x: 0, y: 200 }];
 
     var w = 650;
     var h = 400;
