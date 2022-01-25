e4ac91eb0062c8a635f69829d08ad094bd0edd7e
Andre <ahiwatig@media.ucla.edu>
2022-01-24 19:02:38 -0800

    added an html folder for dining hall js folder; edits to pie chart

--- js/posts/dining-halls/piechart.js
+++ js/posts/dining-halls/piechart.js
@@ -17,16 +17,58 @@ let pieChart = new Chart(ctxPie, {
       {
         label: 'Swipes',
         backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)',
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)',
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
+        'rgb(149, 163, 179)',
+        'rgb(130, 9, 51)',
+        'rgb(132, 90, 109)',
+        'rgb(28, 93, 153)',
+        'rgb(113, 124, 137)',
+        'rgb(232, 141, 103)',
+        'rgb(198, 174, 130)',
+        'rgb(152, 149, 114)',
             ],
-        data: [117463, 104091, 34326, 14579, 11108, 119835, 91102, 11026],
+        data: [117463, 104091, 34326, 14579, 11108, 119835, 91102, 111026],
       },
     ],
-  }
+  },
+  options: {
+    title: {
+      display: true,
+      text: '21-22 Meal Swipes',
+      fontSize: 19,
+    },
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItem, data) {
+          let label = data.labels[tooltipItem.index];
+          let count =
+            data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+          return (
+            label +
+            ': ' +
+            count.toLocaleString('en-US', {
+              maximumFractionDigits: 2,
+            })
+          );
+        },
+      },
+    },
+    plugins: {
+      datalabels: {
+        formatter: function (value, context) {
+          val = value / (117463 + 104091 + 34326 + 14579 + 11108 + 119835 + 91102 + 111026);
+          return val.toLocaleString('en-US', {
+            style: 'percent',
+            maximumFractionDigits: 2,
+          });
+        },
+        font: {
+          size: 16,
+        },
+        anchor: 'end',
+        clamp: true,
+        align: 'start',
+      },
+    },
+  },
+});
+
