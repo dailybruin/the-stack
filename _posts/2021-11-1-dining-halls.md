f75f0d26775c0fc4be3208ca156a60d82cd7ae83
Lindseyp314 <lindseyp314@ucla.edu>
2022-02-03 16:05:44 -0700

    debug of dropdown menus

--- _posts/2021-11-1-dining-halls.md
+++ _posts/2021-11-1-dining-halls.md
@@ -26,8 +26,6 @@ scripts:
   - //cdn.rawgit.com/Keyang/node-csvtojson/d41f44aa/browser/csvtojson.min.js
   - /js/posts/dining-halls/piechart.js
   - /js/posts/dining-halls/busyness-barchart.js
-  - //node_modules/csvtojson/browser/csvtojson.min.js
-  - //cdn.rawgit.com/Keyang/node-csvtojson/d41f44aa/browser/csvtojson.min.js
 
 stylesheets:
   - //cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css
@@ -37,4 +35,6 @@ piechart:
 <div class = 'pie chart'><canvas id = 'SwipesPieChart'></canvas></div>
 
 barchart:
+<select id="Dining-Hall"></select>
+<select id="Day"></select>
 <div class = 'bar_chart'><canvas id = 'barChart'></canvas></div>
--- js/posts/dining-halls/busyness-barchart.js
+++ js/posts/dining-halls/busyness-barchart.js
@@ -1,44 +1,107 @@
 d3.csv('../../../../js/posts/dining-halls/theStudy_Sunday.csv').then(makeChart);
-console.log('accessed bar chart')
 
-function makeChart(csvData) {
-  console.log(csvData);
-
-  const timeIntervals = ["12:00AM", "12:30AM", "1:00AM", "1:30AM", "2:00AM", "2:30AM", "3:00AM", "3:30AM", "4:00AM", 
+//variables to make and update the chart
+let realTimes = [];
+let realCount = [];
+let barChart;
+const timeIntervals = ["12:00AM", "12:30AM", "1:00AM", "1:30AM", "2:00AM", "2:30AM", "3:00AM", "3:30AM", "4:00AM", 
   "4:30AM", "5:00AM", "5:30AM", "6:00AM", "6:30AM", "7:00AM", "7:30AM", "8:00AM", "8:30AM", "9:00AM", "9:30AM",
   "10:00AM", "10:30AM", "11:00AM", "11:30AM", "12:00PM", "12:30PM", "1:00PM", "1:30PM", "2:00PM", "2:30PM",  "3:00PM", 
   "3:30PM", "4:00PM", "4:30PM", "5:00PM", "5:30PM", "6:00PM", "6:30PM", "7:00PM", "7:30PM", "8:00PM", "8:30PM", "9:00PM", 
   "9:30PM", "10:00PM", "10:30PM", "11:00PM", "11:30PM"];
 
-  // const csvFilePath= csvData;
-  // // const csv=require('csvtojson')
-  // csv()
-  // .fromFile(csvFilePath)
-  // .then((jsonObj)=>{
-  //   console.log(jsonObj);
-  // })
+// console.log('accessed bar chart')
 
-  // // Async / await usage
-  // const jsonArray=csv().fromFile(csvFilePath);
+//Suggestion: You may need to set initial values for the chart to load properly
+let hallValue = 'The Study';
+let dayValue = 'Sunday';
 
-  // var Converter = require("csvtojson").Converter;
 
-  // var converter = new Converter({});
+// Drop down to choose a dining hall
+let dining_halls = [
+  "The Study",
+  "Bruin Plate",
+  "Bruin Cafe"
+]
+//Drop down to choose a day of the week
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+]
 
-  // converter.fromFile(csvData,function(err,result){
+// let choice = values[0];
+// console.log(choice)
+// console.log("New choice after button:")
 
-  //     if(err){
-  //         console.log("Error");
-  //         console.log(err);  
-  //     } 
-  //     var jsonArray = result;
+//Makes Dropdown menus
+initDropdown(dining_halls, '#Dining-Hall')
+initDropdown(days, "#Day")
 
-  //     //to check json
-  //     console.log(data);
-  // });
+//Code for init found at https://www.d3-graph-gallery.com/graph/line_select.html
+function initDropdown(values, id) {
+  d3
+    .select(id)
+    .selectAll('option')
+    .data(values)
+    .enter()
+    .append('option')
+    .text(function (d) {return d; }) // text showed in the menu
+    .attr('value', function (d) {
+      return d;
+    }) // corresponding value returned by the button
+    
+}
 
-  let realTimes = [];
-  let realCount = [];
+//On Change Functions
+//These are currently working, I suggest using them to set the path for the data to import
+d3.select('#Day').on('change', function(){
+  dayValue = d3.select(this).property('value');
+  console.log(dayValue)
+  //My suggestion for moving forward:
+  //setPath(dayValue,hallValue)
+})
+
+d3.select('#Dining-Hall').on('change', function () {
+  hallValue = d3.select(this).property('value');
+  console.log(hallValue)
+  //setPath(dayValue, hallValue)
+})
+
+//Possible starting value for the path
+let path = '../../../../datasets/dining-halls/'
+
+//I would use this function to set the path and the call a second function to update the chart
+function setPath(dayValue, hallValue){
+  path = '../../../../datasets/dining-halls/'
+  //Maybe use a switch to choose the path, since the days of the week should display the same way they are listed
+    //it may be most efficient to have each case be a dining hall
+  switch(hallValue){
+    case 'The Study': 
+      path = `${path}theStudy_${dayValue}.csv`
+      break;
+    //don't forget a default in case someone chooses a file that you don't have
+    //default:
+  }
+  console.log(path)
+  //Call a new function to update the chart
+  d3.csv(path).then(updateData);
+}
+
+function updateData(csvData){
+  //find a way to update realValue, and realTimes, then update data
+
+  //Finally this is the syntax for updating data in chartJS
+  barChart.data = data
+  barChart.update()
+}
+
+function makeChart(csvData) {
+  //console.log(csvData);
 
   for(let i = 0; i < csvData.length; i++){
       let time = timeIntervals[csvData[i].IntervalCategory];
@@ -46,8 +109,8 @@ function makeChart(csvData) {
       let swipeCount = csvData[i].count/4;
       realCount.push(parseInt(swipeCount));
   }
-  console.log(realTimes);
-  console.log(realCount);
+  //console.log(realTimes);
+  //console.log(realCount);
 
   let data = {
     labels: realTimes,
@@ -64,80 +127,9 @@ function makeChart(csvData) {
   };
 
   let ctx = document.getElementById('barChart');
-  let barChart = new Chart(ctx, {
+  barChart = new Chart(ctx, {
     type: 'bar',
     data: data,
     options: options,
   });
 }
-
-
-
-
-
-// d3.csv('../../../../js/posts/dining-halls/theStudy_Sunday.csv').then(makeChart);
-// console.log('accessed bar chart')
-
-// function makeChart(csvData) {
-//   console.log(csvData);
-
-//   let data = {
-//     labels: [
-//       'FY 12-13',
-//       'FY 13-14',
-//       'FY 14-15',
-//       'FY 15-16',
-//       'FY 16-17',
-//       'FY 17-18',
-//       'FY 18-19',
-//       'FY 19-20 (Approved but not actual)',
-//     ],
-//     datasets: [],
-//   };
-
-//   let colors = [
-//     'green',
-//     'peachpuff',
-//     'pink',
-//     'lightcoral',
-//     'lavender',
-//     'gold',
-//     'thistle',
-//     'orchid',
-//   ];
-
-//   for (let i = 0; i < 3; i++) {
-//     data.datasets.push({
-//       label: csvData[i].Category,
-//       data: Object.values(csvData[i]).slice(1),
-//       backgroundColor: colors[i + 3],
-//       borderColor: colors[i + 3],
-//     });
-//   }
-
-//   let options = {
-//     title: {
-//       display: true,
-//       text: 'UCPD Revenue',
-//     },
-//     scales: {
-//       xAxes: [
-//         {
-//           stacked: true,
-//         },
-//       ],
-//       yAxes: [
-//         {
-//           stacked: true,
-//         },
-//       ],
-//     },
-//   };
-
-//   let ctx = document.getElementById('barChart');
-//   let stackedBarChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: data,
-//     options: options,
-//   });
-// }
