199af49a5ee3a3764b7a3088d2fca03fa494b96c
Tyler Wu <tylerwu2222@g.ucla.edu>
2022-02-12 12:23:00 -0800

    added more stopwrods

--- js/posts/prof-reviews/globals.js
+++ js/posts/prof-reviews/globals.js
@@ -1,11 +1,11 @@
 // should only remove for single words (could be useful with bigrams)
 export const STOPWORDS = [
-    'give', 'recommend', 'solve', 'learn', 'learned', 'learning', 'participate', 'make', 'knows', 'ask', 'code', 'say', 'improve', 'answer',// verbs
+    'give', 'recommend', 'solve', 'learn', 'learned', 'learning', 'participate', 'make', 'knows', 'ask', 'code', 'say', 'improve', 'answer','wanted', 'teach',// verbs
     'lab','research','content', 'extra', 'credit', 'assignment', 'homework', 'problem', 'problems', 'studies',
     'readings', 'reader', 'essay', 'draft', 'write','test', 'group', 'work', 'clicker', 'lecturer', 'information', 'student', 'professor', 'professors', // class-related nouns
     'major', 'physics', 'math', 'calculus', 'chemistry', 'science', 'art', 'philosophy',// subject-related words
-    'end', 'time',// other nouns
-    'amazing', 'good','fine', 'super','great', 'best', 'cool', 'short', 'high', 'real', 'little', // non-descript adjectives
+    'end', 'time', // other nouns
+    'amazing', 'good','fine', 'super','great','wonderful','best', 'cool', 'short', 'high', 'real', 'little', // non-descript adjectives
     'basically','weekly','honestly' // filler adverbs
 ]; 
 
