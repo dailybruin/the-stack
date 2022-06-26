0eb60eb12be09a2788c1c549e33398ad601e1cba
Lindseyp314 <lindseyp314@ucla.edu>
2022-06-26 14:46:06 -0400

    made templates for all simple request charts

--- collaborations/templates/line_chart.js
+++ collaborations/templates/line_chart.js
@@ -4,14 +4,15 @@ const colors = ['blue', 'yellow','pink','purple'] //CHANGE THESE TO WHAT MATCHES
 const data = {
   labels: labels,
   //DATA SETS IN A LIST. EACH OBJECT IN LIST IS A CATEGORY OF DATA CHANGE THESE OUT WITH YOUR DATA
+  //IF YOU HAVE MORE DATA TO DISPLAY ADD ADDITIONAL {...}
   datasets: [{
-    label: 'LABEL FOR BAR 1 IN EACH GROUP',
-    data: [], //VALUE FOR EACH BAR
+    label: 'LABEL FOR LINE 1',
+    data:[], //VALUE FOR EACH POINT
     backgroundColor: colors[0],
     borderColor: colors[0],
     borderWidth: 1
   },{
-    label: 'LABEL FOR BAR 2 IN EACH GROUP',
+    label: 'LABEL FOR LINE 2',
     data: [], //DATA HERE
     backgroundColor: colors[1],
     borderColor: colors[1],
@@ -20,30 +21,30 @@ const data = {
 };
 //OPTIONS FOR THE DISPLAY OF THE CHART, FOR MORE OPTIONS GO TO CHART.JS
 const options = {
-  scales: {
-    y: {
-      beginAtZero: true,
-      title:{
-        display: true,
-        text: 'Y-AXIS TITLE'
-    }
-    }
-  },
-  plugins: {
-    title: {
-        display: true,
-        text: 'CHART TITLE'
-      },
-  },
-  maintainAspectRatio: false,
-}
+    scales: {
+      y: {
+        beginAtZero: true,
+        title:{
+          display: true,
+          text: 'Y-AXIS TITLE'
+      }
+      }
+    },
+    plugins: {
+      title: {
+          display: true,
+          text: 'CHART TITLE'
+        },
+    },
+    maintainAspectRatio: false,
+  }
 //THIS CODE MAKES THE CHART, MAKE SURE THE CTX VARIABLE DOES NOT MATCH ANY OTHER CTX VARIABLE
 //CHANGE ID-HERE TO WHATEVER YOU MADE YOUR ID IN THE HTML FILE, CHOOSE SOMETHING DESCRIPTIVE
-const ctxBAR = document.getElementById('ID-HERE');
+const ctxLINE= document.getElementById('ID-HERE');
 //THE CHART VARIABLE NEEDS TO HAVE A UNIQUE NAME RENAME IT SOMETHING DESCRIPTIVE
 //MAKE SURE ctxBAR MATCHES THE CONST VARIABLE YOU JUST SET
-const chart = new Chart(ctxBAR, {
-    type: 'bar',
+const chart = new Chart(ctxLINE, {
+    type: 'line',
     data: data,
     options: options
   });
--- collaborations/templates/scatter_chart.js
+++ collaborations/templates/scatter_chart.js
@@ -1,49 +1,59 @@
 Chart.defaults.font.family = 'Noto Serif, serif'  //FONT FOR CHART CHANGE IF NEEDED
-const labels = ['X-AXIS','LABELS','LEFT','TO','RIGHT']; //THIS SHOULD BE A LIST OF WHAT YOU WANT ON THE X-AXIS
 const colors = ['blue', 'yellow','pink','purple'] //CHANGE THESE TO WHAT MATCHES THE SUBJECT
 const data = {
-  labels: labels,
   //DATA SETS IN A LIST. EACH OBJECT IN LIST IS A CATEGORY OF DATA CHANGE THESE OUT WITH YOUR DATA
+  //IF YOU HAVE MORE DATA TO DISPLAY ADD ADDITIONAL {...}
   datasets: [{
-    label: 'LABEL FOR BAR 1 IN EACH GROUP',
-    data: [], //VALUE FOR EACH BAR
+    label: 'LABEL FOR DATASET 1',
+    data:[
+        {x: 1,y: 2},
+        {x: 3,y:4},
+        {x: 5,y: 6}
+    ], //VALUE FOR EACH POINT
     backgroundColor: colors[0],
     borderColor: colors[0],
     borderWidth: 1
   },{
-    label: 'LABEL FOR BAR 2 IN EACH GROUP',
-    data: [], //DATA HERE
+    label: 'LABEL FOR DATASET 2',
+    data: [
+        {x: 2,y: 3},
+        {x: 4,y: 5},
+        {x: 6,y: 7}
+    ], //DATA HERE
     backgroundColor: colors[1],
     borderColor: colors[1],
     borderWidth: 1
   }]
 };
-//OPTIONS FOR THE DISPLAY OF THE CHART, FOR MORE OPTIONS GO TO CHART.JS
+//OPTIONS FOR THE DISPLAY OF THE CHART, FOR MORE OPTIONS SEE CHART.JS
 const options = {
-  scales: {
-    y: {
-      beginAtZero: true,
-      title:{
-        display: true,
-        text: 'Y-AXIS TITLE'
-    }
-    }
-  },
-  plugins: {
-    title: {
-        display: true,
-        text: 'CHART TITLE'
+    scales: {
+      y: {
+        beginAtZero: true,
+        title:{
+          display: true,
+          text: 'Y-AXIS TITLE'
+      }
       },
-  },
-  maintainAspectRatio: false,
-}
+      x:{
+        beginAtZero: true,
+      }
+    },
+    plugins: {
+      title: {
+          display: true,
+          text: 'CHART TITLE'
+        },
+    },
+    maintainAspectRatio: false,
+  }
 //THIS CODE MAKES THE CHART, MAKE SURE THE CTX VARIABLE DOES NOT MATCH ANY OTHER CTX VARIABLE
 //CHANGE ID-HERE TO WHATEVER YOU MADE YOUR ID IN THE HTML FILE, CHOOSE SOMETHING DESCRIPTIVE
-const ctxBAR = document.getElementById('ID-HERE');
+const ctxSCATTER = document.getElementById('ID-HERE');
 //THE CHART VARIABLE NEEDS TO HAVE A UNIQUE NAME RENAME IT SOMETHING DESCRIPTIVE
 //MAKE SURE ctxBAR MATCHES THE CONST VARIABLE YOU JUST SET
-const chart = new Chart(ctxBAR, {
-    type: 'bar',
+const chart = new Chart(ctxSCATTER, {
+    type: 'scatter',
     data: data,
     options: options
   });
