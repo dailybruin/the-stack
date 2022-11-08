30ee97936898368dab8163391e1436b5a0a3f5fe
Lindseyp314 <lindseyp314@ucla.edu>
2022-11-07 21:38:26 -0800

    created markdown file and added pie charts

---
+++ _posts/2022-11-7-biodiversity.md
@@ -0,0 +1,30 @@
+---
+title: Biodiveristy at UCLA
+
+teaser: 
+
+authors:
+  - 
+
+key_takeaways:
+  - 
+  -
+  - 
+
+featured_image:
+  url: 
+  caption: 
+og_image: 
+
+scripts:
+  - //cdnjs.cloudflare.com/ajax/libs/d3/5.7.0/d3.min.js
+  - //cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js
+  - /js/posts/biodiversity/plantsbygarden.js
+  - /js/posts/biodiversity/plantypes.js
+
+stylesheets:
+  - //cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css
+---
+
+<div><canvas id = "PlantD"></canvas></div>
+<div><canvas id = "PlantTypes"></canvas></div>
--- js/posts/biodiversity/plantsbygarden.js
+++ js/posts/biodiversity/plantsbygarden.js
@@ -1,14 +1,14 @@
-Chart.defaults.font.family = 'Noto Serif, serif'  //FONT FOR CHART CHANGE IF NEEDED
-const labels = ['Shrubs', 'Trees', 'Forbs', 'Grasses and Sedges', 'Annuals','Vines', 'Succulents']; //THIS SHOULD BE A LIST OF WHAT EACH SEGMENT IN THE PIE REPRESENTS
-const colors = ['rgb(21, 139, 231)', 'rgb(231, 171, 21)', 'rgb(231, 59, 21)','rgb(132, 21, 231)','rgb(21, 231, 50)', 'rgb(114, 150, 108)','rgb(204, 147, 237)'] //CHANGE THESE TO WHAT MATCHES THE SUBJECT 
+//Chart.defaults.font.family = 'Noto Serif, serif'  //FONT FOR CHART CHANGE IF NEEDED
+const labels = ['Grand Savana/Oakwoodland','Sage Hill','Urban Canyon','Alluvial Corridor', 'Turf Alternative', 'Bioswale/Rain Garden']; //THIS SHOULD BE A LIST OF WHAT EACH SEGMENT IN THE PIE REPRESENTS
+const colors = ['rgb(21, 139, 231)', 'rgb(231, 171, 21)', 'rgb(231, 59, 21)','rgb(132, 21, 231)','rgb(21, 231, 50)', 'rgb(114, 150, 108)'] //CHANGE THESE TO WHAT MATCHES THE SUBJECT
 const data = {
   labels: labels,
   //DATA SETS IN A LIST. EACH OBJECT IN LIST IS A RING OF THE PIE CHART 
   //CHANGE THESE OUT WITH YOUR DATA
   //IF YOU HAVE MORE DATA TO DISPLAY ADD ADDITIONAL {...}, BUT THIS IS *NOT* RECOMMENDED
   datasets: [{
-    label: 'Plant Types at UCLA',
-    data:[62, 34, 30, 18, 14, 5, 3], //VALUE FOR EACH SEGMENT
+    label: 'Plant Distribution at UCLA',
+    data:[39, 77, 81, 21, 21, 85], //VALUE FOR EACH SEGMENT
     backgroundColor: colors,
     borderColor: colors,
     borderWidth: 1
@@ -20,17 +20,17 @@ const options = {
     plugins: {
       title: {
           display: true,
-          text: 'Plant Types at UCLA'
+          text: 'Distribution of Plants at UCLA'
         },
     },
     maintainAspectRatio: false,
   }
 //THIS CODE MAKES THE CHART, MAKE SURE THE CTX VARIABLE DOES NOT MATCH ANY OTHER CTX VARIABLE
 //CHANGE ID-HERE TO WHATEVER YOU MADE YOUR ID IN THE HTML FILE, CHOOSE SOMETHING DESCRIPTIVE
-const ctxPT = document.getElementById('PlantTypes').getContext('2d');
+const ctxPIE = document.getElementById('PlantD').getContext('2d');
 //THE CHART VARIABLE NEEDS TO HAVE A UNIQUE NAME RENAME IT SOMETHING DESCRIPTIVE
 //MAKE SURE ctxBAR MATCHES THE CONST VARIABLE YOU JUST SET
-const chart = new Chart(ctxPT, {
+const chart = new Chart(ctxPIE, {
     type: 'pie', //chose "pie" or "doughnut"
     data: data,
     options: options
