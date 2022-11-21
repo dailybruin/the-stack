9baad10736e5e3aca63c1bd81022bc617ece4ef0
Mari <marihmoto@gmail.com>
2022-11-20 17:25:24 -0800

    chart fix

--- js/posts/covid-research-funding/priv-vs-pub.js
+++ js/posts/covid-research-funding/priv-vs-pub.js
@@ -6,31 +6,33 @@ const data = {
     datasets: [
         {
             label: 'State educational appropriations',
-            data: [454, 536, 493, 448, 511],
+            data: [454,536,493,448,511],
             backgroundColor: 'rgba(50, 132, 191, 0.9)',
             stack: 'Stack 0',
         },
         {
             label: 'Government grants & contracts',
-            data: [893, 803, 773, 743, 707],
+            data: [893,803,773,743,707],
             backgroundColor: 'rgba(50, 132, 191, 0.55)',
             stack: 'Stack 0',
         },
         {
             label: 'Private gifts',
-            data: [405, 365, 367, 379, 308],
+            data: [405,365,367,379,308],
             backgroundColor: 'rgba(255, 210, 0, 0.9)',
             stack: 'Stack 1',
         },
         {
             label: 'Private industry grants & contracts',
-            data: [295, 277, 276, 273, 241],
+            data:  [295,277,276,273,241],
             backgroundColor: 'rgba(255, 210, 0, 0.4)',
             stack: 'Stack 1',
         },
     ]
 
 }
+const pubTotals = [1347,1339,1266,1191,1218]
+const privTotals = [700,642,643,652,549]
 const privvspubbar = new Chart(ctx, {
     type: 'bar',
     data: data,
@@ -45,11 +47,14 @@ const privvspubbar = new Chart(ctx, {
                     title: function(context) {
                         console.log(context[0].label);
                         return `Year: ${context[0].label}`;
-                      },
-                      label: function(context) {
+                    },
+                    label: function(context) {
                         return `$${context.parsed.x} million`;
-                    }
-
+                    },
+                    afterBody: function(context) {
+                        var array = [`Public Total: $${pubTotals[context[0].dataIndex]} million`,`Private Total: $${privTotals[context[0].dataIndex]} million`];
+                        return array;
+                    },
                 }
 
             }
