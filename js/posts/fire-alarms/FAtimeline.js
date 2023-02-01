cda35c869827b67103c6a71d432780e70a13710f
Lindseyp314 <lindseyp314@ucla.edu>
2023-02-01 10:45:07 -0800

    some style changes

--- js/posts/fire-alarms/FAtimeline.js
+++ js/posts/fire-alarms/FAtimeline.js
@@ -7,18 +7,18 @@ const FAtimeline = new Chart(ctz, {
             label: '# of Fire Alarms',
             data: [531, 494, 478, 439, 575],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 188, 53, 0.8)',
+                'rgba(255, 188, 53, 0.8)',
+                'rgba(255, 188, 53, 0.8)',
+                'rgba(255, 188, 53, 0.8)',
+                'rgba(255, 188, 53, 0.8)',
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
+                'rgba(255, 188, 53, 0.8)',
+                'rgba(255, 188, 53, 0.8)',
+                'rgba(255, 188, 53, 0.8)',
+                'rgba(255, 188, 53, 0.8)',
+                'rgba(255, 188, 53, 0.8)',
             ],
             borderWidth: 1.5
         }]
@@ -27,6 +27,12 @@ const FAtimeline = new Chart(ctz, {
         scales: {
             y: {
                 beginAtZero: true
+            }
+        },
+        plugins: {
+            legend: {
+                display: true,
+                onClick: null
             }
         }
     }
