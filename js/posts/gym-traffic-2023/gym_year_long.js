21d04b733944a9c19b0b5e3faa51290ac1673f47
Lindseyp314 <lindseyp314@ucla.edu>
2023-03-04 10:21:35 -0600

    fixed pie and line charts

--- js/posts/gym-traffic-2023/gym_year_long.js
+++ js/posts/gym-traffic-2023/gym_year_long.js
@@ -1,50 +1,93 @@
-Chart.defaults.font.family = 'Noto Serif, serif'  //FONT FOR CHART CHANGE IF NEEDED
-const labels = ['January','February','March','April','May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; //THIS SHOULD BE A LIST OF WHAT YOU WANT ON THE X-AXIS
-const colors = ['rgb(255, 200, 87)', 'rgb(28, 93, 153)'] //CHANGE THESE TO WHAT MATCHES THE SUBJECT
-const data = {
+//Chart.defaults.font.family = 'Noto Serif, serif'  //FONT FOR CHART CHANGE IF NEEDED
+const labels_time = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]; //THIS SHOULD BE A LIST OF WHAT YOU WANT ON THE X-AXIS
+const colors_time = ['rgb(255, 200, 87)', 'rgb(28, 93, 153)']; //CHANGE THESE TO WHAT MATCHES THE SUBJECT
+const data_time = {
   labels: labels,
   //DATA SETS IN A LIST. EACH OBJECT IN LIST IS A CATEGORY OF DATA CHANGE THESE OUT WITH YOUR DATA
   //IF YOU HAVE MORE DATA TO DISPLAY ADD ADDITIONAL {...}
-  datasets: [{
-    label: 'BFIT',
-    data:[3868, 36495, 32054, 41305, 36624, 10073, 7928, 8013, 8488, 35557, 29626, 9516], //VALUE FOR EACH POINT
-    backgroundColor: colors[0],
-    borderColor: colors[0],
-    borderWidth: 1
-  },{
-    label: 'Wooden',
-    data: [13731, 63633, 61066, 85295, 76599, 32322, 33040, 34921, 37166, 73931, 58250, 23298], //DATA HERE
-    backgroundColor: colors[1],
-    borderColor: colors[1],
-    borderWidth: 1
-  }]
+  datasets: [
+    {
+      label: 'BFIT',
+      data: [
+        3868,
+        36495,
+        32054,
+        41305,
+        36624,
+        10073,
+        7928,
+        8013,
+        8488,
+        35557,
+        29626,
+        9516,
+      ], //VALUE FOR EACH POINT
+      backgroundColor: colors_time[0],
+      borderColor: colors_time[0],
+      borderWidth: 1,
+    },
+    {
+      label: 'Wooden',
+      data: [
+        13731,
+        63633,
+        61066,
+        85295,
+        76599,
+        32322,
+        33040,
+        34921,
+        37166,
+        73931,
+        58250,
+        23298,
+      ], //DATA HERE
+      backgroundColor: colors_time[1],
+      borderColor: colors_time[1],
+      borderWidth: 1,
+    },
+  ],
 };
 //OPTIONS FOR THE DISPLAY OF THE CHART, FOR MORE OPTIONS GO TO CHART.JS
-const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-        title:{
-          display: true,
-          text: 'Total Number of Visits Per Month'
-      }
-      }
-    },
-    plugins: {
+const options_time = {
+  scales: {
+    y: {
+      beginAtZero: true,
       title: {
-          display: true,
-          text: 'Number of Visitors Per Month'
-        },
+        display: true,
+        text: 'Total Number of Visits Per Month',
+      },
     },
-    maintainAspectRatio: false,
-  }
+  },
+  plugins: {
+    title: {
+      display: true,
+      text: 'Number of Visitors Per Month',
+    },
+  },
+  fill: false,
+  maintainAspectRatio: false,
+};
 //THIS CODE MAKES THE CHART, MAKE SURE THE CTX VARIABLE DOES NOT MATCH ANY OTHER CTX VARIABLE
 //CHANGE ID-HERE TO WHATEVER YOU MADE YOUR ID IN THE HTML FILE, CHOOSE SOMETHING DESCRIPTIVE
-const ctxLINE= document.getElementById('gym_month_long');
+const ctxLINE = document.getElementById('gym_month_long');
 //THE CHART VARIABLE NEEDS TO HAVE A UNIQUE NAME RENAME IT SOMETHING DESCRIPTIVE
 //MAKE SURE ctxBAR MATCHES THE CONST VARIABLE YOU JUST SET
-const chart = new Chart(ctxLINE, {
-    type: 'line',
-    data: data,
-    options: options
-  });
+const chart_time = new Chart(ctxLINE, {
+  type: 'line',
+  data: data_time,
+  options: options_time,
+});
--- js/posts/gym-traffic-2023/wooden_gym_times_member_pie.js
+++ js/posts/gym-traffic-2023/wooden_gym_times_member_pie.js
@@ -1,47 +1,61 @@
-  //Chart.defaults.font.family = 'Noto Serif, serif'  //FONT FOR CHART CHANGE IF NEEDED
-  const labels = ['Undergraduate', 'Student Affiliate','Undergraduate- HA','Retiree & Emeriti', 'Conference Guest']; //THIS SHOULD BE A LIST OF WHAT EACH SEGMENT IN THE PIE REPRESENTS
-  const member_colors = ['rgb(28, 93, 153)','rgb(232, 107, 146)', 'rgb(114, 150, 108)', 'rgb(130, 9, 51)','rgb(255, 200, 87)'] //CHANGE THESE TO WHAT MATCHES THE SUBJECT
-  const data = {
-    labels: labels,
-    //DATA SETS IN A LIST. EACH OBJECT IN LIST IS A RING OF THE PIE CHART 
-    //CHANGE THESE OUT WITH YOUR DATA
-    //IF YOU HAVE MORE DATA TO DISPLAY ADD ADDITIONAL {...}, BUT THIS IS *NOT* RECOMMENDED
-    datasets: [{
+//Chart.defaults.font.family = 'Noto Serif, serif'  //FONT FOR CHART CHANGE IF NEEDED
+const labels = [
+  'Undergraduate',
+  'Student Affiliate',
+  'Undergraduate- HA',
+  'Retiree & Emeriti',
+  'Conference Guest',
+]; //THIS SHOULD BE A LIST OF WHAT EACH SEGMENT IN THE PIE REPRESENTS
+const member_colors = [
+  'rgb(28, 93, 153)',
+  'rgb(232, 107, 146)',
+  'rgb(114, 150, 108)',
+  'rgb(130, 9, 51)',
+  'rgb(255, 200, 87)',
+]; //CHANGE THESE TO WHAT MATCHES THE SUBJECT
+const data_wooden = {
+  labels: labels,
+  //DATA SETS IN A LIST. EACH OBJECT IN LIST IS A RING OF THE PIE CHART
+  //CHANGE THESE OUT WITH YOUR DATA
+  //IF YOU HAVE MORE DATA TO DISPLAY ADD ADDITIONAL {...}, BUT THIS IS *NOT* RECOMMENDED
+  datasets: [
+    {
       label: 'Membership distribution distrbution at Wooden',
-      data:[456771, 20409, 1251, 1776, 11397], //VALUE FOR EACH SEGMENT
+      data: [456771, 20409, 1251, 1776, 11397], //VALUE FOR EACH SEGMENT
       backgroundColor: member_colors,
       borderColor: 'white',
-      borderWidth: 1
-    }]
-  };
-  //OPTIONS FOR THE DISPLAY OF THE CHART, FOR MORE OPTIONS GO TO CHART.JS
-  const options = {
-    title: {
-      display: true,
-      text: 'Membership Distribution at Wooden',
-      fontSize: 14
+      borderWidth: 1,
     },
-      legend: {
-        position: 'right',
-    },
-    maintainAspectRatio: false,
-    }
-  //THIS CODE MAKES THE CHART, MAKE SURE THE CTX VARIABLE DOES NOT MATCH ANY OTHER CTX VARIABLE
-  //CHANGE ID-HERE TO WHATEVER YOU MADE YOUR ID IN THE HTML FILE, CHOOSE SOMETHING DESCRIPTIVE
-  const ctxPIE = document.getElementById('wooden_member_pie').getContext('2d');
-  //THE CHART VARIABLE NEEDS TO HAVE A UNIQUE NAME RENAME IT SOMETHING DESCRIPTIVE
-  //MAKE SURE ctxBAR MATCHES THE CONST VARIABLE YOU JUST SET
-  const chart = new Chart(ctxPIE, {
-      type: 'pie', //chose "pie" or "doughnut"
-      data: data,
-      options: options
-    });
-  
-    if (window.matchMedia('(max-width: 480px)').matches) {
-      chart.canvas.style = 'max-height:300px';
-      chart.options.legend.position = 'top';
-      chart.options.maintainAspectRatio = false;
-      chart.update();
-    }
-  
-  
+  ],
+};
+//OPTIONS FOR THE DISPLAY OF THE CHART, FOR MORE OPTIONS GO TO CHART.JS
+const options_wooden = {
+  title: {
+    display: true,
+    text: 'Membership Distribution at Wooden',
+    fontSize: 14,
+  },
+  legend: {
+    position: 'right',
+  },
+  maintainAspectRatio: false,
+};
+//THIS CODE MAKES THE CHART, MAKE SURE THE CTX VARIABLE DOES NOT MATCH ANY OTHER CTX VARIABLE
+//CHANGE ID-HERE TO WHATEVER YOU MADE YOUR ID IN THE HTML FILE, CHOOSE SOMETHING DESCRIPTIVE
+const ctxPIE_wooden = document
+  .getElementById('wooden_member_pie')
+  .getContext('2d');
+//THE CHART VARIABLE NEEDS TO HAVE A UNIQUE NAME RENAME IT SOMETHING DESCRIPTIVE
+//MAKE SURE ctxBAR MATCHES THE CONST VARIABLE YOU JUST SET
+const chart_wooden = new Chart(ctxPIE_wooden, {
+  type: 'pie', //chose "pie" or "doughnut"
+  data: data_wooden,
+  options: options_wooden,
+});
+
+if (window.matchMedia('(max-width: 480px)').matches) {
+  chart_wooden.canvas.style = 'max-height:300px';
+  chart_wooden.options.legend.position = 'top';
+  chart_wooden.options.maintainAspectRatio = false;
+  chart_wooden.update();
+}
