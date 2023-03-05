9cfc68fae15735a14303badc097a89c29196fd5a
cjunwon <cjunwon.tech@gmail.com>
2023-03-04 17:46:14 -0800

    Updated line chart

--- js/posts/gym-traffic-2023/gym_year_long.js
+++ js/posts/gym-traffic-2023/gym_year_long.js
@@ -1,5 +1,103 @@
+const verticalLines = [
+  {    
+    x: 'September',    
+    borderColor: '#5A5A5A',    
+    borderWidth: 2,    
+    borderDash: [5, 5],
+    label: {
+      enabled: true,
+      content: 'Fall 2021',
+      position: 'top',
+      color: '#5A5A5A'
+    },
+    month: 'September', // Add month property
+  },
+  {
+    x: 'January',
+    borderColor: '#5A5A5A',
+    borderWidth: 2,
+    borderDash: [5, 5],
+    label: {
+      enabled: true,
+      content: 'Winter 2022',
+      position: 'bottom',
+      color: '#5A5A5A'
+    },
+    month: 'January', // Add month property
+  },
+  {
+    x: 'March',
+    borderColor: '#5A5A5A',
+    borderWidth: 2,
+    borderDash: [5, 5],
+    label: {
+      enabled: true,
+      content: 'Spring 2022',
+      position: 'bottom',
+      color: '#5A5A5A'
+    },
+    month: 'March', // Add month property
+  },
+  {
+    x: 'June',
+    borderColor: '#5A5A5A',
+    borderWidth: 2,
+    borderDash: [5, 5],
+    label: {
+      enabled: true,
+      content: 'Summer 2022',
+      position: 'bottom',
+      color: '#5A5A5A'
+    },
+    month: 'June', // Add month property
+  },
+];
+
+// Define the shift values for each month
+const shiftValues = {
+  'September': 50,  // September
+  'January': 5,  // January
+  'March': 55,  // March
+  'June': 52,  // June
+};
+
+const verticalLinePlugin = {
+  id: 'verticalLine',
+  beforeDraw(chart, args, options) {
+    const { ctx, chartArea: { top, bottom }, scales: { x } } = chart;
+    ctx.save();
+    for (let i = 0; i < verticalLines.length; i++) {
+      const line = verticalLines[i];
+      const xValue = x.getPixelForValue(line.x) + (shiftValues[line.month] || 0);
+      ctx.beginPath();
+      ctx.moveTo(xValue, top);
+      ctx.lineTo(xValue, bottom);
+      ctx.strokeStyle = line.borderColor;
+      ctx.lineWidth = line.borderWidth;
+      ctx.setLineDash(line.borderDash || []);
+      ctx.lineDashOffset = line.borderDashOffset;
+      ctx.stroke();
+
+      if (line.label.enabled) {
+        ctx.fillStyle = line.label.color || '#5A5A5A';
+        ctx.font = '10px sans-serif';
+        const { width } = ctx.measureText(line.label.content);
+        const labelX = xValue + 4;
+        const labelY = top+5;
+        ctx.fillText(line.label.content, labelX, labelY);
+      }
+    }
+    ctx.restore();
+  }
+};
+
+
 //Chart.defaults.font.family = 'Noto Serif, serif'  //FONT FOR CHART CHANGE IF NEEDED
 const labels_time = [
+  'September',
+  'October',
+  'November',
+  'December',
   'January',
   'February',
   'March',
@@ -8,10 +106,6 @@ const labels_time = [
   'June',
   'July',
   'August',
-  'September',
-  'October',
-  'November',
-  'December',
 ]; //THIS SHOULD BE A LIST OF WHAT YOU WANT ON THE X-AXIS
 const colors_time = ['rgb(255, 200, 87)', 'rgb(28, 93, 153)']; //CHANGE THESE TO WHAT MATCHES THE SUBJECT
 const data_time = {
@@ -20,28 +114,12 @@ const data_time = {
   //IF YOU HAVE MORE DATA TO DISPLAY ADD ADDITIONAL {...}
   datasets: [
     {
-      label: 'BFIT',
-      data: [
-        3868,
-        36495,
-        32054,
-        41305,
-        36624,
-        10073,
-        7928,
-        8013,
-        8488,
-        35557,
-        29626,
-        9516,
-      ], //VALUE FOR EACH POINT
-      backgroundColor: colors_time[0],
-      borderColor: colors_time[0],
-      borderWidth: 1,
-    },
-    {
       label: 'Wooden',
       data: [
+        37166,
+        73931,
+        58250,
+        23298,
         13731,
         63633,
         61066,
@@ -50,19 +128,37 @@ const data_time = {
         32322,
         33040,
         34921,
-        37166,
-        73931,
-        58250,
-        23298,
       ], //DATA HERE
       backgroundColor: colors_time[1],
       borderColor: colors_time[1],
-      borderWidth: 1,
+      borderWidth: 2,
+    },
+    {
+      label: 'BFit',
+      data: [
+        8488,
+        35557,
+        29626,
+        9516,
+        3868,
+        36495,
+        32054,
+        41305,
+        36624,
+        10073,
+        7928,
+        8013,
+      ], //VALUE FOR EACH POINT
+      backgroundColor: colors_time[0],
+      borderColor: colors_time[0],
+      borderWidth: 2,
     },
   ],
 };
+
 //OPTIONS FOR THE DISPLAY OF THE CHART, FOR MORE OPTIONS GO TO CHART.JS
 const options_time = {
+  tension: 0.2,
   scales: {
     y: {
       beginAtZero: true,
@@ -76,11 +172,19 @@ const options_time = {
     title: {
       display: true,
       text: 'Number of Visitors Per Month',
+      font: {
+        size: 20, // You can change the font size here
+      },
     },
   },
   fill: false,
   maintainAspectRatio: false,
+  interaction: {
+    mode: 'index',
+    intersect: false,
+  },
 };
+
 //THIS CODE MAKES THE CHART, MAKE SURE THE CTX VARIABLE DOES NOT MATCH ANY OTHER CTX VARIABLE
 //CHANGE ID-HERE TO WHATEVER YOU MADE YOUR ID IN THE HTML FILE, CHOOSE SOMETHING DESCRIPTIVE
 const ctxLINE = document.getElementById('gym_month_long');
@@ -90,4 +194,5 @@ const chart_time = new Chart(ctxLINE, {
   type: 'line',
   data: data_time,
   options: options_time,
+  plugins: [verticalLinePlugin]
 });
