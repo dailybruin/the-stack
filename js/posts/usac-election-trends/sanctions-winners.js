b2e47d3ab7b6be889e08116a98ad4ce547d07ef0
Leo Cardozo <leonardocardozo14@gmail.com>
2023-05-15 16:33:33 -0700

    started bar chart for whether winners were sanctioned

--- js/posts/usac-election-trends/sanctions-winners.js
+++ js/posts/usac-election-trends/sanctions-winners.js
@@ -1,57 +1,47 @@
 Chart.defaults.font.family = 'Noto Serif, serif'  //FONT FOR CHART CHANGE IF NEEDED
 const labels = ['2020','2021','2022','2023']; //THIS SHOULD BE A LIST OF WHAT YOU WANT ON THE X-AXIS
-const colors = ['lightblue', 'pink','purple'] //CHANGE THESE TO WHAT MATCHES THE SUBJECT
+const colors = ['blue', 'yellow','pink','purple'] //CHANGE THESE TO WHAT MATCHES THE SUBJECT
 const data = {
   labels: labels,
   //DATA SETS IN A LIST. EACH OBJECT IN LIST IS A CATEGORY OF DATA CHANGE THESE OUT WITH YOUR DATA
   datasets: [{
-    label: 'Valid Complaints',
-    data: [58, 27, 8, 6], //VALUE FOR EACH BAR
+    label: 'LABEL FOR BAR 1 IN EACH GROUP',
+    data: [6, 10], //VALUE FOR EACH BAR
     backgroundColor: colors[0],
     borderColor: colors[0],
-    borderWidth: 1,
-    stack: "Stack 0"
+    borderWidth: 1
   },{
-    label: 'Partially Valid Complaints',
-    data: [9, 9, 3, 9], //DATA HERE
+    label: 'LABEL FOR BAR 2 IN EACH GROUP',
+    data: [5, 10], //DATA HERE
     backgroundColor: colors[1],
     borderColor: colors[1],
-    borderWidth: 1,
-    stack: "Stack 1"
-  },{
-    label: 'Invalid Complaints',
-    data: [13, 13, 15, 9], //DATA HERE
-    backgroundColor: colors[2],
-    borderColor: colors[2],
-    borderWidth: 1,
-    stack: "Stack 2"
+    borderWidth: 1
   }]
 };
 //OPTIONS FOR THE DISPLAY OF THE CHART, FOR MORE OPTIONS GO TO CHART.JS
 const options = {
-  scales: {
-    y: {
-      beginAtZero: true,
-      title:{
-        display: true,
-        text: 'Number of Citations Filed'
-    }
-    }
+    scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true
+        }
   },
   plugins: {
     title: {
         display: true,
-        text: 'Complaints Filed During USAC Elections, 2020-2023'
+        text: 'CHART TITLE'
       },
   },
   maintainAspectRatio: false,
 }
 //THIS CODE MAKES THE CHART, MAKE SURE THE CTX VARIABLE DOES NOT MATCH ANY OTHER CTX VARIABLE
 //CHANGE ID-HERE TO WHATEVER YOU MADE YOUR ID IN THE HTML FILE, CHOOSE SOMETHING DESCRIPTIVE
-const sanctionsYear = document.getElementById('usac-bar-chart');
+const sanctionWinners = document.getElementById('ID-HERE');
 //THE CHART VARIABLE NEEDS TO HAVE A UNIQUE NAME RENAME IT SOMETHING DESCRIPTIVE
 //MAKE SURE ctxBAR MATCHES THE CONST VARIABLE YOU JUST SET
-const chart = new Chart(sanctionsYear, {
+const chart = new Chart(sanctionWinners, {
     type: 'bar',
     data: data,
     options: options
