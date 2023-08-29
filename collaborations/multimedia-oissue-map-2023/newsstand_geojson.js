b2b2ffe0daf0200ebf9f95f98aab75116d31db5d
Junwon Choi <56373973+cjunwon@users.noreply.github.com>
2023-08-29 11:44:50 +0900

    Finalize map (#165)

---
+++ collaborations/multimedia-oissue-map-2023/newsstand_geojson.js
@@ -0,0 +1,703 @@
+const NEWSgeojson = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4463303, 34.05084436],
+      },
+      properties: {
+        CODE: 'A1',
+        NEWSSTAND: 'Rack',
+        ADDRESS: '1500 S Sepulveda Blvd, Los Angeles, CA 90025',
+        DESCRIPTION: '',
+        LOCATION: 'University Credit Union',
+        GROUP: 'Other',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4480861, 34.05979129],
+      },
+      properties: {
+        CODE: 'A4',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '1070 Veteran Ave, Los Angeles, CA 90024',
+        DESCRIPTION: 'Westwood lot',
+        LOCATION: 'UCLA Parking Structure 32',
+        GROUP: 'Other',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4461036, 34.06455713],
+      },
+      properties: {
+        CODE: 'A7',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '300 Medical Plaza Driveway # B200, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'UCLA Neuropsychiatric and Biobehavioral Health Services',
+        GROUP: 'School Facilities',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4452506, 34.06837476],
+      },
+      properties: {
+        CODE: 'A8',
+        NEWSSTAND: 'Front Desk',
+        ADDRESS: '555 Westwood Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: 'In UCLA Fleet and Transit office on front desk',
+        LOCATION: 'UCLA Transportation Office',
+        GROUP: 'School Facilities',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4498308, 34.07134873],
+      },
+      properties: {
+        CODE: 'A11',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '380 De Neve Drive, Los Angeles, CA 90024',
+        DESCRIPTION: '',
+        LOCATION: 'Sproul Landing',
+        GROUP: 'University Housing',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4436043, 34.06687485],
+      },
+      properties: {
+        CODE: 'A14',
+        NEWSSTAND: 'Rack',
+        ADDRESS: '650 Charles E Young Drive S, Los Angeles, CA 90095',
+        DESCRIPTION:
+          'Front door (off Charles E. Young) 2 shelf rack by elevator',
+        LOCATION: 'Fielding School of Public Health',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4436043, 34.06687485],
+      },
+      properties: {
+        CODE: 'A16',
+        NEWSSTAND: 'Rack',
+        ADDRESS: '650 Charles E Young Drive S, Los Angeles, CA 90095',
+        DESCRIPTION: 'Left corner on very old rack',
+        LOCATION: 'Fielding School of Public Health Patio',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4424542, 34.06868752],
+      },
+      properties: {
+        CODE: 'A17',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '617 Charles E Young Drive E, Los Angeles, CA 90095',
+        DESCRIPTION: 'Right before stairs into bombshelter',
+        LOCATION: 'Court of Sciences Student Center (Bombshelter)',
+        GROUP: 'Cafes / Open Areas',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4401046, 34.07085048],
+      },
+      properties: {
+        CODE: 'A20',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '445 Charles E Young Drive E, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Schoenberg Music Building',
+        GROUP: 'School Facilities',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4393469, 34.07138952],
+      },
+      properties: {
+        CODE: 'A21',
+        NEWSSTAND: 'Wood Box',
+        ADDRESS: '410 Charles E Young Drive E, Los Angeles, CA 90095',
+        DESCRIPTION:
+          'Entrance across from Dickson Court, outside on wooden shelf',
+        LOCATION: 'Murphy Hall (South)',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4392901, 34.07192881],
+      },
+      properties: {
+        CODE: 'A22',
+        NEWSSTAND: 'Wood Box',
+        ADDRESS: '410 Charles E Young Drive E, Los Angeles, CA 90095',
+        DESCRIPTION: 'Entrance across from Dodd, outside on wooden shelf',
+        LOCATION: 'Murphy Hall (North)',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4432652, 34.0711467],
+      },
+      properties: {
+        CODE: 'B4',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '',
+        DESCRIPTION: 'Start of Election Walk',
+        LOCATION: 'Meyerhoff Park / Election Walk',
+        GROUP: 'Cafes / Open Areas',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4457043, 34.07121484],
+      },
+      properties: {
+        CODE: 'B5',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: 'Bruin Walk, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'John Wooden Center',
+        GROUP: 'School Facilities',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4450979, 34.0632381],
+      },
+      properties: {
+        CODE: 'B6',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS:
+          'Bank of America Building, 924 Westwood Blvd #610, Los Angeles, CA 90024',
+        DESCRIPTION: '',
+        LOCATION: 'Westwood Plaza',
+        GROUP: 'Other',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4441537, 34.07037746],
+      },
+      properties: {
+        CODE: 'B8',
+        NEWSSTAND: 'Rack',
+        ADDRESS: '308 Westwood Plaza, Los Angeles, CA 90024',
+        DESCRIPTION: 'Next to ATMs, empty DB metal rack (2 shelves)',
+        LOCATION: 'Ackerman Union Viewpoint Conference Room',
+        GROUP: 'School Facilities',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4439617, 34.07056801],
+      },
+      properties: {
+        CODE: 'B9',
+        NEWSSTAND: 'Rack',
+        ADDRESS: '308 Westwood Plaza, Los Angeles, CA 90024',
+        DESCRIPTION:
+          'Entrance across from Kerckhoff, rack on the left (2 shelves)',
+        LOCATION: 'Ackerman Union 1st floor',
+        GROUP: 'School Facilities',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4436267, 34.07066327],
+      },
+      properties: {
+        CODE: 'B10',
+        NEWSSTAND: 'Rack',
+        ADDRESS: '308 Westwood Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: 'Front of Daily Bruin Office',
+        LOCATION: 'Kerckhoff Hall (Daily Bruin Office)',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4434119, 34.07060443],
+      },
+      properties: {
+        CODE: 'B11',
+        NEWSSTAND: 'Rack',
+        ADDRESS: '308 Westwood Plaza #2, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Kerckhoff Coffeehouse',
+        GROUP: 'Cafes / Open Areas',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4447357, 34.0698635],
+      },
+      properties: {
+        CODE: 'B12',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '425 Westwood Plaza, Los Angeles, CA 90095',
+        DESCRIPTION:
+          'Bottom of Engineering Stairs, south side of Ackerman Union',
+        LOCATION: 'UCLA Parking Structure 6',
+        GROUP: 'Other',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4443966, 34.06881595],
+      },
+      properties: {
+        CODE: 'B13',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '7400 Boelter Hall, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Henry Samueli School of Engineering (Top)',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.442015, 34.06963801],
+      },
+      properties: {
+        CODE: 'B14',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '520 Portola Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: 'Between Franz Hall and Math Sciences building, outside',
+        LOCATION: 'Franz Hall / Math Sciences',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4406432, 34.07001378],
+      },
+      properties: {
+        CODE: 'B15',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: 'Inverted Fountain, Los Angeles, CA 90095',
+        DESCRIPTION: 'East side of fountain',
+        LOCATION: 'Inverted Fountain',
+        GROUP: 'Cafes / Open Areas',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4410891, 34.07059386],
+      },
+      properties: {
+        CODE: 'B16',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '475 Portola Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Knudsen Hall',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4406603, 34.07213317],
+      },
+      properties: {
+        CODE: 'B17',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: 'UCLA Flag Pole, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'UCLA Flag Pole',
+        GROUP: 'Cafes / Open Areas',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4500286, 34.06183061],
+      },
+      properties: {
+        CODE: 'B20',
+        NEWSSTAND: '',
+        ADDRESS: 'Weyburn Terrace, Los Angeles, CA 90024',
+        DESCRIPTION: 'Across from grad housing at bus stop',
+        LOCATION: 'Weyburn Terrace',
+        GROUP: 'University Housing',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4484996, 34.0711648],
+      },
+      properties: {
+        CODE: 'C1',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '340 Bruin Walk, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Drake Stadium',
+        GROUP: 'School Facilities',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4501947, 34.07050356],
+      },
+      properties: {
+        CODE: 'C2',
+        NEWSSTAND: '',
+        ADDRESS: '370 De Neve Drive, Los Angeles, CA 90024',
+        DESCRIPTION: '',
+        LOCATION: 'De Neve Commons',
+        GROUP: 'University Housing',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4491717, 34.06972498],
+      },
+      properties: {
+        CODE: 'C3',
+        NEWSSTAND: '',
+        ADDRESS: 'Gayley Ave, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Tom Bradley International Hall',
+        GROUP: 'School Facilities',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4519774, 34.07234397],
+      },
+      properties: {
+        CODE: 'C4',
+        NEWSSTAND: 'Rack',
+        ADDRESS: '270 De Neve Drive, Los Angeles, CA 90024',
+        DESCRIPTION: 'South side elevators, rack (2 shelves)',
+        LOCATION: 'Rieber',
+        GROUP: 'University Housing',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4526451, 34.0737016],
+      },
+      properties: {
+        CODE: 'C5',
+        NEWSSTAND: 'Box',
+        ADDRESS: '250 De Neve Drive, Los Angeles, CA 90024',
+        DESCRIPTION: 'Outside on top of LA Times kiosk',
+        LOCATION: 'Hedrick',
+        GROUP: 'University Housing',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4499672, 34.07300647],
+      },
+      properties: {
+        CODE: 'C7',
+        NEWSSTAND: '',
+        ADDRESS: '330 De Neve Drive, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Covel Commons',
+        GROUP: 'University Housing',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4496857, 34.07198645],
+      },
+      properties: {
+        CODE: 'C8',
+        NEWSSTAND: '',
+        ADDRESS: '350 De Neve Drive, Los Angeles, CA 90024',
+        DESCRIPTION: '',
+        LOCATION: 'Sproul Hall',
+        GROUP: 'University Housing',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4504756, 34.07174892],
+      },
+      properties: {
+        CODE: 'C9',
+        NEWSSTAND: '',
+        ADDRESS: '360 De Neve Drive, Los Angeles, CA 90095',
+        DESCRIPTION: 'Housing Office in dorms',
+        LOCATION: 'UCLA Housing Services',
+        GROUP: 'University Housing',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4437823, 34.07393731],
+      },
+      properties: {
+        CODE: 'C10',
+        NEWSSTAND: 'Shelf',
+        ADDRESS:
+          'Parking Structure 5, 110 Westwood Plaza, Los Angeles, CA 90077',
+        DESCRIPTION:
+          'Empty wooden shelf in left corner where other newspapers are held',
+        LOCATION: 'Anderson Cafe',
+        GROUP: 'Cafes / Open Areas',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4422721, 34.07435727],
+      },
+      properties: {
+        CODE: 'C11',
+        NEWSSTAND: 'Rack',
+        ADDRESS: 'Royce Drive, Los Angeles, CA 90095',
+        DESCRIPTION: 'Main entrance, metal rack on the left',
+        LOCATION: 'Northern Lights Cafe',
+        GROUP: 'Cafes / Open Areas',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4412621, 34.07436549],
+      },
+      properties: {
+        CODE: 'C12',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS:
+          'Charles E. Young Research Library, 21536, Los Angeles, CA 90095',
+        DESCRIPTION: 'In front of Charles E. Young Research Library',
+        LOCATION: 'North Campus',
+        GROUP: 'Cafes / Open Areas',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4412842, 34.07411152],
+      },
+      properties: {
+        CODE: 'C13',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '335 Portola Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Campbell Hall (North)',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4400779, 34.07413767],
+      },
+      properties: {
+        CODE: 'C14',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '11282 Portola Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Bunche Hall',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.440009, 34.07642825],
+      },
+      properties: {
+        CODE: 'C15',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '235 Charles E Young Drive E, Los Angeles, CA 90024',
+        DESCRIPTION: '',
+        LOCATION: 'Melnitz Hall',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4385019, 34.07450437],
+      },
+      properties: {
+        CODE: 'C16',
+        NEWSSTAND: '',
+        ADDRESS: '330 Charles E Young Drive E, Los Angeles, CA 90024',
+        DESCRIPTION: '',
+        LOCATION: 'UCLA Guest House',
+        GROUP: 'School Facilities',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4384671, 34.07299453],
+      },
+      properties: {
+        CODE: 'C17',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '385 Charles E Young Drive E, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'UCLA School of Law',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4411126, 34.07286716],
+      },
+      properties: {
+        CODE: 'C18',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: 'Portola Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Haines Hall',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.441139, 34.07367091],
+      },
+      properties: {
+        CODE: 'C19',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '335 Portola Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Campbell Hall (East)',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.4393858, 34.07361336],
+      },
+      properties: {
+        CODE: 'C20',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: 'Lu Valle Commons, 398 Portola Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Lu Valle Commons',
+        GROUP: 'Cafes / Open Areas',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.439319, 34.07255958],
+      },
+      properties: {
+        CODE: 'C21',
+        NEWSSTAND: 'Kiosk',
+        ADDRESS: '315 Portola Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'Dodd Hall',
+        GROUP: 'Departments / Lecture Halls',
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.44576, 34.06926547],
+      },
+      properties: {
+        CODE: '',
+        NEWSSTAND: 'Front Desk',
+        ADDRESS: '425 Westwood Plaza, Los Angeles, CA 90095',
+        DESCRIPTION: '',
+        LOCATION: 'UCLA Meyer and Renee Luskin Conference Center',
+        GROUP: 'School Facilities',
+      },
+    },
+  ],
+};
