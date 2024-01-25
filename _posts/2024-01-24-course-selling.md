b0f7a38fb702dd583eacde0b46aa4f76c5de48f5
Junwon Choi <cjunwon.tech@gmail.com>
2024-01-25 01:42:28 -0800

    Update visualizations

---
+++ _posts/2024-01-24-course-selling.md
@@ -0,0 +1,45 @@
+---
+title:
+teaser:
+authors:
+  - cassidy_sadowski
+  - ayushi_kadakia
+  - luca_adams
+  - junwon_choi
+key_takeaways:
+  - A
+  - B
+  - C
+
+featured_image:
+  url:
+  caption:
+og_image:
+scripts:
+  - //cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js
+  - //cdn.anychart.com/releases/8.11.0/js/anychart-core.min.js
+  - //cdn.anychart.com/releases/8.11.0/js/anychart-circle-packing.min.js
+  - //cdn.anychart.com/releases/8.11.0/js/anychart-data-adapter.min.js
+  - /js/posts/course-selling/circlepacking.js
+  - /js/posts/course-selling/moneygraph.js
+  - /js/posts/course-selling/yeargraph.js
+
+stylesheets:
+  -
+---
+
+<style>
+#container {
+  width: 800px;  /* or any other width */
+  height: 600px; /* or any other height */
+}
+</style>
+<div id="container"></div>
+
+<div>
+    <canvas id="moneyGraph"></canvas>
+</div>
+
+<div>
+    <canvas id="yearGraph"></canvas>
+</div>
---
+++ js/posts/course-selling/circlepacking.js
@@ -0,0 +1,85 @@
+anychart.onDocumentReady(function() {
+  // load a json data file
+  // anychart.data.loadJsonFile('test.json',
+  anychart.data.loadJsonFile(
+    '/datasets/course-selling/reddit.json',
+
+    function(data) {
+      // add the data
+      var treeData = anychart.data.tree(data, 'as-table');
+
+      // create a circle packing chart instance
+      var chart = anychart.circlePacking(treeData);
+
+      // customize the tooltip
+      chart
+        .tooltip()
+        .useHtml(true)
+        .format(function() {
+          var src = this.item.get('industry');
+          if (src) {
+            return (
+              '<div>' +
+              // + '<span>Name: ' + this.name + '</span> <br/>'
+              '<span>Impacted: ' +
+              (src == 'Impacted Course' ? 'Yes' : 'No') +
+              '</span>  <br/>' +
+              '<span>Number of posts: ' +
+              this.value +
+              '</span> <br/>' +
+              // + '<span>Source: ' + this.item.get('source') + '</span>'
+              // If 'industry' is 'Impacted', then print "Impacted: Yes". Else print "Impacted: No"
+              // + '<span>Impacted: ' + src + '</span>'
+              '</div>'
+            );
+          }
+          return '<span>Total number of posts: ' + this.value + ' </span>';
+        });
+
+      // add a chart title
+      chart
+        .title()
+        .enabled(true)
+        .useHtml(true)
+        .text(
+          '<span style = "color: #112B3C;font-weight:600;font-size:18px;">Number of Reddit Posts Per Subject Area</span>'
+        );
+
+      // customize the appearance
+      chart.background('#f6f6f6');
+      chart.hovered().stroke(function() {
+        return {
+          thickness: 2,
+        };
+      });
+      chart.stroke(function() {
+        return {
+          thickness: 1,
+        };
+      });
+
+      // customize the labels (country names)
+      chart
+        .labels()
+        .fontSize('14')
+        .fontColor('#696969')
+        .textShadow('none')
+        .anchor('center-top')
+        .offsetY('-3%');
+
+      chart
+        .labels()
+        .background()
+        .enabled(true)
+        .fill('#f6f6f6 0.8')
+        .stroke('#888888')
+        .corners(5);
+
+      // specify the container element id
+      chart.container('container');
+
+      // initiate the drawing of the chart
+      chart.draw();
+    }
+  );
+});
