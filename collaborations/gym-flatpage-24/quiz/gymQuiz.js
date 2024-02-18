6cbc2db8f7d25fd859b305362247b82433dc5be1
lmcglynn <liamo.mcglynn@comcast.net>
2024-02-17 19:20:44 -0800

    Adding temporary photos

--- collaborations/gym-flatpage-24/quiz/gymQuiz.js
+++ collaborations/gym-flatpage-24/quiz/gymQuiz.js
@@ -9,17 +9,17 @@ const dropContainer = document.querySelector('.drop-points');
 const data = ["Chae Campbell", "Selena Harris", "Nya Reed", "Sara Ulias", "Katie McNamara", "Emily Lee"]
 
 let deviceType = "";
-let initialX = 0,initialY = 0;
+let initialX = 0, initialY = 0;
 let currentElement = "";
 let moveElement = false;
 
 //touch device check
 const isTouchDevice = () => {
-    try{
+    try {
         document.createEvent("TouchEvent");
         deviceType = "touch";
         return true;
-    } catch(e) {
+    } catch (e) {
         deviceType = "mouse";
         return false;
     }
@@ -39,8 +39,8 @@ const stopGame = () => {
 };
 
 //drag & drop
-function dragStart(e){
-    if(isTouchDevice()){
+function dragStart(e) {
+    if (isTouchDevice()) {
         initialX = e.touches[0].clientX;
         initialY = e.touches[0].clientY;
         moveElement = true;
@@ -51,13 +51,13 @@ function dragStart(e){
 }
 
 //drop target events
-function dragOver(e){
+function dragOver(e) {
     e.preventDefault();
 }
 
 //touchscreen movement
-const touchMove = (e) => { 
-    if(moveElement){
+const touchMove = (e) => {
+    if (moveElement) {
         e.preventDefault();
         let newX = e.touches[0].clientX;
         let newY = e.touches[0].clientY;
@@ -72,11 +72,11 @@ const touchMove = (e) => {
 //touchscreen drop
 const drop = (e) => {
     e.preventDefault();
-    if(isTouchDevice()){
+    if (isTouchDevice()) {
         moveElement = false;
         const currentDrop = document.querySelector(`div[data-id='${e.target.id}']`);
         const currentDropBound = currentDrop.getBoundingClientRect();
-        if(
+        if (
             initialX >= currentDropBound.left &&
             initialX <= currentDropBound.right &&
             initialY >= currentDropBound.top &&
@@ -85,13 +85,13 @@ const drop = (e) => {
             currentDrop.classList.add("dropped");
             currentElement.classList.add("hide");
             currentDrop.innerHTML = ``;
-            currentDrop.insertAdjacentHTML("afterbegin",`<img src="${currentElement.id}.png"`);
+            currentDrop.insertAdjacentHTML("afterbegin", `<img src="${currentElement.id}.png"`);
             count++;
         }
     } else {
         const data = e.dataTransfer.getData('text');
         const droppableElementData = e.target.getAttribute('data-id');
-        if(draggedElementData === droppableElementData){
+        if (draggedElementData === droppableElementData) {
             e.target.classList.add('dropped');
             draggedElement.classList.add('hide');
             draggedElement.setAttribute('draggable', false);
@@ -100,7 +100,7 @@ const drop = (e) => {
             count++;
         }
     }
-    if(count === 3){
+    if (count === 3) {
         result.innerText = "You Win!";
         stopGame();
     }
@@ -113,58 +113,58 @@ const creator = () => {
     let randomData = [];
     //for string random values in array
     for (let i = 1; i <= 3; i++) {
-      let randomValue = randomValueGenerator();
-      if (!randomData.includes(randomValue)) {
-        randomData.push(randomValue);
-      } else {
-        //If value already exists then decrement i by 1
-        i -= 1;
-      }
+        let randomValue = randomValueGenerator();
+        if (!randomData.includes(randomValue)) {
+            randomData.push(randomValue);
+        } else {
+            //If value already exists then decrement i by 1
+            i -= 1;
+        }
     }
     for (let i of randomData) {
-      const photoDiv = document.createElement("div");
-      photoDiv.classList.add("draggable-image");
-      photoDiv.setAttribute("draggable", true);
-      if (isTouchDevice()) {
-        photoDiv.style.position = "absolute";
-      }
-      photoDiv.innerHTML = `<img src="${i}.png" id="${i}">`;
-      dragContainer.appendChild(photoDiv);
+        const photoDiv = document.createElement("div");
+        photoDiv.classList.add("draggable-image");
+        photoDiv.setAttribute("draggable", true);
+        if (isTouchDevice()) {
+            photoDiv.style.position = "absolute";
+        }
+        photoDiv.innerHTML = `<img src="${i}.png" id="${i}">`;
+        dragContainer.appendChild(photoDiv);
     }
     //Sort the array randomly before creating country divs
     randomData = randomData.sort(() => 0.5 - Math.random());
     for (let i of randomData) {
-      const songDiv = document.createElement("div");
-      songDiv.innerHTML = `<div class='countries' data-id='${i}'>
+        const songDiv = document.createElement("div");
+        songDiv.innerHTML = `<div class='countries' data-id='${i}'>
       ${i.charAt(0).toUpperCase() + i.slice(1).replace("-", " ")}
       </div>
       `;
-      dropContainer.appendChild(songDiv);
+        dropContainer.appendChild(songDiv);
     }
-  };
-  //Start Game
-  startButton.addEventListener(
+};
+//Start Game
+startButton.addEventListener(
     "click",
     (startGame = async () => {
-      currentElement = "";
-      controls.classList.add("hide");
-      startButton.classList.add("hide");
-      //This will wait for creator to create the images and then move forward
-      await creator();
-      count = 0;
-      dropPoints = document.querySelectorAll(".songs");
-      draggableObjects = document.querySelectorAll(".draggable-image");
-      //Events
-      draggableObjects.forEach((element) => {
-        element.addEventListener("dragstart", dragStart);
-        //for touch screen
-        element.addEventListener("touchstart", dragStart);
-        element.addEventListener("touchend", drop);
-        element.addEventListener("touchmove", touchMove);
-      });
-      dropPoints.forEach((element) => {
-        element.addEventListener("dragover", dragOver);
-        element.addEventListener("drop", drop);
-      });
+        currentElement = "";
+        controls.classList.add("hide");
+        startButton.classList.add("hide");
+        //This will wait for creator to create the images and then move forward
+        await creator();
+        count = 0;
+        dropPoints = document.querySelectorAll(".songs");
+        draggableObjects = document.querySelectorAll(".draggable-image");
+        //Events
+        draggableObjects.forEach((element) => {
+            element.addEventListener("dragstart", dragStart);
+            //for touch screen
+            element.addEventListener("touchstart", dragStart);
+            element.addEventListener("touchend", drop);
+            element.addEventListener("touchmove", touchMove);
+        });
+        dropPoints.forEach((element) => {
+            element.addEventListener("dragover", dragOver);
+            element.addEventListener("drop", drop);
+        });
     })
-  );
+);
