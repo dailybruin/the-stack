0e2d57db4ba3b70ecfaf0bb08b6fef4b09b3ccc7
Junwon Choi <cjunwon.tech@gmail.com>
2024-04-12 17:16:23 -0700

    Fixed chart styling issues, ready for preview

--- js/posts/tuition-ucla/difference.js
+++ js/posts/tuition-ucla/difference.js
@@ -1,43 +1,100 @@
-Chart.defaults.font.family = 'Noto Serif, serif'
+Chart.defaults.font.family = 'PT Sans';
 
-const linedata = {
-  labels: ['2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021','2022'],
-  datasets: [{
-    data: [6947, 7259, 8034, 8430, 8667, 8824, 8340, 7258, 5647, 4423, 3805, 3563, 2914, 3294, 3727, 3194, 3966, 4484, 5251, 6302, 4912],
-    backgroundColor: '#000000',
-    borderColor: '#000000',
-    borderWidth: 1
-  }]
+const linedata_difference = {
+  labels: [
+    '2002',
+    '2003',
+    '2004',
+    '2005',
+    '2006',
+    '2007',
+    '2008',
+    '2009',
+    '2010',
+    '2011',
+    '2012',
+    '2013',
+    '2014',
+    '2015',
+    '2016',
+    '2017',
+    '2018',
+    '2019',
+    '2020',
+    '2021',
+    '2022',
+  ],
+  datasets: [
+    {
+      data: [
+        6947,
+        7259,
+        8034,
+        8430,
+        8667,
+        8824,
+        8340,
+        7258,
+        5647,
+        4423,
+        3805,
+        3563,
+        2914,
+        3294,
+        3727,
+        3194,
+        3966,
+        4484,
+        5251,
+        6302,
+        4912,
+      ],
+      backgroundColor: '#000000',
+      borderColor: '#000000',
+      borderWidth: 1,
+    },
+  ],
 };
 
 //THIS CODE MAKES THE CHART, MAKE SURE THE CTX VARIABLE DOES NOT MATCH ANY OTHER CTX VARIABLE
 //CHANGE ID-HERE TO WHATEVER YOU MADE YOUR ID IN THE HTML FILE, CHOOSE SOMETHING DESCRIPTIVE
-const ctxLINE= document.getElementById('difference-line');
+const ctxLINE_difference = document.getElementById('difference-line');
 //THE CHART VARIABLE NEEDS TO HAVE A UNIQUE NAME RENAME IT SOMETHING DESCRIPTIVE
-const chart = new Chart(ctxLINE, {
-    type: 'line',
-    data: linedata,
-    options:
-    {
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'Year'
-          }
+const chart_difference = new Chart(ctxLINE_difference, {
+  type: 'line',
+  data: linedata_difference,
+  options: {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Year',
         },
-        y: {
-          title: {
-            display: true,
-            text: 'Dollars($)'
-          }
-        }
       },
-      maintainAspectRatio: false,
-      plugins: {
-        legend: {
-            display: false
-        }
-      }
-    }
-  });
+      y: {
+        title: {
+          display: true,
+          text: 'U.S. Dollars',
+        },
+        ticks: {
+          callback: function(value, index, values) {
+            return '$' + value;
+          },
+        },
+      },
+    },
+    maintainAspectRatio: false,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Acoss all UCs: Out-of-pocket cost',
+        font: {
+          size: 20, // You can change the font size here
+        },
+      },
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
