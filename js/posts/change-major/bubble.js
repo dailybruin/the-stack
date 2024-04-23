a50484e328146ba8c2f9e3019a161f10f32d1555
Bethany Kim <bethanyyk@g.ucla.edu>
2024-04-23 12:36:11 -0700

    complete bubble chart

--- js/posts/change-major/bubble.js
+++ js/posts/change-major/bubble.js
@@ -1,684 +1,157 @@
 const data = {
   datasets: [
   {
-    label: 'AEROSPACE ENGINEERING',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 689
-    }]
-  },
-  {
-    label: 'AFRICAN AMERICAN STUDIES',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 90
-    }]
-  },
-  {
-    label: 'AMERICAN LITERATURE AND CULTURE',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 104
-    }]
-  },
-  {
-    label: 'ANTHROPOLOGY-BS',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 182
-    }]
-  },
-  {
-    label: 'ANTHROPOLOGY',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 1074
-    }]
-  },
-  {
-    label: 'APPLIED LINGUISTICS',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 57
-    }]
-  },
-  {
     label: 'APPLIED MATHEMATICS',
     data: [{
       x: 64,
       y: 13,
-      r: 1458
-    }]
-  },
-  {
-    label: 'ARCHITECTURAL STUDIES',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 137
-    }]
-  },
-  {
-    label: 'ART HISTORY',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 323
-    }]
-  },
-  {
-    label: 'ART',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 570
-    }]
-  },
-  {
-    label: 'ASIAN HUMANITIES',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 62
-    }]
-  },
-  {
-    label: 'ASIAN STUDIES',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 86
-    }]
-  },
-  {
-    label: 'ASTROPHYSICS',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 393
-    }]
+      r: 3.03
+    }],
+    backgroundColor: '#a5bffc'
   },
   {
     label: 'BIOCHEMISTRY',
     data: [{
       x: 45,
       y: 144,
-      r: 2694
-    }]
-  },
-  {
-    label: 'BIOENGINEERING',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 826
-    }]
-  },
-  {
-    label: 'BIOLOGY',
-    data: [{
-      x: 36,
-      y: 810,
-      r: 4809
-    }]
-  },
-  {
-    label: 'BIOPHYSICS',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 22
-    }]
+      r: 2.33
+    }],
+    backgroundColor: '#a5bffc'
   },
   {
     label: 'BUSINESS ECONOMICS',
     data: [{
       x: 116,
       y: 1419,
-      r: 4872
-    }]
-  },
-  {
-    label: 'CHEMICAL ENGINEERING',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 895
-    }]
-  },
-  {
-    label: 'CHEMISTRY',
-    data: [{
-      x: 0,
-      y: 45,
-      r: 743
-    }]
-  },
-  {
-    label: 'CHEMISTRY/MATERIALS SCIENCE',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 50
-    }]
-  },
-  {
-    label: 'CHICANA AND CHICANO STUDIES',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 215
-    }]
-  },
-  {
-    label: 'CIVIL ENGINEERING',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 1083
-    }]
+      r: 2.58
+    }],
+    backgroundColor: '#fff9a3'
   },
   {
     label: 'COGNITIVE SCIENCE',
     data: [{
       x: 98,
-      y: 0,
-      r: 1869
-    }]
-  },
-  {
-    label: 'COMMUNICATION',
-    data: [{
-      x: 128,
-      y: 0,
-      r: 1522
-    }]
-  },
-  {
-    label: 'COMPARATIVE LITERATURE',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 13
-    }]
+      y: 21,
+      r: 1.92
+    }],
+    backgroundColor: '#b5fec7'
   },
   {
     label: 'COMPUTATIONAL AND SYSTEMS BIOLOGY',
     data: [{
       x: 10,
       y: 10,
-      r: 714
-    }]
-  },
-  {
-    label: 'COMPUTER ENGINEERING',
-    data: [{
-      x: 40,
-      y: 0,
-      r: 311
-    }]
-  },
-  {
-    label: 'COMPUTER SCIENCE AND ENGINEERING',
-    data: [{
-      x: 0,
-      y: 133,
-      r: 638
-    }]
+      r: 1.79
+    }],
+    backgroundColor: '#b5fec7'
   },
   {
     label: 'COMPUTER SCIENCE',
     data: [{
       x: 342,
       y: 28,
-      r: 3601
-    }]
-  },
-  {
-    label: 'DANCE',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 272
-    }]
-  },
-  {
-    label: 'DATA THEORY',
-    data: [{
-      x: 192,
-      y: 73,
-      r: 242
-    }]
-  },
-  {
-    label: 'DESIGN / MEDIA ARTS',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 490
-    }]
-  },
-  {
-    label: 'ECOLOGY, BEHAVIOR, AND EVOLUTION',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 250
-    }]
+      r: 2.56
+    }],
+    backgroundColor: '#fb8c85'
   },
   {
     label: 'ECONOMICS',
     data: [{
       x: 852,
       y: 146,
-      r: 3733
-    }]
-  },
-  {
-    label: 'EDUCATION AND SOCIAL TRANSFORMATION',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 281
-    }]
-  },
-  {
-    label: 'ELECTRICAL ENGINEERING',
-    data: [{
-      x: 0,
-      y: 64,
-      r: 1708
-    }]
-  },
-  {
-    label: 'ENGLISH',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 2253
-    }]
-  },
-  {
-    label: 'ENVIRONMENTAL SCIENCE',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 1185
-    }]
-  },
-  {
-    label: 'ETHNOMUSICOLOGY',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 42
-    }]
-  },
-  {
-    label: 'FILM AND TELEVISION',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 292
-    }]
-  },
-  {
-    label: 'FINANCIAL ACTUARIAL MATHEMATICS',
-    data: [{
-      x: 0,
-      y: 11,
-      r: 621
-    }]
-  },
-  {
-    label: 'GENDER STUDIES',
-    data: [{
-      x: 0,
-      y: 27,
-      r: 289
-    }]
-  },
-  {
-    label: 'GEOGRAPHY',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 208
-    }]
-  },
-  {
-    label: 'GEOGRAPHY/ENVIRONMENTAL STUDIES',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 213
-    }]
-  },
-  {
-    label: 'GLOBAL JAZZ STUDIES',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 83
-    }]
-  },
-  {
-    label: 'GLOBAL STUDIES',
-    data: [{
-      x: 0,
-      y: 22,
-      r: 498
-    }]
-  },
-  {
-    label: 'HISTORY',
-    data: [{
-      x: 0,
-      y: 53,
-      r: 1997
-    }]
-  },
-  {
-    label: 'HUMAN BIOLOGY AND SOCIETY - BA',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 192
-    }]
+      r: 2.82
+    }],
+    backgroundColor: '#fff9a3'
   },
   {
     label: 'HUMAN BIOLOGY AND SOCIETY - BS',
     data: [{
       x: 102,
       y: 79,
-      r: 2018
-    }]
-  },
-  {
-    label: 'INTERNATIONAL DEVELOPMENT STUDIES',
-    data: [{
-      x: 22,
-      y: 0,
-      r: 536
-    }]
-  },
-  {
-    label: 'JAPANESE',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 96
-    }]
-  },
-  {
-    label: 'LABOR STUDIES',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 31
-    }]
-  },
-  {
-    label: 'LINGUISTICS AND COMPUTER SCIENCE',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 322
-    }]
-  },
-  {
-    label: 'LINGUISTICS AND PSYCHOLOGY',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 94
-    }]
-  },
-  {
-    label: 'LINGUISTICS',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 111
-    }]
-  },
-  {
-    label: 'MARINE BIOLOGY',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 166
-    }]
-  },
-  {
-    label: 'MATERIALS ENGINEERING',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 382
-    }]
-  },
-  {
-    label: 'MATHEMATICS FOR TEACHING',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 49
-    }]
+      r: 1.69
+    }],
+    backgroundColor: '#b5fec7'
   },
   {
     label: 'MATHEMATICS OF COMPUTATION',
     data: [{
       x: 13,
       y: 139,
-      r: 841
-    }]
-  },
-  {
-    label: 'MATHEMATICS',
-    data: [{
-      x: 0,
-      y: 64,
-      r: 716
-    }]
-  },
-  {
-    label: 'MATHEMATICS/APPLIED SCIENCE',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 43
-    }]
-  },
-  {
-    label: 'MATHEMATICS/ECONOMICS',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 884
-    }]
-  },
-  {
-    label: 'MECHANICAL ENGINEERING',
-    data: [{
-      x: 21,
-      y: 0,
-      r: 1970
-    }]
+      r: 1.70
+    }],
+    backgroundColor: '#a5bffc'
   },
   {
     label: 'MICROBIOLOGY, IMMUNOLOGY, AND MOLECULAR GENETICS',
     data: [{
       x: 37,
       y: 50,
-      r: 1806
-    }]
+      r: 3.18
+    }],
+    backgroundColor: '#b5fec7'
   },
   {
     label: 'MOLECULAR, CELL, AND DEVELOPMENTAL BIOLOGY',
     data: [{
       x: 222,
       y: 78,
-      r: 2860
-    }]
-  },
-  {
-    label: 'MUSIC EDUCATION',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 13
-    }]
-  },
-  {
-    label: 'MUSIC HISTORY AND INDUSTRY',
-    data: [{
-      x: 0,
-      y: 43,
-      r: 80
-    }]
-  },
-  {
-    label: 'MUSIC INDUSTRY',
-    data: [{
-      x: 43,
-      y: 0,
-      r: 43
-    }]
-  },
-  {
-    label: 'MUSIC PERFORMANCE',
-    data: [{
-      x: 12,
-      y: 0,
-      r: 361
-    }]
-  },
-  {
-    label: 'MUSIC',
-    data: [{
-      x: 0,
-      y: 12,
-      r: 23
-    }]
-  },
-  {
-    label: 'NEUROSCIENCE',
-    data: [{
-      x: 0,
-      y: 226,
-      r: 2268
-    }]
-  },
-  {
-    label: 'NURSING-PRELICENSURE',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 658
-    }]
-  },
-  {
-    label: 'PHILOSOPHY',
-    data: [{
-      x: 0,
-      y: 24,
-      r: 944
-    }]
-  },
-  {
-    label: 'PHYSICS',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 816
-    }]
+      r: 2.91
+    }],
+    backgroundColor: '#b5fec7'
   },
   {
     label: 'PHYSIOLOGICAL SCIENCE',
     data: [{
       x: 321,
       y: 65,
-      r: 2344
-    }]
+      r: 3.67
+    }],
+    backgroundColor: '#b5fec7'
   },
   {
     label: 'POLITICAL SCIENCE',
     data: [{
       x: 67,
       y: 254,
-      r: 6068
-    }]
+      r: 3.23
+    }],
+    backgroundColor: '#fff9a3'
   },
   {
     label: 'PSYCHOBIOLOGY',
     data: [{
       x: 622,
       y: 141,
-      r: 4799
-    }]
+      r: 3.56
+    }],
+    backgroundColor: '#b5fec7'
   },
   {
     label: 'PSYCHOLOGY',
     data: [{
       x: 126,
       y: 255,
-      r: 6446
-    }]
-  },
-  {
-    label: 'PUBLIC AFFAIRS',
-    data: [{
-      x: 50,
-      y: 0,
-      r: 1055
-    }]
+      r: 2.56
+    }],
+    backgroundColor: '#fff9a3'
   },
   {
     label: 'SOCIOLOGY',
     data: [{
       x: 191,
       y: 195,
-      r: 4701
-    }]
-  },
-  {
-    label: 'SPANISH',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 71
-    }]
+      r: 3.02
+    }],
+    backgroundColor: '#fff9a3'
   },
   {
     label: 'STATISTICS AND DATA SCIENCE',
     data: [{
       x: 83,
       y: 12,
-      r: 1199
-    }]
-  },
-  {
-    label: 'THEATER',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 824
-    }]
-  },
-  {
-    label: 'WORLD ARTS AND CULTURES',
-    data: [{
-      x: 0,
-      y: 0,
-      r: 226
-    }]
+      r: 2.33
+    }],
+    backgroundColor: '#a5bffc'
   }
 ]};
 
@@ -686,5 +159,45 @@ const ctx = document.getElementById('major-bubble');
 const config = new Chart(ctx, {
   type: 'bubble',
   data: data,
-  options: {}
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          generateLabels: function(chart) {
+            return [{
+              text: 'Life Sciences',
+              fillStyle: '#b5fec7',
+              hidden: false
+            }, {
+              text: 'Physical Sciences',
+              fillStyle: '#a5bffc',
+              hidden: false
+            }, {
+              text: 'Social Sciences',
+              fillStyle: '#fff9a3',
+              hidden: false
+            }, {
+              text: 'Engineering',
+              fillStyle: '#fb8c85',
+              hidden: false
+            }];
+          }
+        }
+      }
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Number of students who changed into the major'
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Number of students who changed out of the major'
+        }
+      }
+    }
+  }
 });
