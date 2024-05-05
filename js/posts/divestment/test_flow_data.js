7d84d432ec8797ed9f25283d98ef9f3adf818e88
Junwon Choi <cjunwon.tech@gmail.com>
2024-05-04 23:14:45 -0700

    Starter UC sankey

---
+++ js/posts/divestment/test_flow_data.js
@@ -0,0 +1,12 @@
+function getData() {
+  return [
+    ['GEP', 'Public Assets', 9570042852],
+    ['UCRP', 'Public Assets', 57790287100],
+    ['GEP', 'Alternative Assets', 10015469678],
+    ['UCRP', 'Alternative Assets', 25930503014],
+    ['GEP', 'Cash & Cash Equivalents', 935912381],
+    ['UCRP', 'Cash & Cash Equivalents', 4053179404],
+    ['GEP', 'Receivables / Payables & Other Adjustments', 220388278],
+    ['UCRP', 'Receivables / Payables & Other Adjustments', 477480068],
+  ];
+}
